8514065cba70fd9bdaab76688efc7c08e0ad3412
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2008-10-30 13:07:16 +0000

    Implemented #203: Basic authentication controller for login and logout.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@875 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ modules/default/controllers/AuthController.php
@@ -0,0 +1,181 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Module_Default
+ * @author      Ralf Claussnitzer (ralf.claussnitzer@slub-dresden.de)
+ * @copyright   Copyright (c) 2008, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Provides actions for basic authenticating login and logout.
+ *
+ * @category    Application
+ * @package     Module_Default
+ */
+class AuthController extends Zend_Controller_Action {
+
+    /**
+     * Default URL to goto after successful login.
+     *
+     * @var array
+     */
+    protected $_login_url = array('action' => 'index', 'controller' => 'index', 'module' => 'default');
+    
+    /**
+     * Default URL to goto after successful logout.
+     *
+     * @var array
+     */
+    protected $_logout_url = array('action' => 'index', 'controller' => 'index', 'module' => 'default');
+
+    /**
+     * Index action shows login form or logout link respectivly.
+     *
+     * @return void
+     */
+    public function indexAction() {
+        // Retrieve the current configured identity.
+        $identity = Zend_Auth::getInstance()->getIdentity();
+        if (empty($identity) === true) {
+            // Nobody is logged in, so present the login form.
+            $url = $this->view->url(array('action' => 'login', 'controller' => 'auth', 'module' => 'default'));
+            $this->view->form = $this->getLoginForm();
+            $this->view->form->setAction($url);
+        } else {
+            // Somebody is logged in, so present the logout link.
+            $url = $this->view->url(array('action' => 'logout', 'controller' => 'auth', 'module' => 'default'));
+            $text = 'Logout ' . $identity . '.';
+            $this->view->form = '<a href="' . $url . '">' . $text . '</a>';
+        }
+    }
+
+    /**
+     * Login action performs login attempt with login form data. After a successful login
+     * it redirects to the page configured by $_login_url.
+     *
+     * @return void
+     */
+    public function loginAction() {
+        if ($this->getRequest()->isPost() === true) {
+            // Credentials coming in via POST operation.
+            
+            // Get a login form instance for validation.
+            $form = $this->getLoginForm();
+            
+            // Get request data.
+            $data = $this->_request->getPost();
+            
+            if ($form->isValid($data) === true) {
+                // Form data is valid (including the hash field)
+                
+                // Perfom authentication attempt
+                $auth = new Opus_Security_AuthAdapter();
+                $auth->setCredentials($data['login'], $data['password']);
+                $auth_result = $auth->authenticate();
+
+                if ($auth_result->isValid() === true) {
+                    // Persistent the successful authenticated identity.
+                    Zend_Auth::getInstance()->getStorage()->write($data['login']);
+                    
+                    // Redirect to post login url.
+                    $action = $this->_login_url['action'];
+                    $controller = $this->_login_url['controller'];
+                    $module = $this->_login_url['module'];
+                    $this->_helper->_redirector($action, $controller, $module);
+                } else {
+                    // Put authentication failure message to the view.
+                    $this->view->auth_failed_msg = $auth_result->getMessages();
+                    $this->view->auth_failed_msg = $this->view->auth_failed_msg[0];
+                    // Populate the form again to trigger validator decorators.
+                    $form->populate($data);
+                    $this->view->form = $form;
+                }
+            } else {
+                // Put authentication failure message to the view.
+                $this->view->auth_failed_msg = 'Invalid credentials';
+                // Populate the form again to trigger validator decorators.
+                $form->populate($data);
+                $this->view->form = $form;
+            }
+            // Render index script to show the login form again.
+            $this->render('index');
+        } else {
+            // Redirect to index on GET operation.
+            $this->_helper->_redirector('index');
+        }
+    }
+
+    /**
+     * Logout action performs logout and redirects to the page configured by $_logout_url.
+     *
+     * @return void
+     */
+    public function logoutAction() {
+        Zend_Auth::getInstance()->clearIdentity();
+        $action = $this->_logout_url['action'];
+        $controller = $this->_logout_url['controller'];
+        $module = $this->_logout_url['module'];
+        $this->_helper->_redirector($action, $controller, $module);
+    }
+
+
+    /**
+     * Assembles and returns a login form.
+     *
+     * @return unknown
+     */
+    protected function getLoginForm() {
+        $form = new Zend_Form();
+
+        // Add hash element to detect counterfeit formular data via validation.
+        $hash = new Zend_Form_Element_Hash('hash');
+
+        // Login name element.
+        $login = new Zend_Form_Element_Text('login');
+        $login->addValidator(new Zend_Validate_Alnum())
+            ->setRequired()
+            ->setLabel('Login');
+
+        // Password element.
+        $password = new Zend_Form_Element_Password('password');
+        $password->addValidator(new Zend_Validate_Alnum())
+            ->setRequired()
+            ->setLabel('Password');
+
+        // Submit button.
+        $submit = new Zend_Form_Element_Submit('SubmitCredentials');
+        $submit->setLabel('Login');
+
+        $form->setMethod('POST');
+        $form->addElements(array($hash, $login, $password, $submit));
+
+        return $form;
+    }
+
+}
