e3ab29fade25b525520efdafed7f83bbfba15b63
Felix Ostrowski <ostrowski@hbz-nrw.de>
2009-02-09 08:32:28 +0000

    Fixes wrong column name in testdata.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@1878 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/003_create_document_licence_testdata.sql
+++ tests/sql/003_create_document_licence_testdata.sql
@@ -2,7 +2,7 @@
 -- Daten für Tabelle `document_licences`
 --
 
-INSERT INTO `document_licences` (`licence_id`, `active`, `comment_internal`, `desc_markup`, `desc_text`, `language`, `link_licence`, `link_logo`, `link_sign`, `mime_type`, `name_long`, `pod_allowed`, `sort_order`) VALUES
+INSERT INTO `document_licences` (`id`, `active`, `comment_internal`, `desc_markup`, `desc_text`, `language`, `link_licence`, `link_logo`, `link_sign`, `mime_type`, `name_long`, `pod_allowed`, `sort_order`) VALUES
 (1, 1, NULL, NULL, NULL, '', '', NULL, NULL, '', 'Publishing license for publications including print on demand', 0, 0),
 (2, 1, NULL, NULL, NULL, '', '', NULL, NULL, '', 'Publishing license for publications excluding print on demand', 0, 0),
 (3, 1, NULL, NULL, NULL, '', '', NULL, NULL, '', 'Creative Commons - Attribution, Non Commercial, No Derivs', 0, 0),
