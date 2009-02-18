8289aa93efc73ae921df61970f927e97e7778034
Wolfgang Filter <wolfgang.filter@ub.uni-stuttgart.de>
2009-02-18 09:33:00 +0000

    change class FrontdoorNew_IndexController in Frontdoor_IndexController
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@1952 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/frontdoor/controllers/IndexController.php
+++ modules/frontdoor/controllers/IndexController.php
@@ -43,7 +43,7 @@
  *
  *
  */
-class FrontdoorNew_IndexController extends Zend_Controller_Action
+class Frontdoor_IndexController extends Zend_Controller_Action
 {
 
     public function indexAction()
