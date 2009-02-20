1602edbab840dc1dc3bb5338653cbb3aa8086659
Tobias Tappe <tobias.tappe@uni-bielefeld.de>
2009-02-20 10:54:54 +0000

    Adapted testdata to change of hidden root node id from 0 to 1
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@1975 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/001_create_collections_testdata.sql
+++ tests/sql/001_create_collections_testdata.sql
@@ -16,6 +16,28 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Tabellenstruktur für Tabelle `collections_roles`
+--
+
+CREATE TABLE IF NOT EXISTS `collections_roles` (
+  `id` int(11) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `position` int(11) unsigned NOT NULL,
+  `link_docs_path_to_root` tinyint(1) unsigned NOT NULL default '0' COMMENT 'If not 0: Every document belonging to a collection C automatically belongs to every collection on the path from C to the root of the collection tree.',
+  `visible` tinyint(1) unsigned NOT NULL default '1',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Verwaltungstabelle fuer die einzelnen Collection-Baeume';
+
+--
+-- Daten für Tabelle `collections_roles`
+--
+
+INSERT INTO `collections_roles` (`id`, `name`, `position`, `link_docs_path_to_root`, `visible`) VALUES
+(1, 'Dewey Decimal Classification (DDC)', 1, 0, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Tabellenstruktur für Tabelle `collections_contents_1`
 --
 
@@ -31,42 +53,43 @@ CREATE TABLE IF NOT EXISTS `collections_contents_1` (
 --
 
 INSERT INTO `collections_contents_1` (`id`, `name`, `number`) VALUES
-(1, 'Informatik, Informationswissenschaft, allgemeine Werke', '000'),
-(2, 'Philosophie und Psychologie', '100'),
-(3, 'Religion', '200'),
-(4, 'Sozialwissenschaften', '300'),
-(5, 'Sprache', '400'),
-(6, 'Naturwissenschaften und Mathematik', '500'),
-(7, 'Technik, Medizin, angewandte Wissenschaften', '600'),
-(8, 'Künste und Unterhaltung', '700'),
-(9, 'Literatur', '800'),
-(10, 'Geschichte und Geografie', '900'),
-(11, 'Informatik, Wissen, Systeme', '000'),
-(12, 'Bibliografien', '010'),
-(13, 'Bibliotheks- und Informationswissenschaften', '020'),
-(14, 'Enzyklopädien, Faktenbücher', '030'),
-(15, 'Zeitschriften, fortlaufende Sammelwerke', '050'),
-(16, 'Verbände, Organisationen, Museen', '060'),
-(17, 'Publizistische Medien, Journalismus, Verlagswesen', '070'),
-(18, 'Allgemeine Sammelwerke, Zitatensammlungen', '080'),
-(19, 'Handschriften, seltene Bücher', '090'),
-(20, 'Informatik, Informationswissenschaft, allgemeine Werke', '000'),
-(21, 'Wissen', '001'),
-(22, 'Das Buch', '002'),
-(23, 'Systeme', '003'),
-(24, 'Datenverarbeitung; Informatik', '004'),
-(25, 'Computerprogrammierung, Programme, Daten', '005'),
-(26, 'Spezielle Computerverfahren', '006'),
-(27, 'Philosophie', '100'),
-(28, 'Metaphysik', '110'),
-(29, 'Epistemologie', '120'),
-(30, 'Parapsychologie, Okkultismus', '130'),
-(31, 'Philosophische Schulen', '140'),
-(32, 'Psychologie', '150'),
-(33, 'Logik', '160'),
-(34, 'Ethik', '170'),
-(35, 'Antike, mittelalterliche und östliche Philosophie', '180'),
-(36, 'Neuzeitliche westliche Philosophie', '190');
+(1, 'Hidden Root Node', ''),
+(2, 'Informatik, Informationswissenschaft, allgemeine Werke', '000'),
+(3, 'Philosophie und Psychologie', '100'),
+(4, 'Religion', '200'),
+(5, 'Sozialwissenschaften', '300'),
+(6, 'Sprache', '400'),
+(7, 'Naturwissenschaften und Mathematik', '500'),
+(8, 'Technik, Medizin, angewandte Wissenschaften', '600'),
+(9, 'Künste und Unterhaltung', '700'),
+(10, 'Literatur', '800'),
+(11, 'Geschichte und Geografie', '900'),
+(12, 'Informatik, Wissen, Systeme', '000'),
+(13, 'Bibliografien', '010'),
+(14, 'Bibliotheks- und Informationswissenschaften', '020'),
+(15, 'Enzyklopädien, Faktenbücher', '030'),
+(16, 'Zeitschriften, fortlaufende Sammelwerke', '050'),
+(17, 'Verbände, Organisationen, Museen', '060'),
+(18, 'Publizistische Medien, Journalismus, Verlagswesen', '070'),
+(19, 'Allgemeine Sammelwerke, Zitatensammlungen', '080'),
+(20, 'Handschriften, seltene Bücher', '090'),
+(21, 'Informatik, Informationswissenschaft, allgemeine Werke', '000'),
+(22, 'Wissen', '001'),
+(23, 'Das Buch', '002'),
+(24, 'Systeme', '003'),
+(25, 'Datenverarbeitung; Informatik', '004'),
+(26, 'Computerprogrammierung, Programme, Daten', '005'),
+(27, 'Spezielle Computerverfahren', '006'),
+(28, 'Philosophie', '100'),
+(29, 'Metaphysik', '110'),
+(30, 'Epistemologie', '120'),
+(31, 'Parapsychologie, Okkultismus', '130'),
+(32, 'Philosophische Schulen', '140'),
+(33, 'Psychologie', '150'),
+(34, 'Logik', '160'),
+(35, 'Ethik', '170'),
+(36, 'Antike, mittelalterliche und östliche Philosophie', '180'),
+(37, 'Neuzeitliche westliche Philosophie', '190');
 
 -- --------------------------------------------------------
 
@@ -95,28 +118,6 @@ CREATE TABLE IF NOT EXISTS `collections_replacement_1` (
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `collections_roles`
---
-
-CREATE TABLE IF NOT EXISTS `collections_roles` (
-  `id` int(11) unsigned NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `position` int(11) unsigned NOT NULL,
-  `link_docs_path_to_root` tinyint(1) unsigned NOT NULL default '0' COMMENT 'If not 0: Every document belonging to a collection C automatically belongs to every collection on the path from C to the root of the collection tree.',
-  `visible` tinyint(1) unsigned NOT NULL default '1',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Verwaltungstabelle fuer die einzelnen Collection-Baeume';
-
---
--- Daten für Tabelle `collections_roles`
---
-
-INSERT INTO `collections_roles` (`id`, `name`, `position`, `link_docs_path_to_root`, `visible`) VALUES
-(1, 'Dewey Decimal Classification (DDC)', 1, 0, 1);
-
--- --------------------------------------------------------
-
---
 -- Tabellenstruktur für Tabelle `collections_structure_1`
 --
 
@@ -135,43 +136,43 @@ CREATE TABLE IF NOT EXISTS `collections_structure_1` (
 --
 
 INSERT INTO `collections_structure_1` (`id`, `collections_id`, `left`, `right`, `visible`) VALUES
-(1, 0, 1, 74, 0),
-(2, 1, 2, 35, 1),
-(3, 2, 36, 57, 1),
-(4, 3, 58, 59, 1),
-(5, 4, 60, 61, 1),
-(6, 5, 62, 63, 1),
-(7, 6, 64, 65, 1),
-(8, 7, 66, 67, 1),
-(9, 8, 68, 69, 1),
-(10, 9, 70, 71, 1),
-(11, 10, 72, 73, 1),
-(12, 11, 3, 18, 1),
-(13, 12, 19, 20, 1),
-(14, 13, 21, 22, 1),
-(15, 14, 23, 24, 1),
-(16, 15, 25, 26, 1),
-(17, 16, 27, 28, 1),
-(18, 17, 29, 30, 1),
-(19, 18, 31, 32, 1),
-(20, 19, 33, 34, 1),
-(21, 20, 4, 5, 1),
-(22, 21, 6, 7, 1),
-(23, 22, 8, 9, 1),
-(24, 23, 10, 11, 1),
-(25, 24, 12, 13, 1),
-(26, 25, 14, 15, 1),
-(27, 26, 16, 17, 1),
-(28, 27, 37, 38, 1),
-(29, 28, 39, 40, 1),
-(30, 29, 41, 42, 1),
-(31, 30, 43, 44, 1),
-(32, 31, 45, 46, 1),
-(33, 32, 47, 48, 1),
-(34, 33, 49, 50, 1),
-(35, 34, 51, 52, 1),
-(36, 35, 53, 54, 1),
-(37, 36, 55, 56, 1);
+(1, 1, 1, 74, 0),
+(2, 2, 2, 35, 1),
+(3, 3, 36, 57, 1),
+(4, 4, 58, 59, 1),
+(5, 5, 60, 61, 1),
+(6, 6, 62, 63, 1),
+(7, 7, 64, 65, 1),
+(8, 8, 66, 67, 1),
+(9, 9, 68, 69, 1),
+(10, 10, 70, 71, 1),
+(11, 11, 72, 73, 1),
+(12, 12, 3, 18, 1),
+(13, 13, 19, 20, 1),
+(14, 14, 21, 22, 1),
+(15, 15, 23, 24, 1),
+(16, 16, 25, 26, 1),
+(17, 17, 27, 28, 1),
+(18, 18, 29, 30, 1),
+(19, 19, 31, 32, 1),
+(20, 20, 33, 34, 1),
+(21, 21, 4, 5, 1),
+(22, 22, 6, 7, 1),
+(23, 23, 8, 9, 1),
+(24, 24, 10, 11, 1),
+(25, 25, 12, 13, 1),
+(26, 26, 14, 15, 1),
+(27, 27, 16, 17, 1),
+(28, 28, 37, 38, 1),
+(29, 29, 39, 40, 1),
+(30, 30, 41, 42, 1),
+(31, 31, 43, 44, 1),
+(32, 32, 45, 46, 1),
+(33, 33, 47, 48, 1),
+(34, 34, 49, 50, 1),
+(35, 35, 51, 52, 1),
+(36, 36, 53, 54, 1),
+(37, 37, 55, 56, 1);
 
 -- --------------------------------------------------------
 
