bed2663d4eeee304da55b91b1ae225d4dccc1fc8
Wolfgang Filter <wolfgang.filter@ub.uni-stuttgart.de>
2009-03-12 11:03:35 +0000

    omitting order-function, cleaning code, comment-structuring
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2155 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/frontdoornew/controllers/IndexController.php
+++ modules/frontdoornew/controllers/IndexController.php
@@ -32,13 +32,17 @@
  * @version     $Id$
  *
  *
+ */
+
+/**
  *
  *
- *
- * This controller gets an 3-dimensional array with all document fields from ModelDocument.
- * The array has to be proofed for occupied values, then modified to one dimension and pass through
- * filters using functions concerning order and relevance for displaying them in the Frontdoor View.
- * Keywords are collected and combined with language information and converte to strings.
+ * The controller produces an (3-dimensional) array with all fields from
+ * ModelDocument. This array has to be proofed for occupied values, then reduced
+ * to one dimension with recursive iteration and pass through a filter using a
+ * function concerning relevance (Stopwords). Keywords are collected and combined
+ * with language information, converted to strings and added to the resulting array
+ * $mydocdata.
  *
  *
  *
@@ -54,20 +58,19 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
         $document = new Opus_Document($docId);
         $documentType = $document->getType();
         $doc_data = $document->toArray();
-        //print_r($doc_data);
-
-// Filter for relevant keys and order for Frontdoor view
-
+   /**
+    *
+    * Filter for relevant keys. Getting Document Type
+    *
+    */
         $document_data = $this->filterStopwords($doc_data);
-        $document_data['DocumentType'] = $this->view->translate($documentType);
-        $result = $this->my_sort($document_data);
-        //$this->view->result = $result;
-        //print_r($result);
-
-
-//Recursive Iteration of occcupied values in $result
-
-        $arit = new RecursiveArrayIterator($result);
+        $document_data['Type'] = $this->view->translate($documentType);
+   /**
+    *
+    *Recursive Iteration of occcupied values in $document_data
+    *
+    */
+        $arit = new RecursiveArrayIterator($document_data);
         $ritit = new RecursiveIteratorIterator($arit);
         foreach ($ritit as $key => $value)
         {
@@ -76,10 +79,12 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                $mydoc_data_values[] = $value;
             }
         }
-
-   // Iteration for keys in $doc_data for max. 2 sub-arrays
-
-        foreach ($result as $key => $value)
+   /**
+    *
+    *Iteration of keys (with occupied values) in $document_data for max. 2 sub-arrays
+    *
+    */
+        foreach ($document_data as $key => $value)
         {
            if (empty($value) == false)
            {
@@ -93,11 +98,7 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                      $array2 = $value1;
                      foreach ($array2 as $key2 => $value2)
                      {
-                        if (is_array ($value2))
-                        {
-                        // do nothing!
-                        }
-                        else
+                        if (is_array ($value2) === false)
                         {
                            if (empty($value2) == false)
                            {
@@ -121,11 +122,17 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
              }
           }
         }
+   /**
+    *
+    *Combining keys and values in one array
+    *
+    */
         $mydoc_data = array_combine ($mydoc_data_keys, $mydoc_data_values);
-        //Test: print_r ($mydoc_data);
-
-
-   // Collecting SWD-Keywords and and combining them with language information
+   /**
+    *
+    *Collecting SWD-Keywords and and combining them with language information
+    *
+    */
         $myswd_value = Array();
         $myswd_lan = Array();
         foreach ($mydoc_data as $key => $value)
@@ -142,13 +149,10 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                 }
             }
         }
-        //Test: print_r ($myswd_value);
         $mykey_eng = Array();
         $mykey_ger = Array();
         if (array_key_exists ('0', $myswd_lan) == true)
         {
-            //$mykey_eng = Array();
-            //$mykey_ger = Array();
             foreach ($myswd_lan as $key => $value)
             {
                if ($value == 'de')
@@ -161,13 +165,10 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                }
             }
          }
-        //Test: print_r ($mykey_ger);
         $myswd_ger = Array();
         $myswd_eng = Array();
         if (array_key_exists ('0', $myswd_value) == true)
         {
-            //$myswd_ger = Array();
-            //$myswd_eng = Array();
             foreach ($myswd_value as $key => $value)
             {
                if ( in_array($key, $mykey_ger, true))
@@ -180,8 +181,6 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                }
             }
         }
-        //Test: print_r ($swd_eng);
-        //Test: print_r ($swd_ger)
         $swd_eng = Array();
         $swd_ger = Array();
         if (array_key_exists ('0', $myswd_eng) == true)
@@ -194,12 +193,11 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
           $swd_ger = implode (', ' , $myswd_ger);
           $mydoc_data['Swd_ger'] = $swd_ger;
         }
-        //Test: print_r ($swd_ger);
-        //Test: print_r ($mydoc_data);
-
-
- // Collecting uncontrolled Keywords and and combining them with language information
-
+   /**
+    *
+    *Collecting uncontrolled Keywords and and combining them with language information
+    *
+    */
         $myuncont_lan = array();
         $myuncont_value = array();
         foreach ($mydoc_data as $key => $value)
@@ -216,8 +214,6 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                 }
             }
         }
-        //Test: print_r ($myuncont_value);
-        //Test: print_r ($myuncont_lan);
         $mykey_uncont_eng = Array();
         $mykey_uncont_ger = Array();
         foreach ($myuncont_lan as $key => $value)
@@ -231,8 +227,6 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                 $mykey_uncont_eng[] = $key;
             }
         }
-        //Test: print_r ($mykey_uncont_ger);
-        //Test: print_r ($mykey_uncont_eng);
         $myuncont_ger = Array();
         $myuncont_eng = Array();
         foreach ($myuncont_value as $key => $value)
@@ -246,8 +240,6 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
                $myuncont_eng[] = $value;
             }
         }
-        //Test: print_r ($myuncont_eng);
-        //Test: print_r ($myuncont_ger);
         if (array_key_exists ('0', $myuncont_eng) == true)
         {
             $uncont_eng = implode (', ' , $myuncont_eng);
@@ -258,18 +250,15 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
             $uncont_ger = implode (', ' , $myuncont_ger);
             $mydoc_data['Uncontrolled_ger'] = $uncont_ger;
         }
-        //Test: print_r ($uncont_ger);
-        //Test: print_r ($uncont_eng);
-        //print_r ($mydoc_data); //Test
         $this->view->mydoc_data = $mydoc_data;
     }
 
 
-     /**
-      *
-      * List with stopwords
-      *
-      */
+/**
+ *
+ * List with stopwords for omitting irrelevant fields
+ *
+ */
 
 
     private $__stopwords = array('Active', 'CommentInternal', 'DescMarkup',
@@ -277,11 +266,11 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
         'ServerDateUnlocked', 'ServerDateValid', 'Source', 'SwbId', 'PatentCountries', 'PatentDateGranted',
         'PatentApplication', 'Enrichment', 'Email', 'PlaceOfBirth', 'DateOfBirth', 'AcademicTitle');
 
-     /**
-      *
-      * Filter: Stopwords
-      *
-      */
+/**
+ *
+ * Filter-function: Comparing stopword-list with keys in array
+ *
+ */
 
     private function filterStopwords(array &$fields) {
         $result = array();
@@ -295,138 +284,7 @@ class Frontdoornew_IndexController extends Zend_Controller_Action
             }
 
         }
-
         return $result;
     }
-
-    /**
-     *
-     * Filter: Weight (Order)
-     *
-     */
-
-    private function cmp_weight ($a, $b)
-    {
-
-        $weight = array(
-                          'IdentifierUrn' => -100,
-                          'TitleMain' => -90,
-                          'TitleParent' => -80,
-                          'PersonAuthor' => -70,
-                          'CreatingCorporation' => -60,
-                          'ContributingCorporation' => -50,
-                          'NonInstituteAffiliation' => -45,
-                          'SubjectSwd' => -40,
-                          'Swd' => -37,
-                          'SubjectDdc' => -30,
-                          'SubjectUncontrolled' => -20,
-                          'Reviewed' => -15,
-                          'PersonReferee' => -10,
-                          'PersonContributor' => -8,
-                          'PersonEditor' => -5,
-                          'PersonOther' => -2,
-                          'PublishedYear' => 5,
-                          'CompletedYear' => 10,
-                          'CompletedDate' => 12,
-                          'DateAccepted' => 20,
-                          'DocumentType' => 30,
-                          'Language' => 35,
-                          'PageNumber' => 40,
-                          'PageFirst' => 50,
-                          'PageLast' => 60,
-                          'Edition' => 70,
-                          'Issue'   => 80,
-                          'Isbn' => 90,
-                          'TitleAbstract' => 100,
-                          'Licence' => 120,
-        );
-
-        if (array_key_exists($a, $weight) === true)
-        {
-            $a_weight = $weight[$a];
-        }
-        else
-        {
-            $a_weight = 0;
-        }
-
-        if (array_key_exists($b, $weight) === true)
-        {
-            $b_weight = $weight[$b];
-        }
-        else
-        {
-            $b_weight = 0;
-        }
-
-        if ($a_weight === $b_weight)
-        {
-            return 0;
-        }
-        return ($a_weight < $b_weight) ? -1 : 1;
-
-    }
-
-    private function cmp_title_weight ($a, $b)
-    {
-        if ((array_key_exists('Language', $a) === false)
-            or (array_key_exists('Language', $b) === false)) {
-            return 0;
-        }
-
-
-        $lang_a = $a['Language'];
-        $lang_b = $b['Language'];
-
-        $weight = array ('de' => -30, 'en' => 0);
-
-        $a_weight = $weight[$lang_a];
-        $b_weight = $weight[$lang_b];
-
-        if ($a_weight === $b_weight);
-        {
-            return 0;
-        }
-    }
-
-    private function cmp_abstract_weight ($a, $b)
-    {
-
-        if ((array_key_exists('Language', $a) === false)
-            or (array_key_exists('Language', $b) === false)) {
-            return 0;
-        }
-
-
-        $lang_a = $a['Language'];
-        $lang_b = $b['Language'];
-
-        $weight = array ('de' => -30, 'en' => 0);
-
-        $a_weight = $weight[$lang_a];
-        $b_weight = $weight[$lang_b];
-
-        if ($a_weight === $b_weight);
-        {
-            return 0;
-        }
-    }
-
-    private function my_sort(array $a)
-    {
-        $cp = $a;
-        uksort($cp, array($this, 'cmp_weight'));
-
-        if (array_key_exists('TitleMain', $cp) === true) {
-            usort($cp['TitleMain'], array($this, 'cmp_title_weight'));
-        }
-
-        if (array_key_exists('TitleAbstract', $cp) === true) {
-            usort($cp['TitleAbstract'], array($this, 'cmp_abstract_weight'));
-        }
-        return $cp;
-    }
-
 }
-
 
