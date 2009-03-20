15ceb29c2c7abccad64f7c07cc77a6ddcf508142
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2009-03-20 13:43:49 +0000

    Incorporates security mechanism - disabled by default because security information is not aligned with current test data.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2223 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/Controller/Plugin/SecurityRealm.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Controller
+ * @author      Ralf Claussnitzer (ralf.claussnitzer@slub-dresden.de)
+ * @copyright   Copyright (c) 2008, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Identify the Role of the current User and set up Opus_Security_Realm with
+ * the approriate Role.
+ *
+ * @category    Application
+ * @package     Controller
+ */
+class Controller_Plugin_SecurityRealm extends Zend_Controller_Plugin_Abstract {
+
+    /**
+     * Determine the current User's security role and set up Opus_Security_Realm.
+     *
+     * @param Zend_Controller_Request_Abstract $request The current request.
+     * @return void
+     */
+    public function routeStartup(Zend_Controller_Request_Abstract $request) {
+        // Create a Realm instance
+        $realm = Opus_Security_Realm::getInstance();
+        $acl = new Opus_Security_Acl;
+
+        // Set up standard guest role as defined in the database
+        $guest = $acl->getRole('Opus/Security/Role/2');
+        
+        // Set up master Resource object
+        $masterResource = $acl->get('PUBLIC');
+        $realm->setResourceMaster($masterResource);
+        
+        // Start permission checks with assigning the Acl 
+        $realm->setRole($guest);
+        $realm->setAcl($acl);
+    }   
+    
+}
