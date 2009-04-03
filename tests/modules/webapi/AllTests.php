8835900c358dfd9230386193570eb83694243566
Henning Gerhardt <henning.gerhardt@slub-dresden.de>
2009-04-03 08:13:54 +0000

    Added tests for webapi service doctypes.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2353 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/modules/webapi/AllTests.php
+++ tests/modules/webapi/AllTests.php
@@ -36,6 +36,7 @@
 require_once 'PHPUnit/Framework.php';
 
 require_once 'modules/webapi/DocumentTests.php';
+require_once 'modules/webapi/DoctypesTests.php';
 require_once 'modules/webapi/SearchTests.php';
 
 /**
@@ -51,6 +52,7 @@ class Modules_Webapi_AllTests {
     public static function suite() {
         $suite = new PHPUnit_Framework_Testsuite('Opus Application Module: Webapi');
         $suite->addTestSuite('Modules_Webapi_DocumentTests');
+        $suite->addTestSuite('Modules_Webapi_DoctypesTests');
         $suite->addTestSuite('Modules_Webapi_SearchTests');
         return $suite;
     }
