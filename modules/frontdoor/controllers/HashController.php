fe24ec5d07bdc7ca74174270b52edccbc62792c4
Wolfgang Filter <wolfgang.filter@ub.uni-stuttgart.de>
2009-04-16 16:42:16 +0000

    counting available files for one document
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2487 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/frontdoor/controllers/HashController.php
+++ modules/frontdoor/controllers/HashController.php
@@ -4,10 +4,12 @@ class Frontdoor_HashController extends Zend_Controller_Action
     /**
      *
      *getting hashvalues from Opus_Document to display them
+     *case: only one file exists
+     *generalisation for any filenumbers has to be done
      *
      *
      */
-     public function indexAction()
+    public function indexAction()
     {
        $request = $this->getRequest();
        $docId = $request->getParam('docId');
@@ -17,11 +19,41 @@ class Frontdoor_HashController extends Zend_Controller_Action
        $title = $document->getTitleMain('0')->getValue();
        $this->view->title = $title;
 
+       if (is_array ($files = $document->getFile()) === true)
+       {
+          $filenumber = count($files);
+          $this->view->filenumber = $filenumber;
+          $first_hash = $document->getFile('0')->getHashValue('0')->getValue();
+          $this->view->first_hash = $first_hash;
+          $hash_values = array();
+          $hash_types = array();
+/*
+ * Iteration over all files and hashvalues (not completed)
+ *
+
+          if ($first_hash !== NULL)
+          {
+             for ($i = 0; $i < $fileNumber;)
+             {
+                 for ($j = 0; $j < 2; $i++)
+                 {
+                    $hash = $document->getFile($i)->getHashValue($j)->getValue();
+                    $type = $document->getFile($i)->getHashValue($j)->getType();
+
+                    $hash_values[$hash];
+                    $hash_types[$type];
+                 }
+             }
+          }
+*/
+       }
+
        $hash0 = $document->getFile('0')->getHashValue('0')->getValue();
        $hash0_type = $document->getFile('0')->getHashValue('0')->getType();
 
        $hash1 = $document->getFile('0')->getHashValue('1')->getValue();
        $hash1_type = $document->getFile('0')->getHashValue('1')->getType();
+
 
        $this->view->hash0_type = $hash0_type;
        $this->view->hash0 = $hash0;
