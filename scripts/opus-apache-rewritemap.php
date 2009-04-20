0a0e5e6425e40712e2254d29b4732c0e4afdc009
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2009-04-20 15:22:15 +0000

    Rewrite map application adopted to new bootstrapping process.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2510 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ scripts/opus-apache-rewritemap.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the North Rhine-Westphalian Library Service Center,
+ * the Cooperative Library Network Berlin-Brandenburg, the Saarland University
+ * and State Library, the Saxon State Library - Dresden State and University
+ * Library, the Bielefeld University Library and the University Library of
+ * Hamburg University of Technology with funding from the German Research
+ * Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License 
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51 
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category   Application
+ * @package    Opus_Deliver
+ * @author     Pascal-Nicolas Becker <becker@zib.de>
+ * @copyright  Copyright (c) 2009, OPUS 4 development team
+ * @license    http://www.gnu.org/licenses/gpl.html General Public License
+ * @version    $Id$
+ */
+
+// Configure include path.
+set_include_path('.' . PATH_SEPARATOR
+            . PATH_SEPARATOR . dirname(__FILE__)
+            . PATH_SEPARATOR . dirname(dirname(__FILE__)) . '/library'
+            . PATH_SEPARATOR . get_include_path());
+
+// Zend_Loader is'nt available yet. We have to do a require_once
+// in order to find the bootstrap class.
+require_once 'Application/Bootstrap.php';
+
+/**
+ * Bootstraps and runs the application.
+ *
+ * @category    Application
+ */
+class OpusApacheRewritemap extends Opus_Bootstrap_Base {
+
+    /**
+     * Sets URL to file directory.
+     * TODO: make configurable
+     *
+     * @var string  Defaults to '/workspace/files'. 
+     */
+    protected static $_absoluteFileDirURL = '/workspace/files';
+
+    /**
+     * Static function to rewrite document requests.
+     *
+     * @param string $request Input from apache, containing requested address and some information about the user.
+     *
+     * return string 
+     */
+    public function rewriteRequest($request) {
+        // TODO: make pathes configurable
+        return self::$_absoluteFileDirURL . '/' . $request;
+    }
+    
+    /**
+     * Empty method to not setup a backend.
+     *
+     * @return void
+     */
+    protected function _setupBackend() {
+    }    
+    
+    
+    /**
+     * Starts an Opus console.
+     *
+     * @return void
+     */
+    protected function _run() {
+        // Loop to read requests given by apache.
+        while($line = trim(fgets(STDIN))) {
+            echo $this->rewriteRequest($line) . "\n";
+        }
+        // ATTENTION: CODE BELONG THIS LINE WILL NEVER BE REACHED!
+    }
+
+}
+
+// start rewrite map
+$console = new OpusApacheRewritemap;
+$console->run(
+    // application root directory
+    dirname(dirname(__FILE__)),
+    // config level
+    Opus_Bootstrap_Base::CONFIG_TEST,
+    // path to config file
+    dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'config');
