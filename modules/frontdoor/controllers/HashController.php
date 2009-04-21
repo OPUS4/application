23e1f7366d7915f1a074c28021256abfba10ff2d
Wolfgang Filter <wolfgang.filter@ub.uni-stuttgart.de>
2009-04-21 15:44:13 +0000

    generalizing controller for multiple files and hashtypes
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2522 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/frontdoor/controllers/HashController.php
+++ modules/frontdoor/controllers/HashController.php
@@ -4,8 +4,9 @@ class Frontdoor_HashController extends Zend_Controller_Action
     /**
      *
      *getting hashvalues from Opus_Document to display them
-     *case: only one file exists
-     *generalisation for any filenumbers has to be done
+     *case: only one file exists (preliminary)
+     *generalisation for multiple files and hashtypes has been done here by iteration
+     *over all files, hashtypes and -values
      *
      *
      */
@@ -21,32 +22,46 @@ class Frontdoor_HashController extends Zend_Controller_Action
 
        if (is_array ($files = $document->getFile()) === true)
        {
-          $filenumber = count($files);
-          $this->view->filenumber = $filenumber;
+          $fileNumber = count($files);
+          $this->view->filenumber = $fileNumber;
           $first_hash = $document->getFile('0')->getHashValue('0')->getValue();
           $this->view->first_hash = $first_hash;
           $hash_values = array();
           $hash_types = array();
-/*
- * Iteration over all files and hashvalues (not completed)
- *
+          $hash_labels = array();
+          $hashOut = array();
+
+// Iteration over all files, hashtypes and -values
 
           if ($first_hash !== NULL)
           {
-             for ($i = 0; $i < $fileNumber;)
+             for ($i = 0; $i < $fileNumber; $i++)
              {
-                 for ($j = 0; $j < 2; $i++)
+                 if (is_array ($hashes = $document->getFile($i)->getHashValue()) === true)
                  {
-                    $hash = $document->getFile($i)->getHashValue($j)->getValue();
-                    $type = $document->getFile($i)->getHashValue($j)->getType();
+                     $hashNumber = count($hashes);
+                     $label = $document->getFile($i)->getLabel();
+                     $hash_labels[] = $label;
 
-                    $hash_values[$hash];
-                    $hash_types[$type];
-                 }
-             }
+                     for ($j = 0; $j < $hashNumber; $j++)
+                     {
+                        $hash = $document->getFile($i)->getHashValue($j)->getValue();
+                        $type = $document->getFile($i)->getHashValue($j)->getType();
+                        $hash_values[] = $hash;
+                        $hash_types[] = $type;
+                     }
+                  }
+              }
           }
-*/
        }
+       $this->view->hash_labels = $hash_labels;
+       if ($fileNumber = $hashNumber)
+       {
+           $hashOut = array_combine ($hash_values, $hash_types);
+           $this->view->hashOut = $hashOut;
+       }
+
+//getting hashvalues from first file (has to be replaced by iteration above, when view is adjusted)
 
        $hash0 = $document->getFile('0')->getHashValue('0')->getValue();
        $hash0_type = $document->getFile('0')->getHashValue('0')->getType();
