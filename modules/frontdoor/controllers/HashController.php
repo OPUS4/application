f327cc57497645e136038001976b7ed7fbf37d2a
Wolfgang Filter <wolfgang.filter@ub.uni-stuttgart.de>
2009-04-23 15:59:36 +0000

    changing controller for adapting view on files, hasvalues and hashtypes
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2555 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/frontdoor/controllers/HashController.php
+++ modules/frontdoor/controllers/HashController.php
@@ -4,18 +4,19 @@ class Frontdoor_HashController extends Zend_Controller_Action
     /**
      *
      *getting hashvalues from Opus_Document to display them
-     *case: only one file exists (preliminary)
-     *generalisation for multiple files and hashtypes has been done here by iteration
-     *over all files, hashtypes and -values
-     *
+     *case of multiple files and multiple hashtypes exists
+     *commits array 'hashLabel' with all filelabels
+     *commits array 'hashValueType' with all hashvalues and hashtypes
      *
      */
+
     public function indexAction()
     {
        $request = $this->getRequest();
        $docId = $request->getParam('docId');
        $this->view->docId = $docId;
        $document = new Opus_Document($docId);
+       $this->view->document = $document;
 
        $title = $document->getTitleMain('0')->getValue();
        $this->view->title = $title;
@@ -23,13 +24,12 @@ class Frontdoor_HashController extends Zend_Controller_Action
        if (is_array ($files = $document->getFile()) === true)
        {
           $fileNumber = count($files);
-          $this->view->filenumber = $fileNumber;
+          $this->view->fileNumber = $fileNumber;
           $first_hash = $document->getFile('0')->getHashValue('0')->getValue();
           $this->view->first_hash = $first_hash;
-          $hash_values = array();
-          $hash_types = array();
-          $hash_labels = array();
-          $hashOut = array();
+          $hashValueType = array();
+          $hashLabel = array();
+
 
 // Iteration over all files, hashtypes and -values
 
@@ -41,39 +41,23 @@ class Frontdoor_HashController extends Zend_Controller_Action
                  {
                      $hashNumber = count($hashes);
                      $label = $document->getFile($i)->getLabel();
-                     $hash_labels[] = $label;
+                     $hashLabel[] = $label;
 
                      for ($j = 0; $j < $hashNumber; $j++)
                      {
-                        $hash = $document->getFile($i)->getHashValue($j)->getValue();
-                        $type = $document->getFile($i)->getHashValue($j)->getType();
-                        $hash_values[] = $hash;
-                        $hash_types[] = $type;
+                        $hashValue = $document->getFile($i)->getHashValue($j)->getValue();
+                        $hashType = $document->getFile($i)->getHashValue($j)->getType();
+                        $hashValueType['hashValue_' .$i. '_' .$j] = $hashValue;
+                        $hashValueType['hashType_' .$i. '_' .$j] = $hashType;
+
                      }
+
                   }
               }
           }
        }
-       $this->view->hash_labels = $hash_labels;
-       if ($fileNumber = $hashNumber)
-       {
-           $hashOut = array_combine ($hash_values, $hash_types);
-           $this->view->hashOut = $hashOut;
-       }
-
-//getting hashvalues from first file (has to be replaced by iteration above, when view is adjusted)
-
-       $hash0 = $document->getFile('0')->getHashValue('0')->getValue();
-       $hash0_type = $document->getFile('0')->getHashValue('0')->getType();
-
-       $hash1 = $document->getFile('0')->getHashValue('1')->getValue();
-       $hash1_type = $document->getFile('0')->getHashValue('1')->getType();
-
-
-       $this->view->hash0_type = $hash0_type;
-       $this->view->hash0 = $hash0;
-       $this->view->hash1_type = $hash1_type;
-       $this->view->hash1 = $hash1;
+       $this->view->hashValueType = $hashValueType;
+       $this->view->hashLabel = $hashLabel;
     }
 
 }
