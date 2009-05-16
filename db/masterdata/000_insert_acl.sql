a55bffb4d731596a79974012797bc92175715222
Ralf Claußnitzer <ralf.claussnitzer@slub-dresden.de>
2009-05-16 14:41:42 +0000

    Adds root resource entries for all models.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@2837 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/masterdata/000_insert_acl.sql
+++ db/masterdata/000_insert_acl.sql
@@ -20,11 +20,26 @@ INSERT INTO resources (id, parent_id, name) VALUES
 ( 1, NULL,  'SYSTEM'),
 -- resource grouping public resources
 ( 2, 1,     'PUBLIC'),
--- resources for business objects
-(10, 2,     'Opus/Configuration'),
-(11, 2,     'Opus/Document'),
-(12, 2,     'Opus/Licence'),
-(13, 2,     'Opus/Person');
+-- parent resources for business objects
+(10, 2,     'Opus/Collection'),
+(11, 2,     'Opus/CollectionRole'),
+(12, 2,     'Opus/Configuration'),
+(13, 2,     'Opus/Document'),
+(14, 2,     'Opus/Enrichment'),
+(15, 2,     'Opus/File'),
+(16, 2,     'Opus/GPG'),
+(17, 2,     'Opus/HashValues'),
+(18, 2,     'Opus/Identifier'),
+(19, 2,     'Opus/Language'),
+(20, 2,     'Opus/Licence'),
+(21, 2,     'Opus/Note'),
+(22, 2,     'Opus/OrganisationalUnit'),
+(23, 2,     'Opus/OrganisationalUnits'),
+(24, 2,     'Opus/Patent'),
+(25, 2,     'Opus/Person'),
+(26, 2,     'Opus/Reference'),
+(27, 2,     'Opus/Subject'),
+(28, 2,     'Opus/Title');
 --
 -- Roles Privileges
 --
