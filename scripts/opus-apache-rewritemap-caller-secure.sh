81917449903d7b10f071d5bdd9acd4e57176d852
unknown <unknown>
2009-11-17 14:56:56 +0000

    RewriteMap-Script, das selbständig die root-Rechte ablegt und nach "www-data" wechselt.  So kann das Script höchstens als www-data Schaden anrichten, wenn mal was schiefgeht.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@3827 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ scripts/opus-apache-rewritemap-caller-secure.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+#
+# This script is needed to make php forget session information between to requests.
+# Also it makes the rewrite map more robust in case of php errors.
+
+set -x
+
+# please configure the next to lines
+PHP='/usr/bin/php5'
+MAP='/home/matheon/development/scripts/opus-apache-rewritemap.php'
+
+THIS_UID="`/usr/bin/id -u`"
+USER='www-data'
+
+# FIXME: Decide which tool to choose.  "su" is more portable, but "sudo"
+# FIXME: looks nicer in the process listing.
+SU='/bin/su'
+SUDO='/usr/bin/sudo'
+
+
+# FIXME: Every user-id below 10 is "magic" and shouldn't be used.
+# FIXME: Replace "magic" constant by something reasonable.
+if test "0$THIS_UID" -le 10; then
+
+   # exec $SU -c "$0" $USER
+   exec $SUDO -u $USER "$0"
+
+   # exec should never return, so this is just to prevent passing
+   exit
+fi
+
+
+# DO NOT CHANGES ANYTHING BELLOW THIS LINE, EXCEPT YOU REALLY KNOW WHAT YOU ARE DOING!
+# keep this quite simple!
+while read request
+do
+    # FIXME: Don't start RewriteMap-Script for *every* matching request.  Just
+    # FIXME: print one URL on STDOUT when a URL on STDIN arrives.  Forking PHP
+    # FIXME: and bringing up the whole framework is way too expensive.
+    echo `$PHP $MAP "$request"`
+done
