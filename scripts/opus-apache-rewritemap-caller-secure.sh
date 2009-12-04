54cb78ed945b04fab24fd80388c046e3fa6e4ff8
Pascal-Nicolas Becker <opus@pascal-becker.de>
2009-12-04 14:03:27 +0000

    Changes comments an path to RewriteMap.
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@3916 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/opus-apache-rewritemap-caller-secure.sh
+++ scripts/opus-apache-rewritemap-caller-secure.sh
@@ -5,35 +5,31 @@
 
 # please configure the next to lines
 PHP='/usr/bin/php5'
-MAP='/home/matheon/development/scripts/opus-apache-rewritemap.php'
+MAP='/home/developer/workspace/bsz_opus_application/scripts/opus-apache-rewritemap.php'
 
 THIS_UID="`/usr/bin/id -u`"
 USER='www-data'
 
-# FIXME: Decide which tool to choose.  "su" is more portable, but "sudo"
-# FIXME: looks nicer in the process listing.
 SU='/bin/su'
 SUDO='/usr/bin/sudo'
 
 
-# FIXME: Every user-id below 10 is "magic" and shouldn't be used.
-# FIXME: Replace "magic" constant by something reasonable.
+# Every user-id below 10 is "magic" and shouldn't be used.
+# Change this value to a reasonable value deppending your system settings.
 if test "0$THIS_UID" -le 10; then
 
-   # exec $SU -c "$0" $USER
-   exec $SUDO -u $USER "$0"
+    # Decide which tool to choose.  "su" is more portable, but "sudo"
+    # looks nicer in the process listing.
+    # exec $SU -c "$0" $USER
+    exec $SUDO -u $USER "$0"
 
-   # exec should never return, so this is just to prevent passing
-   exit
+	# exec should never return, so this is just to prevent passing
+	exit
 fi
-
 
 # DO NOT CHANGES ANYTHING BELLOW THIS LINE, EXCEPT YOU REALLY KNOW WHAT YOU ARE DOING!
 # keep this quite simple!
 while read request
 do
-    # FIXME: Don't start RewriteMap-Script for *every* matching request.  Just
-    # FIXME: print one URL on STDOUT when a URL on STDIN arrives.  Forking PHP
-    # FIXME: and bringing up the whole framework is way too expensive.
     echo `$PHP $MAP "$request"`
 done
