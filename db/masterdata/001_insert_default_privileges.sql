e64a3c27fc42aaf8ea97f99c071d004a00e79854
Pascal-Nicolas Becker <opus@pascal-becker.de>
2010-01-28 09:32:34 +0000

    Adds default values for table privileges. Renames 001_create_languages.sql into 010_create_languages.sql
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@4073 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ db/masterdata/001_insert_default_privileges.sql
@@ -0,0 +1,12 @@
+--
+-- Insert Default Roles
+--
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- System Roles
+--
+INSERT INTO privileges (id, role_id, privilege, document_server_state, file_id) VALUES
+(1, 2, 'publish', NULL, NULL),
+(2, 2, 'readMetadata', 'published', NULL);
