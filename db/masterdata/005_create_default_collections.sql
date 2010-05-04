b542da5b149f2be7fa99b666b92e18266b041cea
Oliver Marahrens <o.marahrens@tu-harburg.de>
2010-05-04 10:08:42 +0000

    fixes #600
    git-svn-id: https://svn.zib.de/opus4dev/application/trunk@4349 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/masterdata/005_create_default_collections.sql
+++ db/masterdata/005_create_default_collections.sql
@@ -7,9 +7,9 @@ LOCK TABLES `collections_roles` WRITE;
 /*!40000 ALTER TABLE `collections_roles` DISABLE KEYS */;
 INSERT INTO `collections_roles` 
 VALUES 
-(1,'Organisatorische Einheiten','org',1,'none',1, 1, 'Name', 1, 'Name', 1, 'Name'),
-(9, 'Collections', 'coll', 9, 'none', 1, 1, 'Name', 1, 'Name', 1, 'Name'),
-(10, 'Schriftenreihen', 'series', 10, 'none', 1, 1, 'Name', 1, 'Name', 1, 'Name');
+(1,'Organisatorische Einheiten','org',1,'count',1, 1, 'Name', 1, 'Name', 1, 'Name'),
+(9, 'Collections', 'coll', 9, 'count', 1, 1, 'Name', 1, 'Name', 1, 'Name'),
+(10, 'Schriftenreihen', 'series', 10, 'count', 1, 1, 'Name', 1, 'Name', 1, 'Name');
 /*!40000 ALTER TABLE `collections_roles` ENABLE KEYS */;
 UNLOCK TABLES;
 
