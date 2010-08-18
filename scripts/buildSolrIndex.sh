aacbf2db3d83433c0d066a81c13bbe972cd6b266
Sascha Szott <szott@gmx.de>
2010-08-18 20:03:01 +0000

    sasz: deleted buildSolrIndex.sh; added buildSolrIndex.sh.template instead
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@4805 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/buildSolrIndex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# dieses Script muss als www-data aufgerufen werden, da sonst einige der Zend-Cachedateien
-# unter /tmp unterschiedliche Besitzer haben (entweder vom ausführender Benutzer oder von www-data)
-# wenn das passiert, erhält man die folgende Warning vom Zend-Framework: Failed saving metadata to metadataCache
-sudo -u www-data php5 SolrIndexBuilder.php "$@"
