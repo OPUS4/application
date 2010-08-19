b659f14a4180e305aaa30c52840180ba01385e27
Jens Schwidder <schwidder@zib.de>
2010-08-19 11:40:23 +0000

    Added some code to the error controller and page to show different information depending on the type of exception.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@4814 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/default/controllers/ErrorController.php
+++ modules/default/controllers/ErrorController.php
@@ -41,13 +41,30 @@
 
 class ErrorController extends Zend_Controller_Action
 {
-	/**
-	 * Just to be there. No actions taken.
-	 *
-	 * @return void
-	 *
-	 */
+    /**
+     * Just to be there. No actions taken.
+     *
+     * @return void
+     *
+     */
     public function errorAction()
     {
+        $errors = $this->_getParam('error_handler');
+        switch ($errors->type) {
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+                // 404 error -- controller or action not found
+                $this->getResponse()->setHttpResponseCode(404);
+                $this->view->message = 'Page not found';
+                break;
+            default:
+                // application error
+                $this->getResponse()->setHttpResponseCode(500);
+                $this->view->message = 'Application error';
+                break;
+        }
+        $this->view->exception = $errors->exception;
+        $this->view->request   = $errors->request;
     }
 }
