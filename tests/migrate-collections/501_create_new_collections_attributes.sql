f7f9d1d5440a42f70078da05ac6d3d2ddc52ab78
Thoralf Klein <thoralf@fischlustig.de>
2010-08-25 17:41:27 +0000

    Renamed "key" to "key_name" in enrichment tables.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@4962 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/migrate-collections/501_create_new_collections_attributes.sql
+++ tests/migrate-collections/501_create_new_collections_attributes.sql
@@ -3,15 +3,15 @@ CREATE TABLE collections_enrichments (
    -- zu der die Collection gehoert.
    id            INT UNSIGNED NOT NULL,
    collection_id INT(10) unsigned NOT NULL,                                                                                                                                                                         
-   `key`         VARCHAR(255),
-   `value`       VARCHAR(255),
+   key_name      VARCHAR(255),
+   value         VARCHAR(255),
 
    --
    -- Constraints.
    --
    FOREIGN KEY(collection_id)     REFERENCES collections(id),
    PRIMARY KEY(id),
-   INDEX(id, name)
+   INDEX(collection_id, key_name)
 ) ENGINE = InnoDB
 CHARACTER SET = 'utf8'
 COLLATE = 'utf8_general_ci';
