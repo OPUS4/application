ba75782ac0969822b967ae91f9ab25ee67a44edd
Julian Heise <julianheise@gmx.net>
2010-09-13 11:04:30 +0000

    imporved indentation
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@5362 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/frontdoor/controllers/HashController.php
+++ modules/frontdoor/controllers/HashController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * This file is part of OPUS. The software OPUS has been originally developed
@@ -36,96 +37,96 @@
  *
  *
  */
-class Frontdoor_HashController extends Zend_Controller_Action
-{
+class Frontdoor_HashController extends Zend_Controller_Action {
+
     /**
      *
-     *getting hashvalues from Opus_Document to display them
+     * getting hashvalues from Opus_Document to display them
      *
      */
+    public function indexAction() {
+        // get document
+        $request = $this->getRequest();
+        $docId = $request->getParam('docId');
+        $this->view->docId = $docId;
+        $document = new Opus_Document($docId);
+        // get authors
+        $author_names = array();
+        $authors = $document->getPersonAuthor();
+        // more than one author
+        if (true === is_array($authors)) {
+            $ni = 0;
+            foreach ($authors as $author) {
+                $author_names[$ni] = $author->getName();
+                $ni = $ni + 1;
+            }
+            // only one author
+        } else {
+            $author_names[0] = $document->getPersonAuthor()->getName();
+        }
+        $this->view->author = $author_names;
 
-    public function indexAction()
-    {
-       // get document
-       $request = $this->getRequest();
-       $docId = $request->getParam('docId');
-       $this->view->docId = $docId;
-       $document = new Opus_Document($docId);
-       // get authors
-       $author_names = array();
-       $authors = $document->getPersonAuthor();
-       // more than one author
-       if (true === is_array($authors)) {
-           $ni = 0;
-           foreach ($authors as $author) {
-               $author_names[$ni] = $author->getName();
-               $ni = $ni + 1;
-           }
-       // only one author
-       } else {
-           $author_names[0] = $document->getPersonAuthor()->getName();
-       }
-       $this->view->author = $author_names;
+        // get title
+        $title = $document->getTitleMain();
+        $title_value = null;
+        if (true === is_array($title)) {
+            $title_value = $title[0]->getValue();
+        } else {
+            $title_value = $title->getValue();
+        }
+        $this->view->title = $title_value;
 
-       // get title
-       $title = $document->getTitleMain();
-       if (true === is_array($title)) {
-           $title_value = $title[0]->getValue();
-       } else {
-           $title_value = $title->getValue();
-       }
-       $this->view->title = $title_value;
+        // get type
+        $type = $document->getType();
+        $this->view->type = $type;
 
-       // get type
-       $type = $document->getType();
-       $this->view->type = $type;
-
-      //searching for files, getting filenumbers and hashes
-       $fileNumber = 0;
-       $files = $document->getFile();
-       if (true === is_array($files) && count($files) > 0) {
-           $fileNumber = count($files);
-           $this->view->fileNumber = $fileNumber;
-       }
-       // Iteration over all files, hashtypes and -values
-       $gpg = new Opus_GPG();
-       $this->view->verifyResult = array();
-       $fileNames = array();
-       $hashType = array();
-       $hashSoll = array();
-       $hashIst = array();
-       $hashNumber = array();
-       for ($fi = 0; $fi < $fileNumber; $fi++) {
-           $fileNames[$fi] = $document->getFile($fi)->getPathName();
-           $hashNumber[$fi] = 0;
-           if ($document->getFile($fi)->exists() === true) {
-           if (true === is_array ($hashes = $document->getFile($fi)->getHashValue())) {
-                 $countHash = count($hashes);
-                 for ($hi = 0; $hi < $countHash; $hi++) {
-                     $hashNumber[$fi] = $countHash;
-                     $hashSoll[$fi][$hi] = $document->getFile($fi)->getHashValue($hi)->getValue();
-                     $hashType[$fi][$hi] = $document->getFile($fi)->getHashValue($hi)->getType();
-                     if (substr($hashType[$fi][$hi], 0, 3) === 'gpg') {
-                         try {
-                             $this->view->verifyResult[$fileNames[$fi]] = $gpg->verifyPublicationFile($document->getFile($fi));
-                         } catch (Exception $e) {
-                             $this->view->verifyResult[$fileNames[$fi]] = array('result' => array($e->getMessage()), 'signature' => $hashSoll[$fi][$hi]);
-                         }
-                     } else {
-                        $hashIst[$fi][$hi] = 0;
-                        if (true === $document->getFile($fi)->canVerify()) {
-                            $hashIst[$fi][$hi] = $document->getFile($fi)->getRealHash($hashType[$fi][$hi]);
+        //searching for files, getting filenumbers and hashes
+        $fileNumber = 0;
+        $files = $document->getFile();
+        if (true === is_array($files) && count($files) > 0) {
+            $fileNumber = count($files);
+            $this->view->fileNumber = $fileNumber;
+        }
+        // Iteration over all files, hashtypes and -values
+        $gpg = new Opus_GPG();
+        $this->view->verifyResult = array();
+        $fileNames = array();
+        $hashType = array();
+        $hashSoll = array();
+        $hashIst = array();
+        $hashNumber = array();
+        for ($fi = 0; $fi < $fileNumber; $fi++) {
+            $fileNames[$fi] = $document->getFile($fi)->getPathName();
+            $hashNumber[$fi] = 0;
+            if ($document->getFile($fi)->exists() === true) {
+                if (true === is_array($hashes = $document->getFile($fi)->getHashValue())) {
+                    $countHash = count($hashes);
+                    for ($hi = 0; $hi < $countHash; $hi++) {
+                        $hashNumber[$fi] = $countHash;
+                        $hashSoll[$fi][$hi] = $document->getFile($fi)->getHashValue($hi)->getValue();
+                        $hashType[$fi][$hi] = $document->getFile($fi)->getHashValue($hi)->getType();
+                        if (substr($hashType[$fi][$hi], 0, 3) === 'gpg') {
+                            try {
+                                $this->view->verifyResult[$fileNames[$fi]] = $gpg->verifyPublicationFile($document->getFile($fi));
+                            } catch (Exception $e) {
+                                $this->view->verifyResult[$fileNames[$fi]] = array('result' => array($e->getMessage()), 'signature' => $hashSoll[$fi][$hi]);
+                            }
+                        } else {
+                            $hashIst[$fi][$hi] = 0;
+                            if (true === $document->getFile($fi)->canVerify()) {
+                                $hashIst[$fi][$hi] = $document->getFile($fi)->getRealHash($hashType[$fi][$hi]);
+                            }
                         }
-                     }
-                 }
-           }
-           }
-       }
-       $this->view->hashType = $hashType;
-       $this->view->hashSoll = $hashSoll;
-       $this->view->hashIst = $hashIst;
-       $this->view->hashNumber = $hashNumber;
-       $this->view->fileNames = $fileNames;
-       $this->view->fileNumber = $fileNumber;
+                    }
+                }
+            }
+        }
+        $this->view->hashType = $hashType;
+        $this->view->hashSoll = $hashSoll;
+        $this->view->hashIst = $hashIst;
+        $this->view->hashNumber = $hashNumber;
+        $this->view->fileNames = $fileNames;
+        $this->view->fileNumber = $fileNumber;
     }
+
 }
