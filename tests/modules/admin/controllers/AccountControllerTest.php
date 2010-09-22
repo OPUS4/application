e2f7293d82bacf48fa898b507e927b12a4717665
Jens Schwidder <schwidder@zib.de>
2010-09-22 05:29:24 +0000

    Added basic unit tests for Admin_AccountController. Issue #OPUSVIER-523 - Add unit tests for administration controllers
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@5686 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/modules/admin/controllers/AccountControllerTest.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    TODO
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2010, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Basic unit tests for the Admin_AccountController class.
+ */
+class Admin_AccountControllerTest extends ControllerTestCase {
+
+    /**
+     * Tests routing to and successfull execution of 'index' action.
+     */
+    public function testIndexAction() {
+        $this->dispatch('/admin/account');
+        $this->assertResponseCode(200);
+        $this->assertModule('admin');
+        $this->assertController('account');
+        $this->assertAction('index');
+    }
+
+    /**
+     * Tests showing an account.
+     */
+    public function testShowAction() {
+        $this->dispatch('/admin/account/show/id/1');
+        $this->assertResponseCode(200);
+        $this->assertModule('admin');
+        $this->assertController('account');
+        $this->assertAction('show');
+    }
+
+    /**
+     * Tests showing form for new account.
+     */
+    public function testNewAction() {
+        $this->dispatch('/admin/account/new');
+        $this->assertResponseCode(200);
+        $this->assertModule('admin');
+        $this->assertController('account');
+        $this->assertAction('new');
+    }
+
+    /**
+     * Tests showing account for editing.
+     */
+    public function testEditAction() {
+        $this->dispatch('/admin/account/edit/id/1');
+        $this->assertResponseCode(200);
+        $this->assertModule('admin');
+        $this->assertController('account');
+        $this->assertAction('edit');
+    }
+
+    /**
+     * Tests creating a new account.
+     */
+    public function testCreateAction() {
+        $this->markTestIncomplete('Implement.');
+    }
+
+    /**
+     * Tests updating an account.
+     */
+    public function testUpdateAction() {
+        $this->markTestIncomplete('Implement.');
+    }
+
+    /**
+     * Tests deleting an account.
+     */
+    public function testDeleteAction() {
+        $this->markTestIncomplete('Implement.');
+    }
+
+}
+
+?>
