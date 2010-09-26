e14a055d4b06716fba3a16596b0a9e6743b00ab0
Thoralf Klein <thoralf@fischlustig.de>
2010-09-26 17:50:15 +0000

    Removed disfunctional import shell script.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@5898 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mv ../application/configs/doctypes ../config/xmldoctypes.opus
-mv ../application/configs/doctypes/import ../config/xmldoctypes
-
-php Opus3Migration.php
-
-mv ../config/xmldoctypes ../application/configs/doctypes/import
-mv ../config/xmldoctypes.opus ../application/configs/doctypes
