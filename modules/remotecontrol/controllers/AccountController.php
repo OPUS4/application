696bd59612aaafea1de539e0d625052a34e83aeb
Thoralf Klein <thoralf@fischlustig.de>
2010-10-08 17:59:18 +0000

    remotecontrol: Added first version of account controller.  Can only add accounts by now.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@6485 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ modules/remotecontrol/controllers/AccountController.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Module_Collection
+ * @author      Thoralf Klein <thoralf.klein@zib.de>
+ * @copyright   Copyright (c) 2010, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+class Remotecontrol_AccountController extends Controller_Action {
+
+    public function addAction() {
+        $this->_helper->layout()->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+
+        $request = $this->getRequest();
+
+        $login      = $request->getParam('login');
+        $password   = $request->getParam('password');
+        $user_roles = $request->getParam('user-roles');
+
+        $account = new Opus_Account();
+        $account->setLogin($login);
+        $account->setPassword($password);
+
+        try {
+            $account->store();
+        }
+        catch (Opus_Mail_Exception $e) {
+            $this->getResponse()->setHttpResponseCode(400);
+            $this->getResponse()->setBody("ERROR (while creating new account): " . $e->getMessage());
+            return;
+        }
+
+        foreach (explode(",", $user_roles) AS $role_name) {
+            $role_name = trim($role_name);
+            $role = Opus_Role::fetchByName($role_name);
+
+            if ($role instanceof Opus_Role) {
+                $account->addRole($role);
+            }
+        }
+
+        try {
+            $account->store();
+        }
+        catch (Opus_Mail_Exception $e) {
+            $this->getResponse()->setHttpResponseCode(400);
+            $this->getResponse()->setBody("ERROR (while creating roles): " . $e->getMessage());
+            return;
+        }
+
+        $this->getResponse()->setBody('SUCCESS');
+    }
+}
