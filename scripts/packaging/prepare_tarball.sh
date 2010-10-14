9b7278705acb1013479f6f18185d88d6775e309f
Thoralf Klein <thoralf@fischlustig.de>
2010-10-14 12:47:41 +0000

    tarball-script: Added solrconfig, removed obsolete directories.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@6527 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/packaging/prepare_tarball.sh
+++ scripts/packaging/prepare_tarball.sh
@@ -37,6 +37,7 @@ cd $TEMPDIR
 svn export https://svn.zib.de/opus4dev/server/$TAG opus4
 svn export https://svn.zib.de/opus4dev/framework/$TAG/db/schema opus4/db/schema
 svn export https://svn.zib.de/opus4dev/framework/$TAG/library/Opus opus4/library/Opus
+svn export https://svn.zib.de/opus4dev/solrconfig solrconfig
 
 
 #
@@ -50,7 +51,7 @@ rm -rv opus4/public/layouts/{opus33,opus34,darker,matheon}
 rm -rv opus4/import
 rm -v  opus4/modules/publish/views/scripts/form/preprintmatheon.phtml
 rm -rv opus4/application/configs/doctypes/import/{bibliography,repository}
-rm -rv opus4/modules/{pkm,publicationList}
+rm -rv opus4/modules/{pkm,publicationList,collections}
 rm -rv opus4/scripts/{packaging,cron,indexing,install}
 rm -r  opus4/scripts/*{Matheon,ZIB}*.php
 rm -rv opus4/library/Opus/Search/{Adapter,Index}/Lucene/
