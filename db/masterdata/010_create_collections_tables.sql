7490a4e4648681b30ab1221af68240be9bb6b086
Thoralf Klein <thoralf@fischlustig.de>
2010-10-19 14:26:32 +0000

    Collection data: Replace number='' by number=NULL in ddc collection to fulfill unique-constraint. Collection table: Added unique constraint to column "number".
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@6560 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/masterdata/010_create_collections_tables.sql
+++ db/masterdata/010_create_collections_tables.sql
@@ -37,6 +37,7 @@ CREATE TABLE IF NOT EXISTS `collections` (
   KEY `role_id` (`role_id`,`id`),
   UNIQUE KEY `role_id_left` (`role_id`,`left_id`),
   UNIQUE KEY `role_id_right` (`role_id`,`right_id`),
+  UNIQUE KEY `role_id_number` (`role_id`,`number`),
   KEY `parent_id` (`parent_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15985;
 
