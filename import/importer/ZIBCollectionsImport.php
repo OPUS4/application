3983337fda1ef1331c4cf631cc2e7fb6abfda4da
Gunar Maiwald <maiwald@zib.de>
2010-10-22 13:35:13 +0000

    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@6644 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- import/importer/ZIBCollectionsImport.php
+++ import/importer/ZIBCollectionsImport.php
@@ -44,9 +44,13 @@ class ZIBCollectionsImport {
 
 
         /*
-         * Serien sind flache Datenstrukturen. Unterhalb der Wurzel sind alle Serien auf zweiter Ebene angeordnet.
-         * Collections sind hierarchische Datenstrukturen. Mehrere Ebenen sind möglich.
-         * Daher werden zwei unterschiedliche Methoden für den Import verwendet
+         * Serien und Collections sollen für das ZIB in OPUS4 zusammengeführt werden. Folgende Struktur ist vereimnbart:
+         * - ZIB-Report
+         * - Preprint
+         * - Technical Report
+         * - Jahresbericht
+         * - Studienabschlussarbeiten
+         * - Dissertationen
          */
         //$collRole = Opus_CollectionRole::fetchByName('collections');
         $seriesRole = Opus_CollectionRole::fetchByName('series');
@@ -57,7 +61,6 @@ class ZIBCollectionsImport {
                 $this->importSeries($document, $seriesRole);
             }
             if ($document->getAttribute('name') === 'collections') {
-                //$this->importCollectionsDirectly($document, $seriesRole);
                 $this->importCollections($document, $seriesRole);
             }
 	}
@@ -82,107 +85,6 @@ class ZIBCollectionsImport {
 	}
 	return $classification;
     }
-
-    /**
-     * sort multidimensional arrays
-     */
-    private function msort($array, $id="id") {
-        $temp_array = array();
-        while(count($array)>0) {
-            $lowest_id = 0;
-            $index=0;
-            foreach ($array as $item) {
-                if (isset($item[$id]) && $array[$lowest_id][$id]) {
-                    if ($item[$id]<$array[$lowest_id][$id]) {
-                        $lowest_id = $index;
-                    }
-                }
-                $index++;
-            }
-            $temp_array[] = $array[$lowest_id];
-            $array = array_merge(array_slice($array, 0,$lowest_id), array_slice($array, $lowest_id+1));
-        }
-        return $temp_array;
-    }
-
-    /**
-     * Imports Collections from Opus3 to Opus4 directly (from DB-table to DB-tables)
-     *
-     * @param DOMDocument $data XML-Document to be imported
-     * @return array List of documents that have been imported
-     */
-    protected function importCollectionsDirectly($data, $collRole)  {
-        $collections = $this->transferOpusClassification($data);
-        
-        // sort by lft-values
-        $sorted_collections = $this->msort($collections, 'lft');
-
-        if (count($sorted_collections) == 0) {
-            // TODO: Improve error handling in case of empty collections.
-            throw new Exception("ERROR: Sorted collections empty.");
-        }
-
-        if ($sorted_collections[0]['lft'] != 1) {
-            var_dump($sorted_collections[0]);
-            // TODO: Improve error handling in case of wrong left-ids.
-            throw new Exception("ERROR: First left_id is not 1.");
-        }
-
-        // 1 is used as a predefined key and should not be used again!
-        // so lets increment all old IDs by 1
-        $previousRight = null;
-        $previousNode = array();
-        $new_collection = null;
-
-        // Build a mapping file to associate old IDs with the new ones
-        $fp = fopen('../workspace/tmp/collections.map', 'w');
-
-        foreach ($sorted_collections as $row) {
-
-            echo ".";
-            // parent_id is needed
-            if ($previousRight === null) {
-
-                $root = $collRole->getRootCollection();
-                $new_collection = $root->addLastChild();
-//                $collRole->store();
-
-                array_push($previousNode, $new_collection);
-            }
-            else if ( (int) $row['lft'] === (int) $previousRight+1) {
-            	// its a brother of previous node
-            	$left_brother = array_pop($previousNode);
-                $new_collection = $left_brother->addNextSibling();
-                $left_brother->store();
-                
-                array_push($previousNode, $new_collection);
-            }
-            else if ($row['rgt'] < $previousRight) {
-            	// its a child of previous node
-            	$father = array_pop($previousNode);
-                $new_collection = $father->addLastChild();
-                $father->store();
-
-            	array_push($previousNode, $father);
-            	array_push($previousNode, $new_collection);
-            }
-            else {
-                throw new Exception("Should never happen, ({$row['rgt']} < $previousRight) failed");
-            }
-
-            if ($previousRight === null) { continue;}
-            
-            $new_collection->setVisible(1);
-            $new_collection->setName($row['coll_name']);
-            $new_collection->store();
-            $previousRight = $row['rgt'];
-            fputs($fp, $row['coll_id'] . ' ' . $new_collection->getId() . "\n");
-        }
-        echo "\n";
-	fclose($fp);
-
-    }
-
 
     /**
      * Imports Series from Opus3 to Opus4
