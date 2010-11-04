30eb21294018d9ae32acc97210fcd8f9d9b02645
Thoralf Klein <thoralf@fischlustig.de>
2010-11-04 15:09:20 +0000

    sql masterdata: Added default privileges for reviewing-role.  Can now open unpublished documents in frontdoor.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@6833 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/masterdata/001_insert_default_privileges.sql
+++ db/masterdata/001_insert_default_privileges.sql
@@ -7,7 +7,9 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- System Roles
 --
-INSERT INTO privileges (id, role_id, privilege, document_server_state, file_id) VALUES
-(1, 2, 'publish', NULL, NULL),
-(2, 2, 'readMetadata', 'published', NULL),
-(5, 4, 'clearance', NULL, NULL);
+INSERT INTO privileges (role_id, privilege, document_server_state, file_id) VALUES
+(2, 'publish', NULL, NULL),
+(2, 'readMetadata', 'published', NULL),
+(4, 'clearance', NULL, NULL),
+(4, 'readMetadata', 'published', NULL),
+(4, 'readMetadata', 'unpublished', NULL);
--- db/masterdata/004_create_user_accounts.sql
+++ db/masterdata/004_create_user_accounts.sql
@@ -3,8 +3,9 @@
 -- Create user accounts
 -- 
 INSERT INTO `accounts` (`id`, `login`, `password`) VALUES
-(1, 'user',  sha1('useruser')),
-(2, 'admin', sha1('adminadmin'));
+(1, 'user',    sha1('useruser')),
+(2, 'admin',   sha1('adminadmin')),
+(4, 'referee', sha1('refereereferee'));
 
 --
 -- Assign Roles to Accounts
@@ -12,3 +13,4 @@ INSERT INTO `accounts` (`id`, `login`, `password`) VALUES
 INSERT INTO `link_accounts_roles` (`account_id`, `role_id`) VALUES
 (1, 2), -- "user" has role "guest"
 (2, 1); -- "admin" has role "admin"
+(4, 4); -- "referee" has role "reviewer"
