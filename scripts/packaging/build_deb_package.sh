120fadf629b0e485707774afed1e70a85e93c087
Sascha Szott <szott@gmx.de>
2010-11-05 23:00:56 +0000

    added DEBIAN files
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@6882 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ scripts/packaging/build_deb_package.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# LICENCE
+# This code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# @author      Sascha Szott <szott@zib.de>
+# @copyright   Copyright (c) 2010, OPUS 4 development team
+# @license     http://www.gnu.org/licenses/gpl.html General Public License
+# @version     $Id$
+
+set -e
+
+echo "create directory deb_package/var/local/opus4"
+mkdir -p deb_package/var/local/opus4
+
+echo "get OPUS 4 source code"
+./prepare_directories.sh trunk deb_package/var/local/opus4
+
+echo "create deb package"
+chmod +x deb_package/DEBIAN/postinst
+dpkg-deb --build deb_package .
