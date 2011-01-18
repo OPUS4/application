738292f959a4dceafdf279a13c2b0cf9534621c2
Thoralf Klein <thoralf@fischlustig.de>
2011-01-18 20:03:40 +0000

    matheon-imported: Don't use field "CompletedDate" any more.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@7317 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/MatheonMigration_Preprints.php
+++ scripts/MatheonMigration_Preprints.php
@@ -676,7 +676,6 @@ class MatheonMigration_Preprints extends MatheonMigration_Base {
             }
 
             //    <field name="approve_date">2003-12-10 00:00:00</field>
-            $doc->setCompletedDate($preprint['approve_date']);
             $doc->setPublishedDate($preprint['approve_date']);
 
             //    <field name="comment" xsi:nil="true" />
