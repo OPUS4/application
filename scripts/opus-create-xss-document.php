cf59e2c214c7281c04c0fc92bc2e01c1cc109f98
Thoralf Klein <thoralf@fischlustig.de>
2011-02-14 15:08:17 +0000

    server/opus-create-xss-document.php: Added even more XSS vectors to test document.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@7538 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/opus-create-xss-document.php
+++ scripts/opus-create-xss-document.php
@@ -25,7 +25,7 @@
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * @category    Application
- * @author      Pascal-Nicolas Becker <becker@zib.de>
+ * @author      Thoralf Klein <thoralf.klein@zib.de>
  * @copyright   Copyright (c) 2008-2010, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
@@ -36,140 +36,69 @@ require_once dirname(__FILE__) . '/common/bootstrap.php';
 
 $counter = 1;
 function randString($counter) {
-   $template = '<script language="javascript" type="text/javascript">alert(\'alert STRING\');</script>';
+   $template = '<i><script language="javascript" type="text/javascript">alert(\'alert STRING\');</script>';
    return str_replace('STRING', $counter, $template);
 }
 
 
+// error handler function
+function myErrorHandler($errno, $errstr, $errfile, $errline) {
+   echo "WARNING: myErrorHandler($errno, '$errstr', '$errfile', $errline)\n";
+   return true;
+}
+// set to the user defined error handler
+$old_error_handler = set_error_handler("myErrorHandler");
+
+
+
+//
+// Creating document, filling static fields.
+//
 $doc = new Opus_Document();
 $doc->setType(randString($counter++));
 $doc->setServerState('published');
 $doc->setServerDatePublished('01.01.1900');
+$doc->setLanguage('deu'.randString($counter++));
+$doc->setThesisDateAccepted('01.02.2003');
+$doc->setPublishedYear('2010');
+$doc->setPublishedDate('28.09.2010');
+$doc->setCompletedYear('2010');
+$doc->setCompletedDate('27.09.2010');
+$doc->setPublisherName(randString($counter++));
+$doc->setPublisherPlace(randString($counter++));
+$doc->setPageNumber(randString($counter++));
+$doc->setPageFirst(randString($counter++));
+$doc->setPageLast(randString($counter++));
+$doc->setVolume(randString($counter++));
+$doc->setIssue(randString($counter++));
+$doc->setCreatingCorporation(randString($counter++));
+$doc->setContributingCorporation(randString($counter++));
+$doc->setServerDateUnlocking("23.05.1949");
 
-// damn API. $doc->addPersonSubmiter() doesn't work for link models!
-// -> we should change this in 4.x
+
+
+//
+// Persons
+//
 $submitter = new Opus_Person();
+$submitter->getField('Email')->setValidator(null);
 $submitter->setFirstName(randString($counter++))
     ->setLastName(randString($counter++))
     ->setEmail(randString($counter++))
-    ->setDateOfBirth('13.03.1920')
+    ->setAcademicTitle(randString($counter++))
+    ->setDateOfBirth(randString($counter++))
     ->setPlaceOfBirth(randString($counter++));
 $doc->addPersonSubmitter($submitter);
 
 $author = new Opus_Person();
+$author->getField('Email')->setValidator(null);
 $author->setFirstName(randString($counter++))
     ->setLastName(randString($counter++))
-    ->setAcademicTitle(randString($counter++));
+    ->setEmail(randString($counter++))
+    ->setAcademicTitle(randString($counter++))
+    ->setDateOfBirth(randString($counter++))
+    ->setPlaceOfBirth(randString($counter++));
 $doc->addPersonAuthor($author);
-
-$doc->setLanguage('deu'.randString($counter++));
-
-$titleMain = $doc->addTitleMain();
-$titleMain->setValue(randString($counter++));
-$titleMain->setLanguage('deu'.randString($counter++));
-$titleMain2 = $doc->addTitleMain();
-$titleMain2->setValue(randString($counter++));
-$titleMain2->setLanguage('eng'.randString($counter++));
-
-$abstract = $doc->addTitleAbstract();
-$abstract->setValue(randString($counter++));
-$abstract->setLanguage('deu'.randString($counter++));
-
-$titleSub = $doc->addTitleSub();
-$titleSub->setValue(randString($counter++));
-$titleSub->setLanguage('deu'.randString($counter++));
-
-$titleAdditional = $doc->addTitleAdditional();
-$titleAdditional->setValue(randString($counter++));
-$titleAdditional->setLanguage('deu'.randString($counter++));
-
-$titleParent = $doc->addTitleParent();
-$titleParent->setValue(randString($counter++));
-$titleParent->setLanguage('fra'.randString($counter++));
-
-$doc->setPageNumber(randString($counter++));
-$doc->setPageFirst(randString($counter++));
-$doc->setPageLast(randString($counter++));
-
-$doc->setVolume(randString($counter++));
-$doc->setIssue(randString($counter++));
-
-$instituteName='Institut für empirische Forschung ' . randString($counter++);
-$institutesRole = Opus_CollectionRole::fetchByName('institutes');
-if (is_null($institutesRole) === true) {
-    $institutesRole = new Opus_CollectionRole();
-    $institutesRole->setName('institutes'.randString($counter++))
-                   ->setOaiName('institutes'.randString($counter++))
-                   ->setPosition(1)
-                   ->setVisible(1)
-                   ->setVisibleBrowsingStart(1)
-                   ->setDisplayBrowsing('Name')
-                   ->setVisibleFrontdoor(1)
-                   ->setDisplayFrontdoor('Name')
-                   ->setVisibleOai('Name')
-                   ->setDisplayOai('Name')
-                   ->store();
-}
-
-$instituteCollections = Opus_Collection::fetchCollectionsByRoleName($institutesRole->getId(), $instituteName);
-if (count($instituteCollections) >=1) {
-    $instituteCollection = $instituteCollections[0];
-} else {
-    $rootCollection = $institutesRole->getRootCollection();
-    if (is_null($rootCollection) === true) {
-        $rootCollection = $institutesRole->addRootCollection();
-        $rootCollection->setVisible(1)->store();
-        $institutesRole->store();
-    }
-    $instituteCollection = $rootCollection->addLastChild();
-    $instituteCollection->setVisible(1)
-                        ->setName(randString($counter++))
-                        ->store();
-}
-
-$doc->addCollection($instituteCollection);
-
-$doc->setPublishedYear('2010');
-$doc->setPublishedDate('28.09.2010');
-
-$doc->setPublisherName(randString($counter++));
-$doc->setPublisherPlace(randString($counter++));
-
-$doc->setCompletedYear('2010');
-$doc->setCompletedDate('27.09.2010');
-
-$o3id = $doc->addIdentifierOpus3();
-$o3id->setValue(randString($counter++));
-
-// empty URN will be automaticaly replace by new URN.
-$urn = $doc->addIdentifierUrn();
-$urn->setValue('urn:nbn:de:kobv:nn-opus-173:'.randString($counter++));
-
-$isbn = $doc->addIdentifierIsbn();
-$isbn->setValue('978-3-86680-192-9');
-
-$issn = $doc->addIdentifierIssn();
-$issn->setValue('1234-5678');
-
-$doc->addIdentifierOpac()->setValue(randString($counter++));
-
-$doc->setThesisDateAccepted('01.02.2003');
-
-
-
-$dnbInstitute=new Opus_DnbInstitute();
-$dnbInstitute->setName(randString($counter++));
-foreach(Opus_DnbInstitute::getGrantors() as $grantor) {
-    if ($dnbInstitute->getName() === $grantor->getName()) {
-        $dnbInstitute = $grantor;
-        break;
-    }
-}
-if (is_null($dnbInstitute->getId()) === true) {
-        $dnbInstitute->setCity(randString($counter++))->setIsGrantor(1)->store();
-}
-$doc->setThesisGrantor($dnbInstitute);
-$doc->setThesisPublisher($dnbInstitute);
 
 $referee = new Opus_Person();
 $referee->setFirstName('Gyro'.randString($counter++));
@@ -198,41 +127,151 @@ $contributor->setLastName('Smart'.randString($counter++));
 $contributor->store();
 $doc->addPersonContributor($contributor);
 
-$doc->setCreatingCorporation(randString($counter++));
-$doc->setContributingCorporation(randString($counter++));
 
-$swd = $doc->addSubjectSwd();
-$swd->setValue(randString($counter++));
-
-$free_subject_deu = $doc->addSubjectUncontrolled();
-$free_subject_deu->setLanguage(randString($counter++))->setValue(randString($counter++));
-
-$free_subject_eng = $doc->addSubjectUncontrolled();
-$free_subject_eng->setLanguage("eng\0".randString($counter++))->setValue(randString($counter++));
-
-$note1 = $doc->addNote();
-$note1->setVisibility('public')->setMessage(randString($counter++));
-$note2 = $doc->addNote();
-$note2->setVisibility('private')->setMessage(randString($counter++));
-
-$licences = Opus_Licence::getAll();
-if (count($licences) >= 1) {
-    $lic = $licences[0];
-} else {
-    $lic = new Opus_Licence();
-    $lic->setActive(1);
-    $lic->setLanguage('deu'.randString($counter++));
-    $lic->setLinkLicence(randString($counter++));
-    $lic->setNameLong(randString($counter++));
-    $lic->store();
+//
+// Titles
+//
+foreach (array('addTitleMain', 'addTitleAbstract', 'addTitleParent', 'addTitleSub', 'addTitleAdditional') AS $titleMethod) {
+   $doc->$titleMethod()
+      ->setValue(randString($counter++))
+      ->setLanguage(randString($counter++));
+   $doc->$titleMethod()
+      ->setValue(randString($counter++))
+      ->setLanguage('deu');
+   $doc->$titleMethod()
+      ->setValue(randString($counter++))
+      ->setLanguage('eng');
 }
+
+
+
+//
+// Collections
+//
+$institutesRole = new Opus_CollectionRole();
+$institutesRole->setName('institutes'.randString($counter++).rand())
+                   ->setOaiName('institutes'.randString($counter++).rand())
+                   ->setPosition(1)
+                   ->setVisible(1)
+                   ->setVisibleBrowsingStart(1)
+                   ->setDisplayBrowsing('Name')
+                   ->setVisibleFrontdoor(1)
+                   ->setDisplayFrontdoor('Name')
+                   ->setVisibleOai('Name')
+                   ->setDisplayOai('Name')
+                   ->store();
+
+$instituteName='Institut für empirische Forschung ' . randString($counter++);
+$instituteCollections = Opus_Collection::fetchCollectionsByRoleName($institutesRole->getId(), $instituteName);
+if (count($instituteCollections) >=1) {
+    $instituteCollection = $instituteCollections[0];
+} else {
+    $rootCollection = $institutesRole->getRootCollection();
+    if (is_null($rootCollection) === true) {
+        $rootCollection = $institutesRole->addRootCollection();
+        $rootCollection->setVisible(1)->store();
+        $institutesRole->store();
+    }
+    $instituteCollection = $rootCollection->addLastChild();
+    $instituteCollection->setVisible(1)
+                        ->setName(randString($counter++))
+                        ->store();
+}
+$doc->addCollection($instituteCollection);
+
+
+
+//
+// Identifiers
+//
+$o3id = $doc->addIdentifierOpus3();
+$o3id->setValue(randString($counter++));
+
+// empty URN will be automaticaly replace by new URN.
+$urn = $doc->addIdentifierUrn();
+$urn->setValue('urn:nbn:de:kobv:nn-opus-173:'.randString($counter++));
+
+$isbn = $doc->addIdentifierIsbn();
+$isbn->setValue('978-3-86680-192-9');
+
+$issn = $doc->addIdentifierIssn();
+$issn->setValue('1234-5678');
+
+$doc->addIdentifierOpac()->setValue(randString($counter++));
+
+
+
+//
+// DnbInstitutes
+//
+$dnbInstitute = new Opus_DnbInstitute();
+$dnbInstitute->setName(randString($counter++).rand())
+          ->setAddress(randString($counter++))
+          ->setCity(randString($counter++))
+          ->setPhone(randString($counter++))
+          ->setDnbContactId(randString($counter++))
+          ->setIsGrantor(1)
+          ->store();
+
+$doc->setThesisGrantor($dnbInstitute);
+$doc->setThesisPublisher($dnbInstitute);
+
+
+
+//
+// Subjects
+//
+$doc->addSubjectSwd()
+   ->setValue(randString($counter++));
+
+foreach (array('addSubjectUncontrolled', 'addSubjectMSC', 'addSubjectDDC') AS $addMethod) {
+   $doc->$addMethod()
+   ->setLanguage(randString($counter++))
+   ->setValue(randString($counter++))
+   ->setExternalKey(randString($counter++));
+   $doc->$addMethod()
+   ->setLanguage("eng\0".randString($counter++))
+   ->setValue(randString($counter++))
+   ->setExternalKey(randString($counter++));
+   $doc->$addMethod()
+   ->setLanguage("deu")
+   ->setValue(randString($counter++))
+   ->setExternalKey(randString($counter++));
+   $doc->$addMethod()
+   ->setLanguage("eng")
+   ->setValue(randString($counter++))
+   ->setExternalKey(randString($counter++));
+}
+
+
+
+//
+// Notes
+//
+$doc->addNote()
+   ->setVisibility('public')
+   ->setMessage(randString($counter++));
+$doc->addNote()
+   ->setVisibility('private')
+   ->setMessage(randString($counter++));
+
+
+
+//
+// Licenses
+//
+$lic = new Opus_Licence();
+$lic->setActive(1);
+$lic->setLanguage('deu'.randString($counter++));
+$lic->setLinkLicence(randString($counter++));
+$lic->setNameLong(randString($counter++));
+$lic->store();
 $doc->setLicence($lic);
 
-$doc->setServerDateUnlocking("23.05.1949");
 
-$ddc = $doc->addSubjectDDC();
-$ddc->setValue('Allgemeines, Wissenschaft' . randString($counter++))->setLanguage('deu'.randString($counter++))->setExternalKey(randString($counter++));
 
+//
+// Storing...
+//
 $doc->store();
 print("Document stored. ID: " . $doc->getId() . "\n");
-
