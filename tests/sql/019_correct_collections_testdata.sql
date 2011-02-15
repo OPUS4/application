1b54e2af54a5a808e37164e3682b1e5c681e3ddf
Sascha Szott <szott@gmx.de>
2011-02-15 18:10:09 +0000

    bugfix in series collection data (invalid right_id) Issue #OPUSVIER-1070 - Falsches Sprungziel nach abspeichern einer Collection
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@7559 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/019_correct_collections_testdata.sql
+++ tests/sql/019_correct_collections_testdata.sql
@@ -1,2 +1,4 @@
 UPDATE collections SET right_id = 196 WHERE id = 1;
 UPDATE collections SET right_id = 10 WHERE id = 15982 and role_id = 9;
+UPDATE collections SET right_id = 128 WHERE id = 15983;
+
