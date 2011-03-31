b195ee66185c807678567e964d35e7552e0ebc83
Sascha Szott <szott@gmx.de>
2011-03-31 14:53:50 +0000

    BUGFIX: missing collection name in flash message when removing collection from a document Issue #OPUSVIER-1177 - Fehler in Bestätigungsnachricht nach Entfernen der Collectionzuordnung zu einem Dokument
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@7729 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/admin/controllers/DocumentsController.php
+++ modules/admin/controllers/DocumentsController.php
@@ -501,31 +501,32 @@ class Admin_DocumentsController extends Controller_CRUDAction {
      * @return void
      */
     public function unlinkcollectionAction() {
-        if (true === $this->_request->isPost()) {
-            $document_id = $this->getRequest()->getParam('id');            
-            $collection_id = $this->getRequest()->getParam('collection');            
-            $document = new Opus_Document($document_id);
-            $collections = array();
-            $deletedCollectionName = null;
-            foreach ($document->getCollection() as $collection) {
-                if ($collection->getId() !== $collection_id) {
-                    array_push($collections, $collection);
+        if (!$this->_request->isPost()) {
+            return $this->_redirectTo('index');
+        }
+        $document = new Opus_Document($this->getRequest()->getParam('id'));
+        $collection_id = $this->getRequest()->getParam('collection');
+        $collections = array();
+        $deletedCollectionName = null;
+        foreach ($document->getCollection() as $collection) {
+            if ($collection->getId() !== $collection_id) {
+                array_push($collections, $collection);
+            }
+            else {
+                if ($collection->isRoot()) {
+                    $deletedCollectionName = $collection->getRole()->getDisplayName();
                 }
                 else {
                     $deletedCollectionName = $collection->getDisplayName();
                 }
             }
-            $document->setCollection($collections);
-            $document->store();
-            $params = $this->getRequest()->getUserParams();
-            $module = array_shift($params);
-            $controller = array_shift($params);
-            $action = array_shift($params);
-            $this->_redirectTo('edit', 'collection \'' . $deletedCollectionName . '\' was removed successfully', $controller, $module, $params);
         }
-        else {
-            $this->_redirectTo('index');
-        }
-    }
-    
+        $document->setCollection($collections);
+        $document->store();
+        $params = $this->getRequest()->getUserParams();
+        $module = array_shift($params);
+        $controller = array_shift($params);
+        $action = array_shift($params);
+        $this->_redirectTo('edit', "collection '$deletedCollectionName' was removed successfully", $controller, $module, $params);
+    }    
 }
