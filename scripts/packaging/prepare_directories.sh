73fb8741e70c2faf1e84ac865ad5536b43f680c0
Sascha Szott <szott@gmx.de>
2011-04-20 12:26:41 +0000

    removed deletion of scripts/zib in tarball creation script
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@8094 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/packaging/prepare_directories.sh
+++ scripts/packaging/prepare_directories.sh
@@ -76,7 +76,7 @@ rm -v  opus4/application/configs/doctypes/preprintmatheon.xml
 rm -vf opus4/modules/publish/views/scripts/form/demo.phtml
 rm -vf  opus4/application/configs/doctypes/demo.xml
 rm -rv opus4/modules/{pkm,publicationList,remotecontrol,import,matheon}
-rm -rv opus4/scripts/{packaging,cron,indexing,install,zib}
+rm -rv opus4/scripts/{packaging,cron,indexing,install}
 rm -rvf opus4/library/Opus/Search/{Adapter,Index}/Lucene/
 rm -rvf testdata/sql/992_create_documents_testdata__security.sql
 rm -rvf testdata/sql/990_create_documents_testdata__hhhar.sql
