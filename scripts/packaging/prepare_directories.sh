6213e5e1dc7bb454e837375d2378448fc32c5a44
Sascha Szott <szott@gmx.de>
2011-04-20 08:16:18 +0000

    removed module matheon in tarball
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@8066 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/packaging/prepare_directories.sh
+++ scripts/packaging/prepare_directories.sh
@@ -75,7 +75,7 @@ rm -v  opus4/modules/publish/views/scripts/form/preprintmatheon.phtml
 rm -v  opus4/application/configs/doctypes/preprintmatheon.xml
 rm -vf opus4/modules/publish/views/scripts/form/demo.phtml
 rm -vf  opus4/application/configs/doctypes/demo.xml
-rm -rv opus4/modules/{pkm,publicationList,remotecontrol,import}
+rm -rv opus4/modules/{pkm,publicationList,remotecontrol,import,matheon}
 rm -rv opus4/scripts/{packaging,cron,indexing,install}
 rm -r  opus4/scripts/*Matheon*.php
 rm -rv  opus4/scripts/zib
