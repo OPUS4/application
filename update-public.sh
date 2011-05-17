3b3bb99e1315a8b1d0f929a5bc1cf1bcb6201290
Jens Schwidder <schwidder@zib.de>
2011-05-17 09:21:02 +0000

    Ein paar Anpassungenm, um die Funktionen für die Synchonisation von Ordnern zu verwenden. Etwas aufgeräumt und Eingaben etwas flexibler verarbeitet. Die Aktualisierung des Layouts hat noch viele offene Fragen denke ich. Issue #OPUSVIER-1425 - Update des OPUS Layouts überdenken
    git-svn-id: https://svn.zib.de/opus4dev/install/trunk@8293 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- update-public.sh
+++ update-public.sh
@@ -19,6 +19,9 @@
 
 # Updates the OPUS4 *public* folder
 
+# TODO the process for updating public seems confusing, especially for users that are happy with standard layout.
+# TODO What if my_layout already exists?
+
 set -o errexit
 
 BASEDIR=$1
@@ -32,47 +35,57 @@ NEW_PUBLIC=../$NEW_PUBLIC1
 ################################################################
 #Part 5: update public directory (depends)
 ################################################################
-echo "The directory $OLD_PUBLIC is updating now."
-echo "**************************************************************"
+echo "Updating directory $OLD_PUBLIC ..."
 
 LAYOUT_OPUS4=layouts/opus4
 LAYOUT_CUSTOM=layouts/my_layout
 
+# TODO IMPORTANT grep using regular expression to handle whitespace
+# TODO IMPORTANT handle commented out lines (ignore them)
 THEME1=$(grep 'theme = ' $OLD_CONFIG/config.ini)
 THEME_OPUS="; theme = opus4"
+
 echo $THEME1
 echo $THEME_OPUS
 
-if [ -z "$THEME1" ] || [ "$THEME1" == "$THEME_OPUS" ]
-then
-	echo "You are currently using the standard OPUS4 layout. This creates conflicts during update process."
-	read -p "Do you want to keep the current layout as 'my_layout' and update the standard opus4 layout [1] or skip important layout changes [2] ? : " LAYOUT_ANSWER
-	if [ $LAYOUT_ANSWER = '2' ]
-	then
-		echo "Please check layout changes/bugfixes in /opus4/public/layouts/opus4/" >> $CONFLICT 
-	else 
-		if [ -d "$OLD_PUBLIC/$LAYOUT_CUSTOM" ]
-		then			
-			read -p "The layout 'my_layout' already exists. Please enter a different layout name: " LAYOUT_NAME
-			LAYOUT_CUSTOM=layouts/$LAYOUT_NAME			
-		fi
-		
-		echo "Please update layout bugfixes manually in /opus4/public/$LAYOUT_CUSTOM" >> $CONFLICT
-		mkdir $OLD_PUBLIC/$LAYOUT_CUSTOM
-		cp $OLD_PUBLIC/$LAYOUT_OPUS4/* -R $OLD_PUBLIC/$LAYOUT_CUSTOM
-		cp $NEW_PUBLIC/$LAYOUT_OPUS4/* -R $OLD_PUBLIC/$LAYOUT_OPUS4
-		
-		if [ ! -z $LAYOUT_NAME ]
-		then
-			sed -i "s/; theme = opus4/theme = $LAYOUT_NAME/" $OLD_CONFIG/config.ini
-		else 
-			sed -i "s/; theme = opus4/theme = my_layout/" $OLD_CONFIG/config.ini
-		fi
-		echo "Your config.ini has changed => theme = $LAYOUT_NAME"
-	fi
+if [ -z "$THEME1" ] || [ "$THEME1" == "$THEME_OPUS" ]; then
+    echo -e "You are currently using the standard OPUS4 layout. This creates "
+    echo "conflicts during update process."
+    echo -e "Would you like to copy the current layout to $LAYOUT_CUSTOM and "
+    echo -e "update the standard opus4 layout [U] or skip important layout "
+    echo -e "changes [s] [U|s]? : \c " 
+    read LAYOUT_ANSWER
+    if [ -z $LAYOUT_ANSWER ]; then 
+        LAYOUT_ANSWER='u' # default is update layout
+    else
+        LAYOUT_ANSWER=${LAYOUT_ANSWER,,} # convert to lowercase
+        LAYOUT_ANSWER=${LAYOUT_ANSWER:0:1} # get first letter
+    fi
+    if [ $LAYOUT_ANSWER = 's' ]; then
+        echo "Please check layout changes/bugfixes in /opus4/public/layouts/opus4/" >> $CONFLICT 
+    else 
+        # TODO So every update leads to creation of new folder?
+        if [ -d "$OLD_PUBLIC/$LAYOUT_CUSTOM" ]; then			
+            read -p "The layout 'my_layout' already exists. Please enter a different layout name: " LAYOUT_NAME
+            LAYOUT_CUSTOM=layouts/$LAYOUT_NAME			
+        fi
+
+        echo "Please update layout bugfixes manually in /opus4/public/$LAYOUT_CUSTOM" >> $CONFLICT
+        createFolder $OLD_PUBLIC/$LAYOUT_CUSTOM
+        # TODO use functions that log operations for this?
+        cp $OLD_PUBLIC/$LAYOUT_OPUS4/* -R $OLD_PUBLIC/$LAYOUT_CUSTOM
+        
+        updateFolder $NEW_PUBLIC/$LAYOUT_OPUS4 $OLD_PUBLIC/$LAYOUT_OPUS4
+
+        if [ ! -z $LAYOUT_NAME ]; then
+            sed -i "s/; theme = opus4/theme = $LAYOUT_NAME/" $OLD_CONFIG/config.ini
+        else 
+            sed -i "s/; theme = opus4/theme = my_layout/" $OLD_CONFIG/config.ini
+        fi
+        echo "Your config.ini has changed => theme = $LAYOUT_NAME"
+    fi
 else
-	cp $NEW_PUBLIC/$LAYOUT_OPUS4/* -R $OLD_PUBLIC/$LAYOUT_OPUS4
+    updateFolder $NEW_PUBLIC/$LAYOUT_OPUS4 $OLD_PUBLIC/$LAYOUT_OPUS4
 fi		
 
-cp $NEW_PUBLIC/htaccess-template $OLD_PUBLIC/htaccess-template
-echo ""
+updateFile $NEW_PUBLIC/htaccess-template $OLD_PUBLIC/htaccess-template
