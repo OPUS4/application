273790ab03d71cc4bc80a0c7c4a8c41518b05888
Jens Schwidder <schwidder@zib.de>
2011-05-24 13:15:01 +0000

    Disabled question for restarting Apache.
    git-svn-id: https://svn.zib.de/opus4dev/install/trunk@8356 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- update.sh
+++ update.sh
@@ -276,9 +276,9 @@ backup
 # TODO Verify successful update somehow?
 
 # Restart apache
-if [[ askYesNo "Would you like to restart Apache2 now [Y/n]?" ]]; then
+# if [[ askYesNo "Would you like to restart Apache2 now [Y/n]?" ]]; then
     echo "Restarting Apache server ..."
     DRYRUN || /etc/init.d/apache2 restart
-fi
+#fi
 
 # Restart Solr
