91b9c58553d15335cfd8fbd104d259d3eae58c03
Sascha Szott <szott@gmx.de>
2011-05-31 14:03:40 +0000

    adjusted chmod command on workspace folder
    git-svn-id: https://svn.zib.de/opus4dev/install/trunk@8473 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- install.sh
+++ install.sh
@@ -332,8 +332,8 @@ then
 else
   chown -R "$OPUS_USER_NAME" "$BASEDIR"
 fi
-cd "$BASEDIR/workspace"
-chmod -R 777 *
+find "$BASEDIR/workspace/" -type f -print0 |xargs -r0 chmod 777
+find "$BASEDIR/workspace/" -type d -print0 |xargs -r0 chmod 666
 
 # delete tar archives
 cd "$BASEDIR"
