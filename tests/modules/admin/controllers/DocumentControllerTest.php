b37796280b7da46f3b8c2520a6894df0485cb37a
Jens Schwidder <schwidder@zib.de>
2011-07-29 12:59:28 +0000

    Unit Tests für die verschobenen Actions modifiziert und in korrekte Klasse verschoben. Unit Tests wieder aktiviert. Issue #OPUSVIER-1602 - Aufräumen von DocumentsController und DocumentController
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@8719 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/modules/admin/controllers/DocumentControllerTest.php
@@ -0,0 +1,169 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Unit Tests
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2010, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Unit tests for Admin_DocumentController.
+ */
+class Admin_DocumentControllerTest extends ControllerTestCase {
+
+    /**
+     * Tests deleting a document.
+     */
+    public function testDeleteAction() {
+        $this->dispatch('/admin/document/delete/docId/24');
+        $this->assertResponseCode(200);
+        $this->assertModule('admin');
+        $this->assertController('document');
+        $this->assertAction('delete');
+    }
+    
+    /**
+     * Tests user selecting no in delete confirmation form.
+     */
+    public function testDeleteActionConfirmNo() {
+        $this->request
+                ->setMethod('POST')
+                ->setPost(array(
+                    'id' => '24',
+                    'sureno' => 'sureno'
+                ));
+        $this->dispatch('/admin/document/delete');
+        $this->assertModule('admin');
+        $this->assertController('document');
+        $this->assertAction('delete');
+        $this->assertRedirect('/admin/document/index');
+
+        $doc = new Opus_Document(24);
+        $this->assertNotEquals('deleted', $doc->getServerState());
+    }
+
+    /**
+     * Tests user selecting yes in delete confirmation form.
+     */
+    public function testDeleteActionConfirmYes() {
+        $this->request
+                ->setMethod('POST')
+                ->setPost(array(
+                    'id' => '25',
+                    'sureyes' => 'sureyes'
+                ));
+        $this->dispatch('/admin/document/delete');
+        $this->assertModule('admin');
+        $this->assertController('document');
+        $this->assertAction('delete');
+        $this->assertRedirect('/admin/document/index');
+
+        $doc = new Opus_Document(25);
+        $this->assertEquals('deleted', $doc->getServerState());
+    }
+
+    /**
+     * Tests permanently deleting a document.
+     * 
+     * @depends testDeleteActionConfirmYes
+     */
+    public function testPermanentDeleteAction() {
+        $this->dispatch('/admin/document/permanentdelete/docId/25');
+        $this->assertResponseCode(200);
+        $this->assertModule('admin');
+        $this->assertController('document');
+        $this->assertAction('permanentdelete');
+    }
+
+    /**
+     * Tests user answering no in permanent delete confirmation form.
+     * 
+     * @depends testPermanentDeleteAction
+     */
+    public function testPermanentDeleteActionConfirmNo() {
+        $this->request
+                ->setMethod('POST')
+                ->setPost(array(
+                    'id' => '24',
+                    'sureno' => 'sureno'
+                ));
+        $this->dispatch('/admin/document/permanentdelete');
+        $this->assertModule('admin');
+        $this->assertController('document');
+        $this->assertAction('permanentdelete');
+        $this->assertRedirect('/admin/document/index');
+
+        $doc = new Opus_Document(25);
+        $this->assertEquals('deleted', $doc->getServerState());
+    }
+
+    /**
+     * Tests user answering yes in publish confirmation form.
+     */
+    public function testPublishActionConfirmYes() {
+        $this->request
+                ->setMethod('POST')
+                ->setPost(array(
+                    'id' => '100',
+                    'sureyes' => 'sureyes'
+                ));
+        $this->dispatch('/admin/document/publish');
+        $this->assertModule('admin');
+        $this->assertController('document');
+        $this->assertAction('publish');
+        $this->assertRedirect('/admin/document/index');
+
+        $doc = new Opus_Document(100);
+        $this->assertEquals('published', $doc->getServerState());
+    }
+
+    /**
+     * Tests unpublishing document.
+     * 
+     * @depends testPublishActionConfirmYes
+     */
+    public function testUnpublishAction() {
+        $this->request
+                ->setMethod('POST')
+                ->setPost(array(
+                    'id' => '100',
+                    'sureyes' => 'sureyes'
+                ));
+        $this->dispatch('/admin/document/unpublish');
+        $this->assertModule('admin');
+        $this->assertController('document');
+        $this->assertAction('unpublish');
+        $this->assertRedirect('/admin/document/index');
+
+        $doc = new Opus_Document(100);
+        $this->assertEquals('unpublished', $doc->getServerState());
+    }
+
+}
+
+?>
