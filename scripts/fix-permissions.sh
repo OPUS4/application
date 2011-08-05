997ea8e7d9ca98d6787257da8c2b1a247c859e3a
Thoralf Klein <thoralf@fischlustig.de>
2011-08-05 12:54:23 +0000

    IMPROVEMENT: Added error checking and parameters to fix-permission script.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@8860 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/fix-permissions.sh
+++ scripts/fix-permissions.sh
@@ -2,35 +2,49 @@
 
 set -e
 
-FILEOWNER="www-data:www-data"
-DIR="$(pwd)"
-SERVER_DIR="$(dirname $DIR)"
+FILEOWNER="$1"
+SERVER_DIR="$2"
 
-cd $SERVER_DIR/workspace
+
+#
+# Parameter checking.
+#
+
+if [[ -z $FILEOWNER ]] || [[ -z $SERVER_DIR ]]; then
+   echo "usage: $0 <fileowner> <opus4 base directory>"
+   exit 1;
+fi
+
+if [[ ! -d $SERVER_DIR ]] || [[ ! -d $SERVER_DIR/workspace/ ]]; then
+   echo "Invalid/empty base directory '$SERVER_DIR' given."
+   echo "base directory must non-empty and contain subfolder 'workspace'"
+   exit 1
+fi
+
 
 #
 # Fix permissions for server/workspace/cache/*
 #
 
-if [ "$(ls cache)" ]; then
-    chown $FILEOWNER cache/*
-fi
+find "$SERVER_DIR/workspace/cache" -print0 |xargs -r0 chown "$FILEOWNER"
+find "$SERVER_DIR/workspace/cache" -type d -print0 |xargs -r0 chmod 755
+find "$SERVER_DIR/workspace/cache" -type f -print0 |xargs -r0 chmod 644
+
 
 #
 # Fix permissions recursively for server/workspace/files/*
 #
 
-if [ "$(ls files)" ]; then
-    chown -R $FILEOWNER files/[0-9]*
-fi
+find "$SERVER_DIR/workspace/files" -print0 |xargs -r0 chown "$FILEOWNER"
+find "$SERVER_DIR/workspace/files" -type d -print0 |xargs -r0 chmod 755
+find "$SERVER_DIR/workspace/files" -type f -print0 |xargs -r0 chmod 644
+
 
 #
 # Fix permissions for server/workspace/log/opus.log
 #
 
-if [ -f log/opus.log ]; then
-    chown $FILEOWNER log/opus.log
+chmod 777 "$SERVER_DIR/workspace/log/"
+if [ -f "$SERVER_DIR/workspace/log/opus.log" ]; then
+    chown "$FILEOWNER" "$SERVER_DIR/workspace/log/opus.log"
 fi
-
-cd $DIR
-
