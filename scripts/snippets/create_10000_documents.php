6d9b233e15941d76d36319ff90567c8676626c32
Sascha Szott <szott@gmx.de>
2011-10-05 15:37:43 +0000

    added category annotation
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9041 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/snippets/create_10000_documents.php
+++ scripts/snippets/create_10000_documents.php
@@ -24,6 +24,7 @@
  * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
+ * @category    Application
  * @author      Thoralf Klein <thoralf.klein@zib.de>
  * @copyright   Copyright (c) 2008-2011, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
