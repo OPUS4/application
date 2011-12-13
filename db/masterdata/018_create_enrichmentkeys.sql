c113842875cec8caffb8049b41ce1ff47b276121
Gunar Maiwald <maiwald@zib.de>
2011-12-13 09:08:29 +0000

    Bezeichner der Dokumenttypen in ris.xslt wurde korrigiert Issue #OPUSVIER-1851 - Schemaänderung Enrichments: globale Registry für die Enrichments anlegen (Modellierung einer n-zu-m statt einer 1-zu-n Relation zwischen Dokumenten und Enrichments)
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9370 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ db/masterdata/018_create_enrichmentkeys.sql
@@ -0,0 +1,10 @@
+--
+-- Daten für Tabelle `enrichments`
+--
+
+INSERT INTO `enrichmentkeys` (`name`) VALUES
+('submitter.user_id'),
+('reviewer.user_id'),
+('review.rejected_by'),
+('review.accepted_by'),
+('LegalNotices');
