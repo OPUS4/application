8ce56ee7b5d52a5b04581eb727da9a2e862a6478
Thoralf Klein <thoralf@fischlustig.de>
2012-01-03 12:24:28 +0000

    server unit tests: Remove non-existent column "display_oai".  (See OPUSVIER-2155.)
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9577 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/015_create_collections.sql
+++ tests/sql/015_create_collections.sql
@@ -1,8 +1,8 @@
-INSERT INTO `collections_roles` (`id`, `name`, `oai_name`, `position`, `visible`, `visible_browsing_start`, `display_browsing`, `visible_frontdoor`, `display_frontdoor`, `visible_oai`, `display_oai`) VALUES
-(9, 'collections', 'collections', 9, 1, 1, 'Name', 1, 'Name', 1, 'Name'),
-(11, 'reports', 'reports', 11, 1, 1, 'Number, Name', 1, 'Number, Name', 1, 'Number'),
-(15, 'projects', 'projects', 12, 1, 1, 'Number, Name', 1, 'Number, Name', 1, 'Number'),
-(17, 'no-root-test', 'no-root-test', 13, 0, 1, 'Name', 1, 'Name', 1, 'Name');
+INSERT INTO `collections_roles` (`id`, `name`, `oai_name`, `position`, `visible`, `visible_browsing_start`, `display_browsing`, `visible_frontdoor`, `display_frontdoor`, `visible_oai`) VALUES
+(9, 'collections', 'collections', 9, 1, 1, 'Name', 1, 'Name', 1),
+(11, 'reports', 'reports', 11, 1, 1, 'Number, Name', 1, 'Number, Name', 1),
+(15, 'projects', 'projects', 12, 1, 1, 'Number, Name', 1, 'Number, Name', 1),
+(17, 'no-root-test', 'no-root-test', 13, 0, 1, 'Name', 1, 'Name', 1);
 
 INSERT INTO `collections` (`id`, `role_id`, `number`, `name`, `oai_subset`, `left_id`, `right_id`, `parent_id`, `visible`) VALUES
 (1, 1, NULL, NULL, NULL, 1, 2, NULL, 1),
