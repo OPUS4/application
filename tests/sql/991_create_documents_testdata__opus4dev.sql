f6378ff87456d2e8e4fcf68b83cc7ce76732da7a
Sascha Szott <szott@gmx.de>
2012-01-05 16:30:08 +0000

    added two more test files to test document #91 (to test both attributes visible_oai and visible_frontdoor)
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9642 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/991_create_documents_testdata__opus4dev.sql
+++ tests/sql/991_create_documents_testdata__opus4dev.sql
@@ -49,7 +49,9 @@ INSERT INTO `document_files` (`id`, `document_id`, `path_name`, `label`, `mime_t
 (118, 93, 'test.txt', 'test.txt', 'text/plain', 'eng', 591, 1, 1),
 (119, 94, 'test.ps', 'test.ps', 'application/postscript', 'eng', 22041, 1, 1),
 (120, 95, 'test.html', 'test.html', 'text/html', 'eng', 847, 1, 1),
-(127, 91, 'test.txt', 'test.txt', 'text/plain', 'eng', 591, 1, 1);
+(127, 91, 'test.txt', 'test.txt', 'text/plain', 'eng', 591, 1, 1),
+(128, 91, 'frontdoor_invisible.txt', 'frontdoor_invisible.txt', 'text/plain', 'eng', 591, 0, 1),
+(129, 91, 'oai_invisible.txt', 'oai_invisible.txt', 'text/plain', 'eng', 591, 1, 0);
 
 INSERT INTO `file_hashvalues` (`file_id`, `type`, `value`) VALUES
 (116, 'MD5', '1ba50dc8abc619cea3ba39f77c75c0fe'),
