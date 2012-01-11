86c073abc9556b7c7bda366271b98c9ed5b76e50
Sascha Szott <szott@gmx.de>
2012-01-11 12:45:53 +0000

    removed database column *logo* in table document_series and framework dependencies Issue #OPUSVIER-2225 - Attribut logo für Schriftenreihen entfernen
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9739 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/022_create_document_series.sql
+++ tests/sql/022_create_document_series.sql
@@ -1,3 +1,3 @@
-INSERT INTO `document_series` (`id`, `title`, `logo`, `publisher`, `issn`, `infobox`) VALUES
-(1, 'MySeries', 'no logo available', 'no publisher available', NULL, NULL),
-(2, 'Foobar Series', NULL, 'Foobar Publishing Corp.', NULL, NULL);
+INSERT INTO `document_series` (`id`, `title`, `publisher`, `issn`, `infobox`) VALUES
+(1, 'MySeries', 'no publisher available', NULL, NULL),
+(2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL);
