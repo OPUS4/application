f67db3fbfe471499dc794e37d76a614c5b4df112
Sascha Szott <szott@gmx.de>
2012-01-12 13:23:50 +0000

    added values for recently added column *sort_order* to test data Issue #OPUSVIER-2142 - Sortierung der Schriftenreihen ermöglichen: Attribute sort_order für Schriftenreihen anbieten
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9764 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/022_create_document_series.sql
+++ tests/sql/022_create_document_series.sql
@@ -1,4 +1,5 @@
-INSERT INTO `document_series` (`id`, `title`, `publisher`, `issn`, `infobox`, `visible`) VALUES
-(1, 'MySeries', 'no publisher available', NULL, NULL, '1'),
-(2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL, '1'),
-(3, 'Invisble Series', NULL, NULL, NULL, '0');
+INSERT INTO `document_series` (`id`, `title`, `publisher`, `issn`, `infobox`, `visible`, `sort_order`) VALUES
+(1, 'MySeries', 'no publisher available', NULL, NULL, '1', 0),
+(2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL, '1', 1),
+(3, 'Invisible Series', NULL, NULL, NULL, '0', 3),
+(4, 'Visible Series', NULL, NULL, NULL, '0', 2);
