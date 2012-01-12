69b4370918280198c88b8c8ccdfef7c7e7cf7b00
Sascha Szott <szott@gmx.de>
2012-01-12 17:17:39 +0000

    changed copyright
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9779 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/publish/forms/PublishingAbstract.php
+++ modules/publish/forms/PublishingAbstract.php
@@ -28,7 +28,7 @@
  * @category    Application
  * @package     Module_Publish
  * @author      Susanne Gottwald <gottwald@zib.de>
- * @copyright   Copyright (c) 2008-2011, OPUS 4 development team
+ * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
  */
