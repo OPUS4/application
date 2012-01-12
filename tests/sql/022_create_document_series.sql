ec87918e086496058302441551ba7a70a78426f9
Sascha Szott <szott@gmx.de>
2012-01-12 11:49:35 +0000

    modified test data after introducing new attribut visible on Opus_Series Issue #OPUSVIER-2138 - Attribute isVisible für Schriftenreihen anbieten
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9755 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/022_create_document_series.sql
+++ tests/sql/022_create_document_series.sql
@@ -1,3 +1,4 @@
-INSERT INTO `document_series` (`id`, `title`, `publisher`, `issn`, `infobox`) VALUES
-(1, 'MySeries', 'no publisher available', NULL, NULL),
-(2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL);
+INSERT INTO `document_series` (`id`, `title`, `publisher`, `issn`, `infobox`, `visible`) VALUES
+(1, 'MySeries', 'no publisher available', NULL, NULL, '1'),
+(2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL, '1'),
+(3, 'Invisble Series', NULL, NULL, NULL, '0');
