0e9327a9cb71b44740fc548a1c7efd79be144bc1
Susanne Gottwald <gottwald@zib.de>
2012-01-13 15:22:38 +0000

    Testing SVN auto-props inside netbeans.  Please ignore.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9794 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ library/AutoPropsTestClass.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AutoPropsTestClass
+ *
+ * @author susi
+ */
+class AutoPropsTestClass {
+    //put your code here
+}
+
+?>
