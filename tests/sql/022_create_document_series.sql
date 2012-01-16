d000296c75ac8dee50586f18cdfbc0234b23573e
Sascha Szott <szott@gmx.de>
2012-01-16 09:30:08 +0000

    set visibility status of series *Visible Series* to true
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9802 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/022_create_document_series.sql
+++ tests/sql/022_create_document_series.sql
@@ -2,4 +2,4 @@ INSERT INTO `document_series` (`id`, `title`, `publisher`, `issn`, `infobox`, `v
 (1, 'MySeries', 'no publisher available', NULL, NULL, '1', 0),
 (2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL, '1', 1),
 (3, 'Invisible Series', NULL, NULL, NULL, '0', 3),
-(4, 'Visible Series', NULL, NULL, NULL, '0', 2);
+(4, 'Visible Series', NULL, NULL, NULL, '1', 2);
