49543ca3bced9670f87ac28abc26031f37be02fc
Sascha Szott <szott@gmx.de>
2012-01-16 09:33:50 +0000

    changed sort order of default series
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9803 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/022_create_document_series.sql
+++ tests/sql/022_create_document_series.sql
@@ -1,5 +1,5 @@
 INSERT INTO `document_series` (`id`, `title`, `publisher`, `issn`, `infobox`, `visible`, `sort_order`) VALUES
 (1, 'MySeries', 'no publisher available', NULL, NULL, '1', 0),
-(2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL, '1', 1),
+(2, 'Foobar Series', 'Foobar Publishing Corp.', NULL, NULL, '1', 2),
 (3, 'Invisible Series', NULL, NULL, NULL, '0', 3),
-(4, 'Visible Series', NULL, NULL, NULL, '1', 2);
+(4, 'Visible Series', NULL, NULL, NULL, '1', 1);
