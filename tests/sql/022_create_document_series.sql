17f9e0492c27fff3dfc2a6039ca83324e40e14f9
Sascha Szott <szott@gmx.de>
2012-01-16 18:47:33 +0000

    added HTML markup in infoboxes for test series #1 and #2 Issue #OPUSVIER-1977 - Browsing Änderungen bzgl. Schriftenreihen
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9842 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/022_create_document_series.sql
+++ tests/sql/022_create_document_series.sql
@@ -1,5 +1,5 @@
 INSERT INTO `document_series` (`id`, `title`, `infobox`, `visible`, `sort_order`) VALUES
-(1, 'MySeries', NULL, '1', 0),
-(2, 'Foobar Series', NULL, '1', 2),
+(1, 'MySeries', 'Dies ist die Schriftenreihe <b>MySeries</b>.', '1', 0),
+(2, 'Foobar Series', '<p>Welcome to Foobar Series!</p><p><b>HTML markup</b> can be used <i>here</i>.</p>', '1', 2),
 (3, 'Invisible Series', NULL, '0', 3),
 (4, 'Visible Series', NULL, '1', 1);
