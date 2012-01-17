47067d00d9b7f1a85af000575e4e36ef5438aae8
Sascha Szott <szott@gmx.de>
2012-01-17 12:11:25 +0000

    added external link in infobox of test series #2 Issue #OPUSVIER-1977 - Browsing Änderungen bzgl. Schriftenreihen
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@9857 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/022_create_document_series.sql
+++ tests/sql/022_create_document_series.sql
@@ -1,6 +1,6 @@
 INSERT INTO `document_series` (`id`, `title`, `infobox`, `visible`, `sort_order`) VALUES
 (1, 'MySeries', 'Dies ist die Schriftenreihe <b>MySeries</b>.<br/><p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>', '1', 0),
-(2, 'Foobar Series', '<p>Welcome to Foobar Series!</p><p><b>HTML markup</b> can be used <i>here</i>.</p>', '1', 2),
+(2, 'Foobar Series', '<p>Welcome to Foobar Series!</p><p><b>HTML markup</b> can be used <i>here</i>.</p><a href="http://example.org">This is a link.</a>', '1', 2),
 (3, 'Invisible Series', NULL, '0', 3),
 (4, 'Visible Series', NULL, '1', 1),
 (5, 'OPUS Series', '<p>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>', '1', 6);
