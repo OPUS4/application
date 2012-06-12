d09694eaedb2d447a89317e2ee567e68497ae5fa
Sascha Szott <szott@gmx.de>
2012-06-12 13:07:17 +0000

    added very long abstract for test document #92 Issue #OPUSVIER-645 - Frontdoor: Abstracts einklappbar machen
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10346 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/991_create_documents_testdata__opus4dev.sql
+++ tests/sql/991_create_documents_testdata__opus4dev.sql
@@ -138,7 +138,8 @@ INSERT INTO `document_title_abstracts` (`id`, `document_id`, `type`, `value`, `l
 (246, 108, 'main', 'Wally - The vegetarian walruss.', 'eng'),
 (247, 109, 'main', 'What to do if your build is broken!', 'eng'),
 (248, 110, 'main', 'Internet access broken? - What you could do instead.', 'eng'),
-(251, 91, 'parent', 'This is a parent title', 'eng');
+(251, 91, 'parent', 'This is a parent title', 'eng'),
+(271, 92, 'abstract', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 'deu');
 
 --
 -- Daten für Tabelle `persons`
