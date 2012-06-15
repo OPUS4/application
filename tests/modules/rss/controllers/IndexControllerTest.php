abfa97f066948f5c8338bf08bfdf1ae0ca563f86
Sascha Szott <szott@gmx.de>
2012-06-15 13:49:20 +0000

    extended regression test for OPUSVIER-1726 Issue #OPUSVIER-1726 - RSS stirbt, wenn dex Solr-Index Dokumente enthält, die nicht mehr in der Datenbank sind
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10440 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/modules/rss/controllers/IndexControllerTest.php
+++ tests/modules/rss/controllers/IndexControllerTest.php
@@ -77,30 +77,30 @@ class Rss_IndexControllerTest extends ControllerTestCase {
      */
     public function testSolrIndexIsNotUpToDate() {
         // add a document to the search index that is not stored in database
-        $doc = new Opus_Document();
-        $doc->setServerState('published');
-        $doc->setLanguage('eng');
+        $doc1 = new Opus_Document();
+        $doc1->setServerState('published');
+        $doc1->setLanguage('eng');
         $title = new Opus_Title();
         $title->setValue('test document for OPUSVIER-1726');
         $title->setLanguage('eng');
-        $doc->setTitleMain($title);
+        $doc1->setTitleMain($title);
         // unregister index plugin: database changes are not reflected in search index
-        $doc->unregisterPlugin('Opus_Document_Plugin_Index');
-        $doc->store();
+        $doc1->unregisterPlugin('Opus_Document_Plugin_Index');
+        $doc1->store();
 
-        $docId = $doc->getId();
-        $date = new Zend_Date($doc->getServerDatePublished());
-        $dateValue = $date->get(Zend_Date::RFC_2822);
+        $docId1 = $doc1->getId();
+        $date = new Zend_Date($doc1->getServerDatePublished());
+        $dateValue1 = $date->get(Zend_Date::RFC_2822);
         
         $indexer = new Opus_SolrSearch_Index_Indexer();
 
         $class = new ReflectionClass('Opus_SolrSearch_Index_Indexer');
         $methodGetSolrXmlDocument = $class->getMethod('getSolrXmlDocument');
         $methodGetSolrXmlDocument->setAccessible(true);
-        $solrXml = $methodGetSolrXmlDocument->invoke($indexer, $doc);
+        $solrXml = $methodGetSolrXmlDocument->invoke($indexer, $doc1);
 
         // delete document from database
-        $doc->deletePermanent();
+        $doc1->deletePermanent();
 
         // add document to search index
         $methodSendSolrXmlToServer = $class->getMethod('sendSolrXmlToServer');
@@ -108,18 +108,37 @@ class Rss_IndexControllerTest extends ControllerTestCase {
         $methodSendSolrXmlToServer->invoke($indexer, $solrXml);
         $indexer->commit();
 
+        sleep(2); // make sure $doc2 do not get the same value for server_date_published
+        
+        $doc2 = new Opus_Document();
+        $doc2->setServerState('published');
+        $doc2->setLanguage('eng');
+        $title = new Opus_Title();
+        $title->setValue('another test document for OPUSVIER-1726');
+        $title->setLanguage('eng');
+        $doc2->setTitleMain($title);        
+        $doc2->store();
+
+        $docId2 = $doc2->getId();
+        $date = new Zend_Date($doc2->getServerDatePublished());
+        $dateValue2 = $date->get(Zend_Date::RFC_2822);
+
         $this->dispatch('/rss/index/index/searchtype/all');
 
         // make search index up to date
-        $indexer->removeDocumentFromEntryIndexById($docId);
+        $indexer->removeDocumentFromEntryIndexById($docId1);
         $indexer->commit();
-        
+
         $body = $this->getResponse()->getBody();
-        $this->assertNotContains("No Opus_Db_Documents with id $docId in database.", $body);
-        $this->assertContains('<title>test document for OPUSVIER-1726</title>', $body);
-        $this->assertContains("frontdoor/index/index/docId/$docId</link>", $body);
-        $this->assertContains("<pubDate>$dateValue</pubDate>", $body);
-        $this->assertContains("<lastBuildDate>$dateValue</lastBuildDate>", $body);
+        $this->assertNotContains("No Opus_Db_Documents with id $docId1 in database.", $body);
+        $this->assertNotContains('<title>test document for OPUSVIER-1726</title>', $body);
+        $this->assertContains('<title>another test document for OPUSVIER-1726</title>', $body);        
+        $this->assertNotContains("frontdoor/index/index/docId/$docId1</link>", $body);
+        $this->assertContains("frontdoor/index/index/docId/$docId2</link>", $body);
+        $this->assertNotContains("<pubDate>$dateValue1</pubDate>", $body);
+        $this->assertNotContains("<lastBuildDate>$dateValue1</lastBuildDate>", $body);
+        $this->assertContains("<pubDate>$dateValue2</pubDate>", $body);
+        $this->assertContains("<lastBuildDate>$dateValue2</lastBuildDate>", $body);
         $this->assertEquals(200, $this->getResponse()->getHttpResponseCode());
     }
 
