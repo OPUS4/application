22e40e60c250275873e1e2c64f7927fd2d51f74d
Sascha Szott <szott@gmx.de>
2012-06-15 10:25:49 +0000

    added regression test for OPUSVIER-1726 Issue #OPUSVIER-1726 - RSS stirbt, wenn dex Solr-Index Dokumente enthält, die nicht mehr in der Datenbank sind
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10427 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/modules/export/controllers/IndexControllerTest.php
+++ tests/modules/export/controllers/IndexControllerTest.php
@@ -205,5 +205,55 @@ class Export_IndexControllerTest extends ControllerTestCase {
         Zend_Registry::set('Zend_Config', $config);
     }
 
+    /**
+     * Regression test for OPUSVIER-1726
+     */
+    public function testSolrIndexIsNotUpToDate() {
+        // add a document to the search index that is not stored in database
+        $doc = new Opus_Document();
+        $doc->setServerState('published');
+        $doc->setLanguage('eng');
+        $title = new Opus_Title();
+        $title->setValue('test document for OPUSVIER-1726');
+        $title->setLanguage('eng');
+        $doc->setTitleMain($title);
+        // unregister index plugin: database changes are not reflected in search index
+        $doc->unregisterPlugin('Opus_Document_Plugin_Index');
+        $doc->store();
+
+        $docId = $doc->getId();
+        $date = new Zend_Date($doc->getServerDatePublished());
+        $dateValue = $date->get(Zend_Date::RFC_2822);
+
+        $indexer = new Opus_SolrSearch_Index_Indexer();
+
+        $class = new ReflectionClass('Opus_SolrSearch_Index_Indexer');
+        $methodGetSolrXmlDocument = $class->getMethod('getSolrXmlDocument');
+        $methodGetSolrXmlDocument->setAccessible(true);
+        $solrXml = $methodGetSolrXmlDocument->invoke($indexer, $doc);
+
+        // delete document from database
+        $doc->deletePermanent();
+
+        // add document to search index
+        $methodSendSolrXmlToServer = $class->getMethod('sendSolrXmlToServer');
+        $methodSendSolrXmlToServer->setAccessible(true);
+        $methodSendSolrXmlToServer->invoke($indexer, $solrXml);
+        $indexer->commit();
+
+        $this->dispatch('/export/index/index/searchtype/simple/query/opusvier-1726/export/xml');
+
+        // make search index up to date
+        $indexer->removeDocumentFromEntryIndexById($docId);
+        $indexer->commit();
+
+        $body = $this->getResponse()->getBody();
+        echo $body;
+        $this->assertNotContains("No Opus_Db_Documents with id $docId in database.", $body);
+        $this->assertContains('Language="eng" Value="test document for OPUSVIER-1726" Type="main"', $body);
+        $this->assertContains('<Opus_Document Id="' . $docId . '" Language="eng"', $body);
+        $this->assertEquals(200, $this->getResponse()->getHttpResponseCode());        
+    }
+
 }
 
