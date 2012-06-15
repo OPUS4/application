ac4fc3a6389e8fe27781297aedc2be05041e92f2
Thoralf Klein <thoralf@fischlustig.de>
2012-06-15 13:41:08 +0000

    server/unit tests: Close logfile in tearDown() to make sure, we don't end up with 1.000 open logfiles.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10438 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/ControllerTestCase.php
+++ tests/ControllerTestCase.php
@@ -77,6 +77,10 @@ class ControllerTestCase extends Zend_Test_PHPUnit_ControllerTestCase {
     protected function tearDown() {
         $this->logoutUser();
 
+        // Close logfile to prevent plenty of open logfile.
+        $log = Zend_Registry::get('Zend_Log');
+        $log->__destruct();
+
         parent::tearDown();
     }
 
