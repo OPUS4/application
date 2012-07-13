48b19fa3a4e40ea0534b6d64026c30d97502264e
Jens Schwidder <schwidder@zib.de>
2012-07-13 11:46:03 +0000

    Unit Test hinzugefügt,, der alle TMX Dateien für die Administration mit 'xmllint' überprüft. Issue #OPUSVIER-2587 - Übersetzungsresource modules/admin/language/access.tmx ist nicht valide
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10655 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/modules/admin/language/Admin_LanguageTest.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Unit Tests
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+/**
+ * Description of Admin_LanguageTest
+ *
+ * @author Jens Schwidder <schwidder(at)zib.de>
+ */
+class Admin_LanguageTest extends ControllerTestCase {
+
+    public function testTmxFilesValid() {
+        $dir = APPLICATION_PATH . '/modules/admin/language';
+        $filesIter = new DirectoryIterator($dir);
+        foreach ($filesIter as $fileInfo) {
+            $filename = $fileInfo->getFilename();
+            if (pathinfo($fileInfo->getFilename(), PATHINFO_EXTENSION) === 'tmx') {
+                $xmllintCall = "xmllint --valid --noout {$fileInfo->getRealPath()}";
+                exec(escapeshellcmd($xmllintCall), $xmllintOutput, $xmllintFeedback);
+                $this->assertEquals('0', $xmllintFeedback, "File '$filename' not valid. (Check with 'xmllint'!)");
+            }
+        }
+    }
+
+}
+
+?>
