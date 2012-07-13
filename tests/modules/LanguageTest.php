c576f52363604c247fcf1adf85a925f882c16eb9
Jens Schwidder <schwidder@zib.de>
2012-07-13 12:09:36 +0000

    Entwurf für Test der alle TMX Dateien prüft hinzugefügt. Es gibt noch Probleme mit der Prüfung von Dateien wie "frontdoor/language/messages.tmx" bei denen Übersetzungen Tags enthalten. Issue #OPUSVIER-2588 - Prüfen ob TMX Dateien valide sind (xmllint)
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10656 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/modules/LanguageTest.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Unit Tests
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+/**
+ * Description of LanguageTest
+ *
+ * @author Jens Schwidder <schwidder(at)zib.de>
+ */
+class LanguageTest extends ControllerTestCase {
+
+    public function testAllTmxFiles() {
+        $this->markTestIncomplete("Die Datei 'frontdoor/language/messages.tmx' enthält Tags in den Strings. Das wird als Fehler gemeldet. Daher kann dieser Unit Test noch nicht eingesetzt werden.");
+        $dir = APPLICATION_PATH . '/modules';
+        $DirIter = new RecursiveDirectoryIterator($dir);
+        $Iterator = new RecursiveIteratorIterator($DirIter);
+        $Regex = new RegexIterator($Iterator, '/^.+\.tmx$/i', RecursiveRegexIterator::GET_MATCH);
+        foreach ($Regex as $file) {
+            $filePath = $file[0];
+            $xmllintCall = "xmllint --valid --noout {$filePath}";
+            exec(escapeshellcmd($xmllintCall), $xmllintOutput, $xmllintFeedback);
+            $this->assertEquals('0', $xmllintFeedback, "File '$filePath' not valid. (Check with 'xmllint'!)");
+        }
+    }
+
+}
+
+?>
