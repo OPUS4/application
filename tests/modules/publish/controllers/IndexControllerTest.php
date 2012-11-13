aa3e14f3bea0dc8d796280ebbf405478f2e9df03
Sascha Szott <szott@gmx.de>
2012-11-13 20:50:03 +0000

    added tests for config key form.first.bibliographie
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10758 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/modules/publish/controllers/IndexControllerTest.php
+++ tests/modules/publish/controllers/IndexControllerTest.php
@@ -95,9 +95,7 @@ class Publish_IndexControllerTest extends ControllerTestCase{
         }
         else {
             $config->form->first->enable_upload = $oldval;
-        }
-
-        echo $this->getResponse()->getBody();
+        }        
 
         $this->assertResponseCode(200);
         $this->assertController('index');
@@ -110,8 +108,69 @@ class Publish_IndexControllerTest extends ControllerTestCase{
         $this->assertNotContains("<input type='file' name='fileupload' id='fileupload' enctype='multipart/form-data' title='Bitte wählen Sie eine Datei, die Sie hochladen möchten ' size='30' />", $this->getResponse()->getBody());
         $this->assertNotContains("<label for='uploadComment'>Kommentar</label>", $this->getResponse()->getBody());
         $this->assertNotContains("<textarea name='uploadComment' class='form-textarea' cols='30' rows='5'  title='hint_uploadComment'  id='uploadComment'></textarea>", $this->getResponse()->getBody());
-
     }
-    
+
+
+    public function testShowBibliographyCheckbox() {
+        $config = Zend_Registry::get('Zend_Config');
+
+        // manipulate config
+        $oldval = null;
+        if (isset($config->form->first->bibliographie)) {
+            $oldval = $config->form->first->bibliographie;
+        }
+        $config->form->first->bibliographie = 1;
+
+        $this->dispatch('/publish');
+
+        // undo config changes before asserting anything
+        if (is_null($oldval)) {
+            unset($config->form->first->bibliographie);
+        }
+        else {
+            $config->form->first->bibliographie = $oldval;
+        }
+
+        $this->assertResponseCode(200);
+        $this->assertController('index');
+        $this->assertAction('index');
+
+        $this->assertContains('<h3 class="document-type">Dokumenttyp und Datei wählen</h3>', $this->getResponse()->getBody());
+        $this->assertContains('<legend>Bibliographie</legend>', $this->getResponse()->getBody());
+        $this->assertContains("<input type='checkbox' class='form-checkbox' name='bibliographie' id='bibliographie' value='1'  />", $this->getResponse()->getBody());        
+        $this->assertContains("<label for='bibliographie'>Zur Bibliographie hinzufügen?</label>", $this->getResponse()->getBody());
+        $this->assertContains("<input type='hidden' name='bibliographie' value='0' />", $this->getResponse()->getBody());
+    }
+
+    public function testDoNotShowBibliographyCheckbox() {
+        $config = Zend_Registry::get('Zend_Config');
+
+        // manipulate config
+        $oldval = null;
+        if (isset($config->form->first->bibliographie)) {
+            $oldval = $config->form->first->bibliographie;
+        }
+        $config->form->first->bibliographie = 0;
+
+        $this->dispatch('/publish');
+
+        // undo config changes before asserting anything
+        if (is_null($oldval)) {
+            unset($config->form->first->bibliographie);
+        }
+        else {
+            $config->form->first->bibliographie = $oldval;
+        }
+
+        $this->assertResponseCode(200);
+        $this->assertController('index');
+        $this->assertAction('index');
+
+        $this->assertContains('<h3 class="document-type">Dokumenttyp und Datei wählen</h3>', $this->getResponse()->getBody());
+        $this->assertNotContains('<legend>Bibliographie</legend>', $this->getResponse()->getBody());
+        $this->assertNotContains("<input type='checkbox' class='form-checkbox' name='bibliographie' id='bibliographie' value='1'  />", $this->getResponse()->getBody());
+        $this->assertNotContains("<label for='bibliographie'>Zur Bibliographie hinzufügen?</label>", $this->getResponse()->getBody());
+        $this->assertNotContains("<input type='hidden' name='bibliographie' value='0' />", $this->getResponse()->getBody());
+    }
 
 }
