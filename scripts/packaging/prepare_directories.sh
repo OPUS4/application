135c8efd17a747a485ed6151cd5a72a4dfe64bd7
Sascha Szott <szott@gmx.de>
2013-04-09 15:02:01 +0000

    added regression tests for OPUSVIER-2759 Issue #OPUSVIER-2759 - Auswahl bei einstufigen Collections im Publish-Formular wird nicht übernommen, wenn man "Browse Down"-Button verwendet
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11179 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/packaging/prepare_directories.sh
+++ scripts/packaging/prepare_directories.sh
@@ -97,6 +97,7 @@ rm -v opus4/application/configs/doctypes/talkzib.xml
 rm -v opus4/application/configs/doctypes/demo_invalid.xml
 rm -v opus4/application/configs/doctypes/demo_invalidfieldname.xml
 rm -v opus4/application/configs/doctypes/demodemo.xml
+rm -v opus4/application/configs/doctypes/single_level_collection.xml
 
 # exclude PHTML-templates for test document types
 rm -v opus4/modules/publish/views/scripts/form/preprintmatheon.phtml
@@ -104,6 +105,7 @@ rm -v opus4/modules/publish/views/scripts/form/demo.phtml
 rm -v opus4/modules/publish/views/scripts/form/collections.phtml
 rm -v opus4/modules/publish/views/scripts/form/talkzib.phtml
 rm -v opus4/modules/publish/views/scripts/form/demodemo.phtml
+rm -v opus4/modules/publish/views/scripts/form/single-level-collection.phtml
 
 # exclude modules
 rm -rv opus4/modules/{remotecontrol,matheon}
