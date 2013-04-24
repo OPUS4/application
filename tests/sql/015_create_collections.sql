1aca28a427812bcbcab0d33a554869fdec2ebe4a
Gunar Maiwald <maiwald@zib.de>
2013-04-24 12:01:24 +0000

    - Fixed Buildbreak
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11216 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/015_create_collections.sql
+++ tests/sql/015_create_collections.sql
@@ -6,7 +6,8 @@ INSERT INTO `collections_roles` (`id`, `name`, `oai_name`, `position`, `visible`
 (17, 'no-root-test', 'no-root-test', 13, 0, 1, 'Name', 1, 'Name', 1),
 (18, 'frontdoor-test-1', 'frontdoor-test-1', 14, 1, 1, 'Name', 1, 'Name', 1),
 (19, 'frontdoor-test-2', 'frontdoor-test-2', 15, 1, 1, 'Name', 1, 'Name', 1),
-(20, 'single-level collection', 'single-level collection', 16, 1, 1, 'Name', 1, 'Name', 1);
+(20, 'single-level collection', 'single-level collection', 16, 1, 1, 'Name', 1, 'Name', 1),
+(21, 'publists', 'publists', 17, 1, 1, 'Name', 1, 'Name', 1);
 
 INSERT INTO `collections` (`id`, `role_id`, `number`, `name`, `oai_subset`, `left_id`, `right_id`, `parent_id`, `visible`) VALUES
 (1, 1, NULL, NULL, NULL, 1, 2, NULL, 1),
@@ -19,4 +20,7 @@ INSERT INTO `collections` (`id`, `role_id`, `number`, `name`, `oai_subset`, `lef
 (16205, 18, '2', 'Test Frontdoor 1.2', 0, 4, 5, 16202, 1),
 (16206, 20, NULL, NULL, NULL, 1, 2, NULL, 1),
 (16207, 20, '1', 's-l coll entry A', 0, 2, 3, 16206, 1),
-(16208, 20, '2', 's-l coll entry B', 0, 4, 5, 16206, 1);
+(16208, 20, '2', 's-l coll entry B', 0, 4, 5, 16206, 1),
+(16209, 21, NULL, NULL, NULL, 1, 6, NULL, 1),
+(16210, 21, 'coll_visible', 'Sichtbare Publikationsliste', 0, 2, 3, 16209, 1),
+(16211, 21, 'coll_invisible', 'Unsichtbare Publikationsliste', 0, 4, 5, 16209, 0);
