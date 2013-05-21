5a672b395fa2ff60c29d03a29c6884c474783c33
Jens Schwidder <schwidder@zib.de>
2013-05-21 11:27:53 +0000

    Klasse {{Admin_Model_FormElementFactory}} um ein paar neue Funktionen erweitert, die verschiedene Formularelement für das neue Metadaten-Formular generieren. Die neuen Funktionen erzeugen zum Beispiel Formularelement für Datums- oder Jahreseingaben. Die neuen Formularelement können an verschiedenen Stellen eingesetzt werden und sind nicht abhängig von einem bestimmten Feld im Model. Issue #OPUSVIER-2788 - Allgemeine Formularklassen für neues Metadaten-Formular
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11247 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/admin/models/FormElementFactory.php
+++ modules/admin/models/FormElementFactory.php
@@ -34,6 +34,10 @@
 
 /**
  * Factory for creating Zend form elements.
+ * 
+ * TODO convert into class for creating neutral Form Elements with validation
+ * TODO remove Model/Field dependent code
+ * TODO move validation configuration into INI file?
  */
 class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
 
@@ -41,14 +45,17 @@ class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
      * Helper for generating translation keys.
      */
     private $__translationKeys;
+    
+    private $__translate;
+    
+    private $__translationPrefix = '';
 
     /**
      * Constructs a Admin_Model_FormElementFactory.
      */
     public function __construct() {
-        $this->__translationKeys =
-                Zend_Controller_Action_HelperBroker::getStaticHelper(
-                        'Translation');
+        $this->__translationKeys = Zend_Controller_Action_HelperBroker::getStaticHelper('Translation');
+        $this->__translate = Zend_Registry::get('Zend_Translate');
     }
 
     /**
@@ -67,7 +74,8 @@ class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
             else {
                 $modelName = get_class($model);
             }
-            $element = $this->_createSelect($modelName, $field);
+            $element = $this->_createSelect($modelName, $field, $model);
+            $element->setDisableTranslator(true);
         }
         elseif ($field->isTextarea()) {
             $element = $this->_createTextarea($field);
@@ -81,13 +89,15 @@ class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
         // TODO consider always prepending the class
         switch ($fieldName) {
             case "Type":
-                $element->setLabel(get_class($model) . "_" . $fieldName);
+                $element->setLabel($this->__translate->translate(get_class($model) . "_" . $fieldName));
                 break;
             default:
-                $element->setLabel($field->getName());
+                $element->setLabel($this->__translate->translate($fieldName));
                 break;
         }
-
+        
+        $element->setDisableTranslator(true);
+        
         return $element;
     }
 
@@ -141,7 +151,7 @@ class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
      * @param Opus_Model_Field $field
      * @return Zend_Form_Element_Select
      */
-    protected function _createSelect($modelName, $field) {
+    protected function _createSelect($modelName, $field, $model = null) {
         $name = $field->getName();
 
         $select = new Zend_Form_Element_Select($name);
@@ -174,6 +184,13 @@ class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
                 case 'Licence':
                 case 'ThesisPublisher':
                 case 'ThesisGrantor':
+                    if ($option instanceof Opus_Model_Abstract) {
+                        $select->addMultiOption($option->getId(), $option);
+                    }
+                    else {
+                        $select->addMultiOption($index, $option);
+                    }
+                    break;
                 case 'Language':
                     if ($option instanceof Opus_Model_Abstract) {
                         $select->addMultiOption($option->getId(), $option);
@@ -181,6 +198,7 @@ class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
                     else {
                         $select->addMultiOption($index, $option);
                     }
+                    $select->setDisableTranslator(true); // TODO Label muß übersetzt werden
                     break;
                 default:
                     if ($name === 'Type' && $modelName === 'Opus_Document') {
@@ -198,6 +216,126 @@ class Admin_Model_FormElementFactory extends Admin_Model_AbstractModel {
         }
 
         return $select;
+    }
+    
+    /**
+     * Erzeugt SELECT für Identifier-Typ.
+     * @return \Zend_Form_Element_Select
+     * 
+     * TODO Validierung hinzufügen?
+     */
+    public function getIdentifierTypeSelect($name = 'Type') {
+        $select = new Zend_Form_Element_Select($name);
+        
+        $types = $this->_getIdentifierTypes();
+        
+        foreach ($types as $type) {
+            // TODO use translationKeys helper?
+            $select->addMultiOption($type, 'Opus_Identifier_Type_Value_' . ucfirst($type));
+        }
+        
+        return $select;
+    }
+    
+    /**
+     * Liefert die möglichen Werte fuer Identifier-Typ zurück.
+     * @return array
+     */
+    protected function _getIdentifierTypes() {
+        $identifier = new Opus_Identifier();
+        return $identifier->getField('Type')->getDefault();
+    }
+    
+
+    public function createLanguageSelect($name = 'Language') {
+        $languages = Zend_Registry::get('Available_Languages');
+        
+        $select = new Zend_Form_Element_Select($name);
+        
+        foreach ($languages as $index => $language) {
+            $select->addMultiOption($index, $language);
+            $select->setDisableTranslator(false); // TODO Check for multiple translations
+        }
+        
+        return $select;
+    }
+    
+    public function createDocumentTypeSelect($name = 'Type') {
+        $docTypeHelper = Zend_Controller_Action_HelperBroker::getStaticHelper('DocumentTypes');
+        
+        $options = $docTypeHelper->getDocumentTypes();
+        
+        $select = new Zend_Form_Element_Select($name);
+        
+        foreach ($options as $index => $language) {
+            $select->addMultiOption($index, $language);
+            $select->setDisableTranslator(false); // TODO Check for multiple translations
+        }
+        
+        return $select;
+    }
+    
+    /**
+     * Prefix fuer Uebersetzungsschluessel von Validierungsfehlermeldungen.
+     * 
+     * TODO Meldungen fuer Publish und Admin vereinigen
+     */
+    const TRANSLATION_KEY_PREFIX = 'admin_validation_error';
+    
+    /**
+     * Erzeugt Eingabefeld fuer Datumsangaben.
+     * 
+     * Ein Datum wird in einem Textfeld eingegeben (leichtes Copy & Paste). Die Validierung haengt von der aktiven 
+     * Sprache ab.
+     * 
+     * @param type $name
+     * @return \Zend_Form_Element_Text
+     */
+    public function createDateElement($name) {
+        $element = new Zend_Form_Element_Text($name);
+        $element->setLabel($this->__translationPrefix . $name);
+
+        $validator = new Form_Validate_Date();
+        $validator->setMessages(array(
+            Zend_Validate_Date::INVALID => self::TRANSLATION_KEY_PREFIX . 'date_invalid',
+            Zend_Validate_Date::INVALID_DATE => self::TRANSLATION_KEY_PREFIX . 'date_invaliddate',
+            Zend_Validate_Date::FALSEFORMAT => self::TRANSLATION_KEY_PREFIX . 'date_falseformat'
+        ));
+        $element->setValidators(array($validator));
+        
+        return $element;
+    }
+    
+    /**
+     * Erzeugt Eingabefeld fuer Jahreszahlen.
+     * 
+     * Jahreszahlen werden in einem Textfeld eingegeben und muessen Integer und groesser als 0 sein.
+     *  
+     * @param string $name
+     * @return \Zend_Form_Element_Text
+     * 
+     * TODO Validierung so konfigurieren, daß nur eine Meldung angezeigt wird?
+     */
+    public function createYearElement($name) {
+        $element = new Zend_Form_Element_Text($name);
+        $element->setLabel($this->__translationPrefix . $name);
+        $validators = array();
+        
+        $validator = new Zend_Validate_Int();
+        $validator->setMessages(array(
+            Zend_Validate_Int::NOT_INT => self::TRANSLATION_KEY_PREFIX . 'year_invalid'
+        ));
+        $validators[] = $validator;
+        
+        $validator = new Zend_Validate_GreaterThan(0);
+        $validator->setMessages(array(
+            Zend_Validate_GreaterThan::NOT_GREATER => self::TRANSLATION_KEY_PREFIX . 'year_invalid'
+        ));
+        $validators[] = $validator;
+        
+        $element->setValidators($validators, true);
+        
+        return $element;
     }
 
 }
