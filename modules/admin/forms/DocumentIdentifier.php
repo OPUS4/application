ab44873dad999db0d847cb70e88ed87209434988
Jens Schwidder <schwidder@zib.de>
2013-05-21 11:24:33 +0000

    Unterformularklasse und Unit Tests für das Editieren der Identifier eines Dokuments hinzugefügt. Issue #OPUSVIER-2792 - Identifier eines Dokuments editieren
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11246 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ modules/admin/forms/DocumentIdentifier.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Module_Admin
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2013, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Formular fuer einen Identifier eines Dokuments.
+ */
+class Admin_Form_DocumentIdentifier extends Admin_Form_AbstractModelSubForm {
+    
+    /**
+     * Name fuer Formularelement fuer Identifier-Wert.
+     */
+    const ELEMENT_VALUE = 'Value';
+    
+    /**
+     * Name fuer Formularelement fuer Identifier-Id.
+     */
+    const ELEMENT_ID = 'Id';
+    
+    /**
+     * Name fuer Forumlarelement fuer Identifer-Typ.
+     */
+    const ELEMENT_TYPE = 'Type';
+
+    /**
+     * Erzeugt Elemente fuer Identifier Formular.
+     */
+    public function init() {
+        parent::init();
+        
+        $elementFactory = new Admin_Model_FormElementFactory();
+        
+        $select = $elementFactory->getIdentifierTypeSelect(self::ELEMENT_TYPE);
+        $select->setRequired(true);
+        $this->addElement($select);
+        
+        $element = new Zend_Form_Element_Text(self::ELEMENT_VALUE);
+        $element->setRequired(true);
+        $this->addElement($element);
+        
+        $element = new Zend_Form_Element_Hidden(self::ELEMENT_ID);
+        $this->addElement($element);
+    }
+    
+    /**
+     * Befuehlt Formularelement von Opus_Identifier Instanz.
+     * @param Opus_Identifier $identifier
+     */
+    public function populateFromModel($identifier) {
+        $this->getElement(self::ELEMENT_TYPE)->setValue($identifier->getType());
+        $this->getElement(self::ELEMENT_VALUE)->setValue($identifier->getValue());
+        $this->getElement(self::ELEMENT_ID)->setValue($identifier->getId());
+    }
+        
+    /**
+     * Aktualisiert Opus_Identifier Instanz aus Formularelementen.
+     * @param Opus_Identifier $identifier
+     */
+    public function updateModel($identifier) {
+        $value = $this->getElement(self::ELEMENT_TYPE)->getValue();
+        $identifier->setType($value);
+        
+        $value = $this->getElement(self::ELEMENT_VALUE)->getValue();
+        $identifier->setValue($value);
+    }
+    
+    public function getModel() {
+        $id = $this->getElement(self::ELEMENT_ID)->getValue();
+                
+        // TODO empty Funktion ist nicht ausreichen ("00" ist empty)
+        if (!empty($id)) {
+            $identifier = new Opus_Identifier($id);
+        }
+        else {
+            $identifier = new Opus_Identifier();
+        }
+        
+        $this->updateModel($identifier);
+                
+        return $identifier;
+    }
+                
+}
---
+++ tests/modules/admin/forms/DocumentIdentifierTest.php
@@ -0,0 +1,119 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application Unit Test
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2013, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Unit Test fuer Identifier Formular Klasse.
+ */
+class Admin_Form_DocumentIdentifierTest extends ControllerTestCase {
+    
+    public function testCreateForm() {
+        $form = new Admin_Form_DocumentIdentifier();
+        
+        $this->assertNotNull($form->getElement('Value'));
+        $this->assertNotNull($form->getElement('Id'));
+        $this->assertNotNull($form->getElement('Type'));
+    }
+    
+    /**
+     * Testet das Setzen der Elemente entsprechend Opus_Identifier. 
+     * 
+     * Dokument 146 wird verwendet, da es vollständig besetzt ist und normalerweise in den Unit Tests nicht modifiziert
+     * wird.
+     */
+    public function testPopulateFromModel() {
+        $form = new Admin_Form_DocumentIdentifier();
+        
+        $document = new Opus_Document(146);
+        $identifiers = $document->getIdentifier();
+        $identifier = $identifiers[0];
+
+        $form->populateFromModel($identifier);
+        
+        $this->assertEquals($identifier->getId(), $form->getElement('Id')->getValue());
+        $this->assertEquals($identifier->getType(), $form->getElement('Type')->getValue());
+        $this->assertEquals($identifier->getValue(), $form->getElement('Value')->getValue());
+    }
+    
+    public function testUpdateModel() {
+        $form = new Admin_Form_DocumentIdentifier();
+        
+        $form->getElement('Type')->setValue('url');
+        $form->getElement('Value')->setValue('test-urn-1');
+        
+        $identifier = new Opus_Identifier();
+        
+        $form->updateModel($identifier);
+        
+        $this->assertEquals('url', $identifier->getType());
+        $this->assertEquals('test-urn-1', $identifier->getValue());
+    }
+    
+    /**
+     * Prueft, dass bei nicht gesetztem Id-Element ein neuer Identifier zurueck geliefert wird.
+     */
+    public function testGetModel() {
+        $form = new Admin_Form_DocumentIdentifier();
+        
+        $form->getElement('Type')->setValue('url');
+        $form->getElement('Value')->setValue('test-urn-1');
+        
+        $identifier = $form->getModel();
+        
+        $this->assertNull($identifier->getId());
+        $this->assertEquals('url', $identifier->getType());
+        $this->assertEquals('test-urn-1', $identifier->getValue());
+    }
+    
+    /**
+     * Prueft, dass bei gesetztem Id-Element, der Identifier mit dieser Id und aktualisierten Werten zurueck geliefert
+     * wird.
+     */
+    public function testGetModelExistingIdentifier() {
+        $form = new Admin_Form_DocumentIdentifier();
+        
+        $document = new Opus_Document(146);
+        $identifiers = $document->getIdentifier();
+        $identifierId = $identifiers[0]->getId();
+        
+        $form->getElement('Id')->setValue($identifierId);
+        $form->getElement('Type')->setValue('url');
+        $form->getElement('Value')->setValue('test-urn-1');
+        
+        $identifier = $form->getModel();
+        
+        $this->assertEquals($identifierId, $identifier->getId());
+        $this->assertEquals('url', $identifier->getType());
+        $this->assertEquals('test-urn-1', $identifier->getValue());
+    }
+    
+}
