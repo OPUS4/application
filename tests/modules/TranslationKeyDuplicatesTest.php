e250e9b70018465b02169a97f4bcb9c8144e74bd
Jens Schwidder <schwidder@zib.de>
2013-06-05 11:31:13 +0000

    Unit Test für doppelte Übersetzungsschlüssel erweitert, so daß die Ursache für Failure leichter erkennbar ist. Issue #OPUSVIER-2807 - Übersetzungen prüfen und anpassen
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11283 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/modules/TranslationKeyDuplicatesTest.php
+++ tests/modules/TranslationKeyDuplicatesTest.php
@@ -27,7 +27,7 @@
  * @category    Application
  * @package     Tests
  * @author      Sascha Szott <szott@zib.de>
- * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @copyright   Copyright (c) 2008-2013, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
  */
@@ -37,8 +37,7 @@ class TranslationKeyDuplicatesTest extends ControllerTestCase {
     public function testDetectDuplicateKeys() {
         $modulesDir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'modules';
         $output = shell_exec("find $modulesDir -name '*tmx' -print0 |xargs -r0 grep -h 'tuid=' |sed -e 's/^.*tuid=\"([^\"]+)\".*$/\1/g' |sort |uniq -d");
-        $this->assertNull($output);
+        $this->assertNull($output, $output);
     }
 
 }
-?>
