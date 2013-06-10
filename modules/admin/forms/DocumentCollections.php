5673f1958777adfc74d4492bcfd1cd7136675a7c
Jens Schwidder <schwidder@zib.de>
2013-06-10 15:49:05 +0000

    Value für *continue* Parameter zum Hinzufügen einer Collection gefixt. Issue #OPUSVIER-2782 - Editieren von Collections im Metadaten-Formular
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11389 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/admin/forms/DocumentCollections.php
+++ modules/admin/forms/DocumentCollections.php
@@ -176,7 +176,7 @@ class Admin_Form_DocumentCollections extends Admin_Form_AbstractDocumentSubForm 
     }
     
     public function continueEdit($request) {
-        if ($request->getParam('continue', null) == 'coladd') {
+        if ($request->getParam('continue', null) == 'addcol') {
             $colId = $request->getParam('colId');
             
             $this->_addCollection($colId);
