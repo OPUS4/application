dc4c7356bc8fadfd5cbc8558d4c85d3505ae2940
Sascha Szott <szott@gmx.de>
2013-06-13 14:54:37 +0000

    added new test document for regression test of OPUSVIER-2843 Issue #OPUSVIER-2843 - Enthält eine Schriftenreihe eine Bandnummer, die mit der ID der Schriftenreihe übereinstimmt, so kann im Publish diese Schriftenreihe nicht mehr ausgewählt werden
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11442 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/sql/999_create_documents_testdata__series.sql
+++ tests/sql/999_create_documents_testdata__series.sql
@@ -4,3 +4,9 @@ INSERT INTO `documents` (`id`, `completed_date`, `completed_year`, `contributing
 INSERT INTO `link_documents_series` (`document_id`, `series_id`, `number`, `doc_sort_order`) VALUES
 (149,3,'id-3-is-invisible',1),
 (149,4,'id-4-is-visible',1);
+
+INSERT INTO `documents` (`id`, `completed_date`, `completed_year`, `contributing_corporation`, `creating_corporation`, `thesis_year_accepted`, `type`, `edition`, `issue`, `language`, `page_first`, `page_last`, `page_number`, `publication_state`, `published_date`, `published_year`, `publisher_name`, `publisher_place`, `server_date_modified`, `server_date_published`, `server_state`, `volume`, `belongs_to_bibliography`) VALUES
+(301, NULL, 2012, NULL, NULL, '2013', 'preprint', NULL, NULL, 'deu', NULL, NULL, NULL, 'draft', NULL, 2012, '', NULL, '2013-01-23T15:23:42Z', '2013-01-23T15:23:42Z', 'unpublished', NULL, 0);
+
+INSERT INTO `link_documents_series` (`document_id`, `series_id`, `number`, `doc_sort_order`) VALUES
+(301,6,'6',1);
