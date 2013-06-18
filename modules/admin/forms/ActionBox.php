06d16872bfcdc614356aa70a975d820c0ef2d5bd
Jens Schwidder <schwidder@zib.de>
2013-06-18 08:39:08 +0000

    Anpassungen an InfoBox. Übersetzungen. Buttons. Rendering der Navigationsliste. Issue #OPUSVIER-2787 - Actionbox für nicht editierbare Felder und Action-Element wie Speichern implementieren
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11558 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- modules/admin/forms/ActionBox.php
+++ modules/admin/forms/ActionBox.php
@@ -40,18 +40,59 @@
  */
 class Admin_Form_ActionBox extends Admin_Form_AbstractDocumentSubForm {
     
+    const ELEMENT_SAVE = 'Save';
+    
+    const ELEMENT_CANCEL = 'Cancel';
+    
     private $document;
 
     public function init() {
+        parent::init();
         
+        $element = new Zend_Form_Element_Submit(self::ELEMENT_SAVE);
+        $element->setValue('save');
+        $element->removeDecorator('DtDdWrapper');
+        $this->addElement($element);
+        
+        $element = new Zend_Form_Element_Submit(self::ELEMENT_CANCEL);
+        $element->setValue('cancel');
+        $element->removeDecorator('DtDdWrapper');
+        $this->addElement($element);
     }
     
     public function populateFromModel($document) {
         $this->document= $document;
     }
     
+    public function constructFromPost($post, $document = null) {
+        $this->document = document;
+    }
+    
     public function getDocument() {
         return $this->document;
+    }
+    
+    /**
+     * 
+     * @return string
+     * 
+     * TODO Über clevere Lösung zum automatischen Generieren aus der Formularstruktur nachdenken.
+     */
+    public function getJumpLinks() {
+        $links = array();
+        
+        $links['#fieldset-General'] = 'Allgemeines';
+        $links['#fieldset-Persons'] = 'Personen';
+        $links['#fieldset-Titles'] = 'Titles';
+        $links['#fieldset-Series'] = 'Schriftenreihe';
+        $links['#fieldset-Enrichments'] = 'Erweiterung';
+        $links['#fieldset-Collections'] = 'Collection';
+        $links['#fieldset-Identifiers'] = 'Identifier';
+        $links['#fieldset-Licences'] = 'Lizenzen';
+        $links['#fieldset-Patents'] = 'Patentinformationen';
+        $links['#fieldset-Notes'] = 'Bemerkung';
+        
+        return $links;
     }
     
     public function loadDefaultDecorators() {
