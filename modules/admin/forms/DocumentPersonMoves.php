32789647156d975b0f4947141a06f12d1d3195d7
Jens Schwidder <schwidder@zib.de>
2013-06-21 13:49:59 +0000

    Neues Unterformular für die Buttons zum Verschieben einer Person. Issue #OPUSVIER-2773 - Formular(e) zum Verwalten der Personen eines Dokuments
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11690 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ modules/admin/forms/DocumentPersonMoves.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Module_Admin
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2013, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Unterformular fuer die Buttons, um die Rolle einer Person zu ändern.
+ */
+class Admin_Form_DocumentPersonMoves extends Admin_Form_AbstractDocumentSubForm {
+    
+    const POSITION_FIRST = 'first';
+    
+    const POSITION_LAST = 'last';
+    
+    const RESULT_MOVE = 'move';
+    
+    /**
+     * Mögliche Rollen für eine Person.
+     * @var array
+     * 
+     * TODO centralize
+     */
+    private $moves =  array('First', 'Up', 'Down', 'Last');
+    
+    public function __construct($position = null, $options = null) {
+        parent::__construct($options);
+        if (!is_null($role)) {
+            $this->removeElement($this->getRoleElementName($role));
+        }
+    }
+    
+    /**
+     * Erzeugt Buttons für sämtliche Rollen und kümmert sich um Dekoratoren.
+     */
+    public function init() {
+        parent::init();
+        
+        $this->setDecorators(array(
+            'FormElements',
+            array('HtmlTag', array('tag' => 'ul'))
+        ));
+        
+        foreach ($this->moves as $move) {
+            $element = new Form_Element_Submit($move);
+            $element->setDecorators(array(
+                'ViewHelper',
+                array('HtmlTag', array('tag' => 'li'))
+                ));
+            
+            $this->addElement($element);
+        }
+    }
+    
+    /**
+     * Prüft ob in einem POST einer der Rollen-Buttons geklickt wurde.
+     * @param array $post POST Daten für Formular
+     * @param array $context POST Daten für gesamtes Formular
+     * @return array
+     */
+    public function processPost($post, $context) {
+        // Prüfen, ob Button für Rollenänderung ausgewählt wurde
+        foreach ($this->moves as $move) {
+            if (array_key_exists($move, $post)) {
+                return array(
+                    'result' => self::RESULT_MOVE,
+                    'move' => $move
+                );
+            }
+        }
+        
+        return null;
+    }
+        
+    public function populateFromModel($personLink) {
+    }
+    
+}
