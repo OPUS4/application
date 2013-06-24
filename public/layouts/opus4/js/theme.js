957f51d2055b88738123e5e9db791868ab060281
Jens Schwidder <schwidder@zib.de>
2013-06-24 08:34:26 +0000

    CSS, Javascrip und Bilder vom Grafiker aktualisiert. Issue #OPUSVIER-2747 - Überarbeitung der Gestaltung der Administration durch Grafiker
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@11718 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- public/layouts/opus4/js/theme.js
+++ public/layouts/opus4/js/theme.js
@@ -49,4 +49,45 @@ $(document).ready(function() {
 			$(".dropdown-open").removeClass("dropdown-open");
 		}
 	});
+	
+	jQuery(function() {
+		jQuery.support.placeholder = false;
+		test = document.createElement('input');
+		if('placeholder' in test) jQuery.support.placeholder = true;
+	});
+
+	$(function() {
+		if(!$.support.placeholder) {
+			var active = document.activeElement;
+			$(':text').focus(function () {
+			if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+				$(this).val('').removeClass('hasPlaceholder');
+			}
+			}).blur(function () {
+			if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+			$(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+			}
+			});
+			$(':text').blur();
+			$(active).focus();
+			$('form').submit(function () {
+			$(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
+			});
+		}
+	});
+
+	// Styling placeholder
+	$('[placeholder]').addClass("blur");
+	$('[placeholder]').focus(function(){
+		$(this).removeClass("blur");
+	});
+	$('[placeholder]').blur(function()
+	{
+		if( !$(this).val() ) {
+			  $(this).addClass('blur');
+		} else {
+			  $(this).removeClass('blur');
+		}
+	});
+
 });
