135251ab2eb9d30fdc43c84cb9de55d7541bac66
Sascha Szott <szott@gmx.de>
2013-07-24 11:51:24 +0000

    enforce "ASCII characters only" for directory tree output
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@12296 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- scripts/packaging/doku-directory-tree.sh
+++ scripts/packaging/doku-directory-tree.sh
@@ -22,7 +22,7 @@ VERSION=trunk
 TEMPDIR="$(date --iso-8601=seconds)-doku-tree-opus4-$VERSION"
 
 ./prepare_tarball.sh "$VERSION" "$TEMPDIR"
-tree -L 3 -F --dirsfirst "$TEMPDIR" >"$TEMPDIR.tree"
+tree -L 3 -F --dirsfirst --charset ascii "$TEMPDIR" >"$TEMPDIR.tree"
 
 rm -rv "$TEMPDIR/"
 rm -rv "$TEMPDIR.tgz"
