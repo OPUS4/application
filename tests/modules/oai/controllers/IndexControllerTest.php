046fa3547bc9413061a0f77bb3ccd9f4d0bd8887
Edouard Simon <e.simon@gmx.net>
2013-09-19 11:25:10 +0000

    Fixed Test broken  due to OPUSVIER-3072
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@12696 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/modules/oai/controllers/IndexControllerTest.php
+++ tests/modules/oai/controllers/IndexControllerTest.php
@@ -401,12 +401,12 @@ class Oai_IndexControllerTest extends ControllerTestCase {
         $xpath = $this->prepareXpathFromResultString($response->getBody());
 
         // Regression test for OPUSVIER-1788 (show DDC 51)
-        $elements = $xpath->query('//xMetaDiss:xMetaDiss/dc:subject[@xsi:type="dcterms:DDC" and text()="51"]');
+        $elements = $xpath->query('//xMetaDiss:xMetaDiss/dc:subject[@xsi:type="xMetaDiss:DDC-SG" and text()="51"]');
         $this->assertEquals(1, $elements->length,
                 "Unexpected count for ddc:51 (should be visible)");
 
         // Regression test for OPUSVIER-1788 (dont show DDC 28)
-        $elements = $xpath->query('//xMetaDiss:xMetaDiss/dc:subject[@xsi:type="dcterms:DDC" and text()="28"]');
+        $elements = $xpath->query('//xMetaDiss:xMetaDiss/dc:subject[@xsi:type="xMetaDiss:DDC-SG" and text()="28"]');
         $this->assertEquals(0, $elements->length,
                 "Unexpected count for ddc:28 (should be invisible)");
     }
