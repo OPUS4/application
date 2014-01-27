d0c76a6eeb645f70dd9bd88fdbbca266d5f35728
Jens Schwidder <schwidder@zib.de>
2014-01-27 10:36:47 +0000

    OPUSVIER-2996 - Formularklasse für Opus_CollectionRole implementiert. Unit Tests.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@12922 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ modules/admin/forms/CollectionRole.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Admin_Form
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2014, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+class Admin_Form_CollectionRole extends Application_Form_Model_Abstract {
+
+    const ELEMENT_NAME = 'Name';
+    const ELEMENT_OAI_NAME = 'OaiName';
+    const ELEMENT_POSITION = 'Position';
+    const ELEMENT_VISIBLE = 'Visible';
+    const ELEMENT_VISIBLE_BROWSING_START = 'VisibleBrowsingStart';
+    const ELEMENT_VISIBLE_FRONTDOOR = 'VisibleFrontdoor';
+    const ELEMENT_VISIBLE_OAI = 'VisibleOai';
+    const ELEMENT_DISPLAY_BROWSING = 'DisplayBrowsing';
+    const ELEMENT_DISPLAY_FRONTDOOR = 'DisplayFrontdoor';
+
+    public function init() {
+        parent::init();
+
+        $this->setRemoveEmptyCheckbox(false);
+        $this->setUseNameAsLabel(true);
+
+        $this->addElement('text', self::ELEMENT_NAME, array('required' => true, 'size' => 70));
+        $this->addElement('text', self::ELEMENT_OAI_NAME, array('required' => true, 'size' => 30));
+        $this->addElement('Position', self::ELEMENT_POSITION);
+        $this->addElement('checkbox', self::ELEMENT_VISIBLE);
+        $this->addElement('checkbox', self::ELEMENT_VISIBLE_BROWSING_START);
+        $this->addElement('checkbox', self::ELEMENT_VISIBLE_FRONTDOOR);
+        $this->addElement('checkbox', self::ELEMENT_VISIBLE_OAI);
+        $this->addElement('CollectionDisplayFormat', self::ELEMENT_DISPLAY_BROWSING);
+        $this->addElement('CollectionDisplayFormat', self::ELEMENT_DISPLAY_FRONTDOOR);
+
+        $this->removeElement('Cancel');
+    }
+
+    public function populateFromModel($collectionRole) {
+        $this->getElement(self::ELEMENT_MODEL_ID)->setValue($collectionRole->getId());
+        $this->getElement(self::ELEMENT_NAME)->setValue($collectionRole->getName());
+        $this->getElement(self::ELEMENT_OAI_NAME)->setValue($collectionRole->getOaiName());
+        $this->getElement(self::ELEMENT_POSITION)->setValue($collectionRole->getPosition());
+        $this->getElement(self::ELEMENT_VISIBLE)->setValue($collectionRole->getVisible());
+        $this->getElement(self::ELEMENT_VISIBLE_OAI)->setValue($collectionRole->getVisibleOai());
+        $this->getElement(self::ELEMENT_VISIBLE_BROWSING_START)->setValue($collectionRole->getVisibleBrowsingStart());
+        $this->getElement(self::ELEMENT_VISIBLE_FRONTDOOR)->setValue($collectionRole->getVisibleFrontdoor());
+        $this->getElement(self::ELEMENT_DISPLAY_BROWSING)->setValue($collectionRole->getDisplayBrowsing());
+        $this->getElement(self::ELEMENT_DISPLAY_FRONTDOOR)->setValue($collectionRole->getDisplayFrontdoor());
+    }
+
+    public function updateModel($collectionRole) {
+        $collectionRole->setName($this->getElementValue(self::ELEMENT_NAME));
+        $collectionRole->setOaiName($this->getElementValue(self::ELEMENT_OAI_NAME));
+        $collectionRole->setPosition($this->getElementValue(self::ELEMENT_POSITION));
+        $collectionRole->setVisible($this->getElementValue(self::ELEMENT_VISIBLE));
+        $collectionRole->setVisibleBrowsingStart($this->getElementValue(self::ELEMENT_VISIBLE_BROWSING_START));
+        $collectionRole->setVisibleFrontdoor($this->getElementValue(self::ELEMENT_VISIBLE_FRONTDOOR));
+        $collectionRole->setVisibleOai($this->getElementValue(self::ELEMENT_VISIBLE_OAI));
+        $collectionRole->setDisplayBrowsing($this->getElementValue(self::ELEMENT_DISPLAY_BROWSING));
+        $collectionRole->setDisplayFrontdoor($this->getElementValue(self::ELEMENT_DISPLAY_FRONTDOOR));
+    }
+
+}
---
+++ tests/modules/admin/forms/CollectionRoleTest.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application Unit Test
+ * @package     Admin_Form
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2014, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+class Admin_Form_CollectionRoleTest extends ControllerTestCase {
+
+    public function testConstructForm() {
+        $form = new Admin_Form_CollectionRole();
+
+        $this->assertEquals(11, count($form->getElements()));
+
+        $this->assertNotNull($form->getElement('Name'));
+        $this->assertNotNull($form->getElement('OaiName'));
+        $this->assertNotNull($form->getElement('Position'));
+        $this->assertNotNull($form->getElement('Visible'));
+        $this->assertNotNull($form->getElement('VisibleBrowsingStart'));
+        $this->assertNotNull($form->getElement('VisibleFrontdoor'));
+        $this->assertNotNull($form->getElement('VisibleOai'));
+        $this->assertNotNull($form->getElement('DisplayBrowsing'));
+        $this->assertNotNull($form->getElement('DisplayFrontdoor'));
+
+        $this->assertNotNull($form->getElement('Save'));
+        $this->assertNull($form->getElement('Cancel'));
+        $this->assertNotNull($form->getElement('Id'));
+    }
+
+    public function testPopulateFromModel() {
+        $form = new Admin_Form_CollectionRole();
+
+        $model = new Opus_CollectionRole();
+
+        $model->setName('TestName');
+        $model->setOaiName('TestOaiName');
+        $model->setPosition(5);
+        $model->setVisible(1);
+        $model->setVisibleBrowsingStart(1);
+        $model->setVisibleFrontdoor(1);
+        $model->setVisibleOai(1);
+        $model->setDisplayBrowsing('Name');
+        $model->setDisplayFrontdoor('NumberName');
+
+        $form->populateFromModel($model);
+
+        $this->assertEquals('TestName', $form->getElement('Name')->getValue());
+        $this->assertEquals('TestOaiName', $form->getElement('OaiName')->getValue());
+        $this->assertEquals(5, $form->getElement('Position')->getValue());
+        $this->assertEquals(1, $form->getElement('Visible')->getValue());
+        $this->assertEquals(1, $form->getElement('VisibleBrowsingStart')->getValue());
+        $this->assertEquals(1, $form->getElement('VisibleFrontdoor')->getValue());
+        $this->assertEquals(1, $form->getElement('VisibleOai')->getValue());
+        $this->assertEquals('Name', $form->getElement('DisplayBrowsing')->getValue());
+        $this->assertEquals('NumberName', $form->getElement('DisplayFrontdoor')->getValue());
+    }
+
+    public function testPopulateFromModelWithId() {
+        $form = new Admin_Form_CollectionRole();
+
+        $model = new Opus_CollectionRole(2);
+
+        $form->populateFromModel($model);
+
+        $this->assertEquals(2, $form->getElement('Id')->getValue());
+        $this->assertEquals('ddc', $form->getElement('Name')->getValue());
+    }
+
+    public function testUpdateModel() {
+        $form = new Admin_Form_CollectionRole();
+
+        $form->getElement('Id')->setValue(99);
+        $form->getElement('Name')->setValue('TestName');
+        $form->getElement('OaiName')->setValue('TestOaiName');
+        $form->getElement('Position')->setValue(7);
+        $form->getElement('Visible')->setValue(1);
+        $form->getElement('VisibleBrowsingStart')->setValue(1);
+        $form->getElement('VisibleFrontdoor')->setValue(1);
+        $form->getElement('VisibleOai')->setValue(1);
+        $form->getElement('DisplayBrowsing')->setValue('NumberName');
+        $form->getElement('DisplayFrontdoor')->setValue('NameNumber');
+
+        $model = new Opus_CollectionRole();
+
+        $form->updateModel($model);
+
+        $this->assertNull($model->getId());
+        $this->assertEquals('TestName', $model->getName());
+        $this->assertEquals('TestOaiName', $model->getOaiName());
+        $this->assertEquals(7, $model->getPosition());
+        $this->assertEquals(1, $model->getVisible());
+        $this->assertEquals(1, $model->getVisibleBrowsingStart());
+        $this->assertEquals(1, $model->getVisibleFrontdoor());
+        $this->assertEquals(1, $model->getVisibleOai());
+        $this->assertEquals('NumberName', $model->getDisplayBrowsing());
+        $this->assertEquals('NameNumber', $model->getDisplayFrontdoor());
+    }
+
+    public function testValidationEmptyPost() {
+        $form = new Admin_Form_CollectionRole();
+
+        $this->assertFalse($form->isValid(array()));
+
+        $this->assertContains('isEmpty', $form->getErrors('Name'));
+        $this->assertContains('isEmpty', $form->getErrors('OaiName'));
+    }
+
+    public function testValidationSuccess() {
+        $form = new Admin_Form_CollectionRole();
+
+        $this->assertTrue($form->isValid(array(
+            'Name' => 'TestName',
+            'OaiName' => 'TestOaiName'
+        )));
+    }
+
+}
