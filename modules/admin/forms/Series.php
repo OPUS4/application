eb7dd57345c08063c2d2397e36435f1c31bfa801
Jens Schwidder <schwidder@zib.de>
2014-01-30 10:37:50 +0000

    OPUSVIER-2995 - Formularklasse für Opus_Series implementiert. Unit Tests.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@12923 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ modules/admin/forms/Series.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Admin_Form
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2014, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+class Admin_Form_Series extends Application_Form_Model_Abstract {
+
+    const ELEMENT_TITLE = 'Title';
+    const ELEMENT_INFOBOX = 'Infobox';
+    const ELEMENT_VISIBLE = 'Visible';
+    const ELEMENT_SORT_ORDER = 'SortOrder';
+
+    public function init() {
+        parent::init();
+
+        $this->setRemoveEmptyCheckbox(false);
+        $this->setUseNameAsLabel(true);
+
+        $this->addElement('text', self::ELEMENT_TITLE, array('required' => true, 'size' => 70));
+        $this->addElement('textarea', self::ELEMENT_INFOBOX);
+        $this->addElement('checkbox', self::ELEMENT_VISIBLE);
+        $this->addElement('text', self::ELEMENT_SORT_ORDER); // TODO improve?
+    }
+
+    public function populateFromModel($series) {
+        $this->getElement(self::ELEMENT_MODEL_ID)->setValue($series->getId());
+        $this->getElement(self::ELEMENT_TITLE)->setValue($series->getTitle());
+        $this->getElement(self::ELEMENT_INFOBOX)->setValue($series->getInfobox());
+        $this->getElement(self::ELEMENT_VISIBLE)->setValue($series->getVisible());
+        $this->getElement(self::ELEMENT_SORT_ORDER)->setValue($series->getSortOrder());
+    }
+
+    public function updateModel($series) {
+        $series->setTitle($this->getElementValue(self::ELEMENT_TITLE));
+        $series->setInfobox($this->getElementValue(self::ELEMENT_INFOBOX));
+        $series->setVisible($this->getElementValue(self::ELEMENT_VISIBLE));
+        $series->setSortOrder($this->getElementValue(self::ELEMENT_SORT_ORDER));
+    }
+
+}
---
+++ tests/modules/admin/forms/SeriesTest.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application Unit Test
+ * @package     Admin_Form
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2014, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+class Admin_Form_SeriesTest extends ControllerTestCase {
+
+    public function testConstructForm() {
+        $form = new Admin_Form_Series();
+
+        $this->assertEquals(7, count($form->getElements()));
+
+        $this->assertNotNull($form->getElement('Title'));
+        $this->assertNotNull($form->getElement('Infobox'));
+        $this->assertNotNull($form->getElement('Visible'));
+        $this->assertNotNull($form->getElement('SortOrder'));
+
+        $this->assertNotNull($form->getElement('Save'));
+        $this->assertNotNull($form->getElement('Cancel'));
+        $this->assertNotNull($form->getElement('Id'));
+    }
+
+    public function testPopulateFromModel() {
+        $form = new Admin_Form_Series();
+
+        $series = new Opus_Series();
+        $series->setTitle('TestTitle');
+        $series->setInfobox('TestInfo');
+        $series->setVisible(1);
+        $series->setSortOrder(20);
+
+        $form->populateFromModel($series);
+
+        $this->assertEquals('TestTitle', $form->getElement('Title')->getValue());
+        $this->assertEquals('TestInfo', $form->getElement('Infobox')->getValue());
+        $this->assertEquals(1, $form->getElement('Visible')->getValue());
+        $this->assertEquals(20, $form->getElement('SortOrder')->getValue());
+    }
+
+    public function testPopulateFromModelWithId() {
+        $form = new Admin_Form_Series();
+
+        $series = new Opus_Series(2);
+
+        $form->populateFromModel($series);
+
+        $this->assertEquals(2, $form->getElement('Id')->getValue());
+    }
+
+    public function testUpdateModel() {
+        $form = new Admin_Form_Series();
+
+        $form->getElement('Title')->setValue('TestTitle');
+        $form->getElement('Infobox')->setValue('TestInfo');
+        $form->getElement('Visible')->setValue(1);
+        $form->getElement('SortOrder')->setValue(22);
+
+        $series = new Opus_Series();
+
+        $form->updateModel($series);
+
+        $this->assertEquals('TestTitle', $series->getTitle());
+        $this->assertEquals('TestInfo', $series->getInfobox());
+        $this->assertEquals(1, $series->getVisible());
+        $this->assertEquals(22, $series->getSortOrder());
+    }
+
+    public function testValidationEmptyPost() {
+        $form = new Admin_Form_Series();
+
+        $this->assertFalse($form->isValid(array()));
+
+        $this->assertContains('isEmpty', $form->getErrors('Title'));
+    }
+
+    public function testValidationEmptyFields() {
+        $form = new Admin_Form_Series();
+
+        $this->assertFalse($form->isValid(array('Title' => '  ')));
+
+        $this->assertContains('isEmpty', $form->getErrors('Title'));
+    }
+
+    public function testValidationTrue() {
+        $form = new Admin_Form_Series();
+
+        $this->assertTrue($form->isValid(array('Title' => 'TestTitle')));
+    }
+
+}
