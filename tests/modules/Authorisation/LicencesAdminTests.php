3e5db66cc16998af56bf6ff87be01104331f57b2
Michael Lang <lang@zib.de>
2014-03-20 13:51:41 +0000

    OPUSVIER-3256 - Authorisation/LicencesAdminTest wurde übertragen
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@12981 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/modules/Authorisation/LicencesAdminTests.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: michael
+ * Date: 3/20/14
+ * Time: 2:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class LicencesAdminTest extends ControllerTestCase {
+
+    public function setUp() {
+        parent::setUp();
+        $this->enableSecurity();
+        $this->loginUser('security2', 'security2pwd');
+    }
+
+    public function tearDown() {
+        $this->logoutUser();
+        $this->restoreSecuritySetting();
+        parent::tearDown();
+    }
+
+    /**
+     * Prüft, ob nur die erlaubten Einträge im Admin Menu angezeigt werden.
+     */
+    public function testAdminMenuFiltering() {
+        $this->dispatch('/admin');
+        $this->assertQuery('//a[@href="/admin/licence"]');
+        $this->assertQuery('//a[@href="/admin/index/info"]');
+        $this->assertNotQuery('//a[@href="/admin/documents"]');
+        $this->assertNotQuery('//a[@href="/admin/index/security"]');
+        $this->assertNotQuery('//a[@href="/admin/collectionroles"]');
+        $this->assertNotQuery('//a[@href="/admin/series"]');
+        $this->assertNotQuery('//a[@href="/admin/language"]');
+        $this->assertNotQuery('//a[@href="/admin/dnbinstitute"]');
+        $this->assertQuery('//a[@href="/admin/index/setup"]');
+        $this->assertNotQuery('//a[@href="/review"]');
+    }
+
+    /**
+     * Prüft, ob auf die Seite zur Verwaltung von Lizenzen zugegriffen werden kann.
+     */
+    public function testAccessLicenceController() {
+        $this->useEnglish();
+        $this->dispatch('/admin/licence');
+        $this->assertQueryContentContains('//html/head/title', 'Admin Licences');
+    }
+
+    /**
+     * Prüft, das nicht auf die Seite zur Verwaltung von Dokumenten zugegriffen werden kann.
+     */
+    public function testNoAccessDocumentsController() {
+        $this->useEnglish();
+        $this->dispatch('/admin/documents');
+        $this->assertRedirectTo('/auth');
+    }
+
+    /**
+     * Prüft, ob fuer Nutzer mit vollem Zugriff auf Admin Modul der Edit Link in der Frontdoor angezeigt wird.
+     */
+    public function testEditLinkInFrontdoorNotPresent() {
+        $this->useEnglish();
+        $this->dispatch('/frontdoor/index/index/docId/92');
+        $this->assertNotQueryContentContains('//html/body', 'Edit this document');
+    }
+
+    public function testNoAccessFilebrowserController() {
+        $this->useEnglish();
+        $this->dispatch('/admin/filebrowser/index/docId/92');
+        $this->assertRedirectTo('/auth');
+    }
+
+    public function testNoAccessWorkflowController() {
+        $this->useEnglish();
+        $this->dispatch('/admin/workflow/changestate/docId/300/targetState/deleted');
+        $this->assertRedirectTo('/auth');
+    }
+
+    public function testNoAccessAccessController() {
+        $this->useEnglish();
+        $this->dispatch('/admin/access/listmodule/roleid/2');
+        $this->assertRedirectTo('/auth');
+    }
+
+}
