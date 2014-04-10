ddcdf376189470322decb3a8f805592d2b0680ad
Michael Lang <lang@zib.de>
2014-04-10 13:50:14 +0000

    OPUSVIER-2346 - Feld sort-order wurde wieder gelöscht
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@13060 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/masterdata/013_create_collections_open_access.sql
+++ db/masterdata/013_create_collections_open_access.sql
@@ -6,8 +6,8 @@ INSERT INTO `collections_roles` (`id`, `name`, `oai_name`, `position`, `visible`
 INSERT INTO `collections_roles` (`id`, `name`, `oai_name`, `position`, `visible`, `visible_browsing_start`, `display_browsing`, `visible_frontdoor`, `display_frontdoor`, `visible_oai`) VALUES
 (23, 'ec_fundedresources', 'open_aire', 19, 0, 1, 'Name', 1, 'Name', 1);
 
-INSERT INTO `collections` (`id`, `role_id`, `number`, `name`, `oai_subset`, `sort_order`, `left_id`, `right_id`, `parent_id`, `visible`, `visible_publish`) VALUES
-(16200, @roleid, NULL, NULL, NULL, 0, 1, 4, NULL, 1, 1),
-(16201, @roleid, '', 'open_access', 'open_access', 0, 2, 3, 16200, 1, 1),
-(16215, 23, NULL, '', '', 0, 1, 4, NULL, 1, 1),
-(16216, 23, NULL, 'ec_fundedresources', 'open_aire', 0, 2, 3, 16215, 0, 1);
+INSERT INTO `collections` (`id`, `role_id`, `number`, `name`, `oai_subset`, `left_id`, `right_id`, `parent_id`, `visible`, `visible_publish`) VALUES
+(16200, @roleid, NULL, NULL, NULL, 1, 4, NULL, 1, 1),
+(16201, @roleid, '', 'open_access', 'open_access', 2, 3, 16200, 1, 1),
+(16215, 23, NULL, '', '', 1, 4, NULL, 1, 1),
+(16216, 23, NULL, 'ec_fundedresources', 'open_aire', 2, 3, 16215, 0, 1);
