8a46395aeaadddd1e939275143f746026d020a1b
Jens Schwidder <schwidder@zib.de>
2014-07-03 10:03:04 +0000

    OPUSVIER-3256 - Änderung, um direkten Zugriff auf Variablen der abgeleiteten Klasse zu entfernen. Header bei allen Dateien hinzugefügt.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@13410 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/modules/Authorisation/AccountSecurity17Test.php
+++ tests/modules/Authorisation/AccountSecurity17Test.php
@@ -1,24 +1,46 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: michael
- * Date: 3/21/14
- * Time: 12:45 PM
- * To change this template use File | Settings | File Templates.
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application Unit Test
+ * @author      Michael Lang <lang@zib.de>
+ * @copyright   Copyright (c) 2008-2014, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
  */
-
 class AccountSecurity17Test extends AccessModuleSetupAndAdminTest {
 
     public function setUp() {
-        $this->username = 'security17';
-        $this->password = 'security17pwd';
-        $this->acls = array(
+        parent::setUp();
+        parent::setUpTests('security17', 'security17pwd', array(
             'module_admin' => true,
             'module_setup' => false,
             'controller_staticpage' => true,
             'controller_helppage' => false,
             'controller_language' => false
-        );
-        parent::setUp();
+        ));
     }
+
 }
