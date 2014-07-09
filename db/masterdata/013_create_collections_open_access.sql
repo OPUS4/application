642e06e6bb2a68118c54b1b1250af1ec9fac1933
Michael Lang <lang@zib.de>
2014-07-09 10:38:02 +0000

    OPUSVIER-2346 - Nach OpenAire 3.0 muss das Set 'openaire' heißen. Außerdem muss die Collection und C-Role sichtbar sein, um im OAI von OpenAire ausgelesen zu werden
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@13444 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- db/masterdata/013_create_collections_open_access.sql
+++ db/masterdata/013_create_collections_open_access.sql
@@ -4,10 +4,10 @@ INSERT INTO `collections_roles` (`id`, `name`, `oai_name`, `position`, `visible`
 (@roleid, 'open_access', 'open_access', 100, 1, 0, 'Name', 0, 'Name', 1);
 
 INSERT INTO `collections_roles` (`id`, `name`, `oai_name`, `position`, `visible`, `visible_browsing_start`, `display_browsing`, `visible_frontdoor`, `display_frontdoor`, `visible_oai`) VALUES
-(23, 'open_aire', 'ec_fundedresources', 19, 0, 1, 'Name', 1, 'Name', 1);
+(23, 'OpenAIRE', 'openaire', 19, 1, 1, 'Name', 1, 'Name', 1);
 
 INSERT INTO `collections` (`id`, `role_id`, `number`, `name`, `oai_subset`, `left_id`, `right_id`, `parent_id`, `visible`, `visible_publish`) VALUES
 (16200, @roleid, NULL, NULL, NULL, 1, 4, NULL, 1, 1),
 (16201, @roleid, '', 'open_access', 'open_access', 2, 3, 16200, 1, 1),
 (16215, 23, NULL, '', '', 1, 4, NULL, 1, 1),
-(16216, 23, NULL, 'open_aire', 'ec_fundedresources', 2, 3, 16215, 0, 1);
+(16216, 23, NULL, 'OpenAIRE', 'openaire', 2, 3, 16215, 1, 1);
