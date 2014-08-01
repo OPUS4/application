c0701835c345d69e77fa2075dc254fa5f1d6fde1
Jens Schwidder <schwidder@zib.de>
2014-08-01 14:45:45 +0000

    OPUSVIER-3306 - Test, der prüft, ob Nutzer mit Zugriff auf Schriftenreihenverwaltung, die Edit-Seite für eine Schriftenreihe aufrufen kann.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@13502 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/modules/Authorisation/SeriesAdminTest.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application Unit Test
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2014, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+class SeriesAdminTest extends ControllerTestCase {
+
+    private $userName = 'seriesadminuser';
+
+    private $roleName = 'test-seriesadmin';
+
+    public function setUp() {
+        parent::setUp();
+        $this->enableSecurity();
+
+        $userRole = new Opus_UserRole();
+        $userRole->setName($this->roleName);
+        $userRole->appendAccessModule('admin');
+        $userRole->appendAccessModule('resource_series');
+        $userRole->store();
+
+        $user = new Opus_Account();
+        $user->setLogin($this->userName);
+        $user->setPassword('seriesadminpwd');
+        $user->addRole($userRole);
+        $user->store();
+
+        $this->loginUser($this->userName, 'seriesadminpwd');
+    }
+
+    public function tearDown() {
+        $this->logoutUser();
+        $this->restoreSecuritySetting();
+
+        $user = Opus_Account::fetchAccountByLogin($this->userName);
+        $user->delete();
+
+        $userRole = Opus_UserRole::fetchByName($this->roleName);
+        $userRole->delete();
+
+        parent::tearDown();
+    }
+
+
+    /**
+     * Regression Test für OPUSVIER-3306.
+     */
+    public function testAccessEditSeries() {
+        $this->useEnglish();
+        $this->dispatch('/admin/series/edit/id/4');
+        $this->assertResponseCode(200);
+
+        $this->assertQueryContentContains('//html/head/title', 'Edit Series');
+        $this->assertXPath("//input[@type = 'hidden' and @value = '4']");
+    }
+
+}
