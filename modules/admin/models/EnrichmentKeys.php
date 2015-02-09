1b3ae055c835436a739e5525fea575a10d1c3ac3
Jens Schwidder <schwidder@zib.de>
2015-02-09 14:22:20 +0000

    OPUSVIER-3186 -  Model for handling of enrichment keys added.
    git-svn-id: https://svn.zib.de/opus4dev/server/trunk@13844 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ modules/admin/models/EnrichmentKeys.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Module_Admin
+ * @subpackage  Model
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2015, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * Model for handling operations for enrichment keys.
+ *
+ * enrichmentkey.protected.modules
+ * enrichmentkey.protected.migration
+ *
+ * @category    Application
+ * @package     Module_Admin
+ * @subpackage  Model
+ */
+class Admin_Model_EnrichmentKeys extends Application_Model_Abstract {
+
+    /**
+     * Enrichment keys that are configured as protected.
+     * @var array
+     */
+    private $_protectedKeys;
+
+    /**
+     * Reads list of protected enrichment keys from configuration.
+     *
+     * TODO separate configurations for modules and migration smells funny
+     *
+     * @return array
+     */
+    public function getProtectedEnrichmentKeys() {
+        if (is_null($this->_protectedKeys)) {
+            $config = $this->getConfig();
+
+            $protectedKeys = array();
+
+            if (isset($config->enrichmentkey->protected->modules)) {
+                $protectedKeys = explode(',', $config->enrichmentkey->protected->modules);
+            }
+            else {
+                $this->getLogger()->warn(
+                    'config key \'enrichmentkey.protected.modules\' is not defined in config file'
+                );
+            }
+
+            if (isset($config->enrichmentkey->protected->migration)) {
+                $protectedKeys = array_merge(
+                    $protectedKeys, explode(',', $config->enrichmentkey->protected->migration)
+                );
+            }
+            else {
+                $this->getLogger()->warn(
+                    'config key \'enrichmentkey.protected.migration\' is not defined in config file'
+                );
+            }
+
+            $this->_protectedKeys = $protectedKeys;
+        }
+
+        return $this->_protectedKeys;
+    }
+
+    /**
+     * Sets list of protected enrichment keys in model.
+     * @param $keys array
+     */
+    public function setProtectedEnrichmentKeys($keys) {
+        $this->_protectedKeys = $keys;
+    }
+
+}
---
+++ tests/modules/admin/models/EnrichmentKeysTest.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application Unit Test
+ * @package     Module_Admin
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2015, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+/**
+ * @category    Application Unit Test
+ * @package     Module_Admin
+ */
+class Admin_Model_EnrichmentKeysTest extends ControllerTestCase {
+
+    public function testGetProtectedEnrichmentKeys() {
+        $model = new Admin_Model_EnrichmentKeys();
+
+        $config = new Zend_Config(array('enrichmentkey' => array('protected' => array(
+            'modules' => 'pkey1,pkey2',
+            'migration' => 'pkey3,pkey4'
+        ))));
+
+        $model->setConfig($config);
+
+        $protectedKeys = $model->getProtectedEnrichmentKeys();
+
+        $this->assertCount(4, $protectedKeys);
+        $this->assertContains('pkey1', $protectedKeys);
+        $this->assertContains('pkey2', $protectedKeys);
+        $this->assertContains('pkey3', $protectedKeys);
+        $this->assertContains('pkey4', $protectedKeys);
+
+        $config = new Zend_Config(array('enrichmentkey' => array('protected' => array(
+            'migration' => 'pkey3,pkey4'
+        ))));
+
+        $model->setConfig($config);
+        $model->setProtectedEnrichmentKeys(null);
+
+        $protectedKeys = $model->getProtectedEnrichmentKeys();
+
+        $this->assertCount(2, $protectedKeys);
+        $this->assertContains('pkey3', $protectedKeys);
+        $this->assertContains('pkey4', $protectedKeys);
+
+        $config = new Zend_Config(array('enrichmentkey' => array('protected' => array(
+            'modules' => 'pkey1,pkey2',
+        ))));
+
+        $model->setConfig($config);
+        $model->setProtectedEnrichmentKeys(null);
+
+        $protectedKeys = $model->getProtectedEnrichmentKeys();
+
+        $this->assertCount(2, $protectedKeys);
+        $this->assertContains('pkey1', $protectedKeys);
+        $this->assertContains('pkey2', $protectedKeys);
+    }
+
+    public function testGetProtectedEnrichmentKeysNotConfigured() {
+        $model = new Admin_Model_EnrichmentKeys();
+
+        $config = new Zend_Config(array());
+
+        $model->setConfig($config);
+
+        $protectedKeys = $model->getProtectedEnrichmentKeys();
+
+        $this->assertInternalType('array', $protectedKeys);
+        $this->assertCount(0, $protectedKeys);
+    }
+
+}
