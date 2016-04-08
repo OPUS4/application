ba030b2b0d37d489d4ff5ab9ab8c69b2acee7338
j3nsch <schwidder@zib.de>
2016-04-08 17:32:36 +0200

    OPUSVIER-3583 - Added licence header and removed unused PHP module.

--- bin/install-ubuntu-packages.sh
+++ bin/install-ubuntu-packages.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# LICENCE
+# This code is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# @author      Jens Schwidder <schwidder@zib.de>
+# @copyright   Copyright (c) 2010-2016, OPUS 4 development team
+# @license     http://www.gnu.org/licenses/gpl.html General Public License
+#
 
 # Installs required PHP packages on Ubuntu
 apt-get install php5
@@ -9,7 +25,6 @@ apt-get install php5-dev
 apt-get install php5-gd
 apt-get install php5-mcrypt
 apt-get install php5-mysql
-apt-get install php5-uuid
 apt-get install php5-xsl
 apt-get install php-log
 apt-get install libapache2-mod-php5
