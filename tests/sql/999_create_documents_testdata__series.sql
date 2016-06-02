92fc4e59656a665fe27bcbaa80f370098a033aea
j3nsch <schwidder@zib.de>
2016-06-02 12:08:11 +0200

    OPUSVIER-3617 - Added SQL_MODE to create documents without server_date_created.

--- tests/sql/999_create_documents_testdata__series.sql
+++ tests/sql/999_create_documents_testdata__series.sql
@@ -1,3 +1,5 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
 INSERT INTO `documents` (`id`, `completed_date`, `completed_year`, `contributing_corporation`, `creating_corporation`, `thesis_year_accepted`, `type`, `edition`, `issue`, `language`, `page_first`, `page_last`, `page_number`, `publication_state`, `published_date`, `published_year`, `publisher_name`, `publisher_place`, `server_date_modified`, `server_date_published`, `server_state`, `volume`, `belongs_to_bibliography`) VALUES
 (149, NULL, 2012, NULL, NULL, '2012', 'preprint', NULL, NULL, 'deu', NULL, NULL, NULL, 'draft', NULL, 2012, '', NULL, '2012-01-23T15:23:42Z', '2012-01-23T15:23:42Z', 'published', NULL, 0);
 
