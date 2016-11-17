3fb20003037ad4e2ad50a03c137f7ecdfcd64b62
j3nsch <schwidder@zib.de>
2016-11-17 17:25:06 +0100

    OPUSVIER-3615 - Added database update to update script.

--- library/Application/Update/Database.php
+++ library/Application/Update/Database.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This file is part of OPUS. The software OPUS has been originally developed
  * at the University of Stuttgart with funding from the German Research Net,
  * the Federal Department of Higher Education and Research and the Ministry
@@ -25,62 +25,28 @@
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * @category    Application
- * @package     Application
+ * @package     Application_Update
  * @author      Jens Schwidder <schwidder@zib.de>
  * @copyright   Copyright (c) 2008-2016, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
- */
-
-/**
- * Class for performing updates of OPUS 4.
  *
- * TODO logging to file
- * TODO output version numbers
+ * Updates database.
  */
-class Application_Update extends Application_Update_PluginAbstract
+class Application_Update_Database extends Application_Update_PluginAbstract
 {
 
     /**
-     * Bootstrap Zend_Application for update process.
-     */
-    public function bootstrap()
-    {
-        $configFiles = array(
-            APPLICATION_PATH . '/application/configs/application.ini',
-            APPLICATION_PATH . '/application/configs/config.ini'
-        );
-
-        $consoleIniPath = APPLICATION_PATH . '/application/configs/console.ini';
-
-        if (is_readable($consoleIniPath))
-        {
-            $configFiles[] = $consoleIniPath;
-        }
-
-        $application = new Zend_Application(APPLICATION_ENV, array("config"=>$configFiles));
-
-        $application->bootstrap('Configuration');
-    }
-
-    /**
-     * Perform update.
-     *
-     * TODO modify update steps dynamically based on versions involved
+     * Performs database update.
      */
     public function run()
     {
-        $this->log('Updating OPUS 4 ...');
+        $this->println('Updating database ...');
 
-        // Create console.ini if missing
-        $consoleIni = new Application_Update_ConsoleIni();
-        $consoleIni->run();
+        $database = new Opus_Database();
 
-        // Bootstrap again with console.ini containing admin credentials
-        $this->bootstrap();
+        $database->update();
 
-        // Update database
-        $database = new Application_Update_Database();
-        $database->run();
+        $this->println('Database update finished');
     }
 
 }
