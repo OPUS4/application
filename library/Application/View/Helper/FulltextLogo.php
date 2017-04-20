6473a1cec887a8353373f8c27a7b18d77511fe90
j3nsch <schwidder@zib.de>
2017-04-20 11:56:29 +0200

    OPUSVIER-3655 - Added tooltip to fulltext logo.

---
+++ library/Application/View/Helper/FulltextLogo.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     View_Helper
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2017, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ */
+
+/**
+ * View helper for rendering the fulltext logo for documents in the search result list.
+ */
+class Application_View_Helper_FulltextLogo extends Application_View_Helper_Abstract
+{
+
+    public function fulltextLogo($doc)
+    {
+        if (!$doc instanceof Opus_Document)
+        {
+            // TODO log
+            return;
+        }
+
+        $cssClass = "fulltext-logo";
+        $tooltip = null;
+
+
+        if ($doc->hasFulltext())
+        {
+            $cssClass .= ' fulltext';
+            $tooltip = 'fulltext-icon-tooltip';
+        }
+
+        if ($doc->isOpenAccess())
+        {
+            $cssClass .= ' openaccess';
+            $tooltip = 'fulltext-icon-oa-tooltip';
+        }
+
+        $output = "<div class=\"$cssClass\"";
+
+        if (!is_null($tooltip))
+        {
+            $tooltip = $this->view->translate(array($tooltip));
+            $output .= " title=\"$tooltip\"";
+        }
+
+        $output .= "></div>";
+
+        return $output;
+    }
+
+}
