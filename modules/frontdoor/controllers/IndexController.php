b6ae1874dc5440ee61679ac3343da4d1b29c52a1
j3nsch <schwidder@zib.de>
2017-05-15 14:54:54 +0200

    OPUSVIER-3485 - Coding style.

--- modules/frontdoor/controllers/IndexController.php
+++ modules/frontdoor/controllers/IndexController.php
@@ -325,12 +325,13 @@ class Frontdoor_IndexController extends Application_Controller_Action {
                 $metas[] = array("DC.Date", $yearPublished);
             }
         }
+
         $licences = $document->getLicence();
-        foreach ($licences as $docLicence) {
-                   $metas[] = array('DC.rights', $docLicence->getModel()->getLinkLicence() );
 
-            }
-
+        foreach ($licences as $docLicence)
+        {
+            $metas[] = array('DC.rights', $docLicence->getModel()->getLinkLicence() );
+        }
 
         return $metas;
     }
