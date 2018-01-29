da5a76d1419266ea1b8c1754a213b75dff31ffb9
Maximilian Salomon <32635839+biofreak95@users.noreply.github.com>
2018-01-29 19:46:19 +0100

    OPUSVIER-711
    Adding interface

---
+++ modules/admin/controllers/SystemstatusController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: salomon
+ * Date: 29.01.18
+ * Time: 10:45
+ */
+
+class Admin_SystemstatusController extends Application_Controller_Action
+{
+    public function indexAction()
+    {
+
+    }
+}
