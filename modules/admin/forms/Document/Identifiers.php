26166b7bc4fa907a25412eb40b620323dfd23888
Sascha Szott <szott@gmx.de>
2018-04-04 01:20:33 +0200

    added missing form classes

---
+++ modules/admin/forms/Document/Identifiers.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Module_Admin
+ * @author      Sascha Szott <szott@zib.de>
+ * @copyright   Copyright (c) 2018, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ */
+
+class Admin_Form_Document_Identifiers extends Admin_Form_Document_Section {
+
+    public function init() {
+        parent::init();
+
+        // visueller Balken, der den Anfang des Identifier-Blocks markiert
+        $this->setLegend('admin_document_section_identifier');
+
+        $this->addSubForm(
+            new Admin_Form_Document_MultiIdentifierSubForm('Admin_Form_Document_IdentifierDOI'),
+            'IdentifiersDOI'
+        );
+
+        $this->addSubForm(
+            new Admin_Form_Document_MultiIdentifierSubForm('Admin_Form_Document_IdentifierURN'),
+            'IdentifiersURN'
+        );
+
+        // alle anderen Typen von Identifiern: der Validator stellt sicher, dass es nicht möglich ist, zwei Identifier
+        // vom gleichen Typ mit dem gleichen Wert einzutragen (zwei unterschiedliche Werte für den gleichen Typ werden
+        // aber unterstützt)
+        $this->addSubForm(
+            new Admin_Form_Document_MultiIdentifierOtherSubForm(
+                'Admin_Form_Document_Identifier',
+                'Identifier',
+                new Application_Form_Validate_MultiSubForm_RepeatedValues(
+                    'Value',
+                    'admin_document_error_repeated_identifier',
+                    'Type'
+                ),
+                array(
+                    'columns' => array(
+                        array('label' => 'Opus_Identifier_Type'),
+                        array('label' => 'Text')
+                    )
+                )
+            ),
+            'Identifiers'
+        );     
+    }
+
+
+
+}
