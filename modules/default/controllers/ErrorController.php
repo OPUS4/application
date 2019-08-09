b3cff80d708b09a9fe17ed493bf0770562469e06
j3nsch <j3nsch@gmail.com>
2019-08-09 17:22:24 +0200

    OPUSVIER-4112 Fixed bug checking configuration option.

--- modules/default/controllers/ErrorController.php
+++ modules/default/controllers/ErrorController.php
@@ -99,7 +99,7 @@ class ErrorController extends Application_Controller_Action
             return;
         }
 
-        $this->view->showException = isset($this->view->showException)
+        $this->view->showException = isset($errorConfig->showException)
             && filter_var($errorConfig->showException, FILTER_VALIDATE_BOOLEAN);
         if (isset($errorConfig->showRequest) && filter_var($errorConfig->showRequest, FILTER_VALIDATE_BOOLEAN)) {
             $this->view->errorRequest = $errors->request;
