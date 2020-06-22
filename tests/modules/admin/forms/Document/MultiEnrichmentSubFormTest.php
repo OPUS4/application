5fe481e8099c2e827f3a3a0913a20f8024c8bc3a
Sascha Szott <szott@gmx.de>
2020-06-22 19:54:34 +0200

    Revert commit 585ecfcf (try to repeat build break)

--- tests/modules/admin/forms/Document/MultiEnrichmentSubFormTest.php
+++ tests/modules/admin/forms/Document/MultiEnrichmentSubFormTest.php
@@ -38,7 +38,7 @@
 class Admin_Form_Document_MultiEnrichmentSubFormTest extends ControllerTestCase
 {
 
-    protected $additionalResources = ['database'];
+    protected $additionalResources = ['database', 'translation'];
 
     // dieser Enrichment-Key Name stellt sicher, dass der Enrichment-Key
     // im Auswahlfeld aller Enrichment-Keys an der ersten Position steht
