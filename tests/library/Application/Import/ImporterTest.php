01a222913fc1166528e5236dfe0c178cae59a56f
Sascha Szott <szott@gmx.de>
2020-11-23 17:43:13 +0100

    added test cases for OPUSVIER-4298

--- tests/library/Application/Import/ImporterTest.php
+++ tests/library/Application/Import/ImporterTest.php
@@ -56,6 +56,42 @@ class Application_Import_ImporterTest extends ControllerTestCase
         $this->assertEquals('Berlin', $document->getEnrichmentValue('City'));
     }
 
+    /**
+     * Bei der Angabe eines EmbargoDate im Import-XML muss eine Tages- und Monatsangabe sowie
+     * eine Jahresangabe enthalten sein. Eine alleinige Jahresangabe ist nicht zulässig.
+     */
+    public function testImportInvalidEmbargoDate()
+    {
+        $xml = file_get_contents(APPLICATION_PATH . '/tests/resources/import/incomplete-embargo-date.xml');
+
+        $importer = new Application_Import_Importer($xml, false, Log::get());
+
+        $this->setExpectedException(Application_Import_MetadataImportSkippedDocumentsException::class);
+        $importer->run();
+
+        $document = $importer->getDocument();
+        $this->assertNull($document);
+    }
+
+    public function testValidEmbargoDate()
+    {
+        $xml = file_get_contents(APPLICATION_PATH . '/tests/resources/import/embargo-date.xml');
+
+        $importer = new Application_Import_Importer($xml, false, Log::get());
+
+        $importer->run();
+
+        $document = $importer->getDocument();
+
+        $this->assertNotNull($document);
+        $this->assertInstanceOf('Opus\Document', $document);
+
+        $embargoDate = $document->getEmbargoDate();
+        $this->assertEquals(12, $embargoDate->getDay());
+        $this->assertEquals(11, $embargoDate->getMonth());
+        $this->assertEquals(2042, $embargoDate->getYear());
+    }
+
     public function testFromArray()
     {
         $doc = Document::get(146);
