de4eb6308509c4385470f3e367b2fd7e818f0a0d
j3nsch <j3nsch@gmail.com>
2022-01-10 20:04:49 +0100

    #487 Replaced DocumentFinder usage

--- library/Application/Controller/Action/Helper/Documents.php
+++ library/Application/Controller/Action/Helper/Documents.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * This file is part of OPUS. The software OPUS has been originally developed
  * at the University of Stuttgart with funding from the German Research Net,
@@ -24,16 +25,13 @@
  * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * @category    TODO
- * @author      Jens Schwidder <schwidder@zib.de>
- * @author      Thoralf Klein <thoralf.klein@zib.de>
- * @copyright   Copyright (c) 2008-2010, OPUS 4 development team
+ * @copyright   Copyright (c) 2008-2022, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
 
 use Opus\Document;
-use Opus\DocumentFinder;
 use Opus\Model\NotFoundException;
+use Opus\Repository;
 
 /**
  * Helper for getting a list of document IDs used by admin and review module.
@@ -87,30 +85,30 @@ class Application_Controller_Action_Helper_Documents extends \Zend_Controller_Ac
      */
     public function getSortedDocumentIds($sortOrder = null, $sortReverse = true, $state = null)
     {
-        $finder = new DocumentFinder(); // TODO DocumentFinder sorting order
+        $finder = Repository::getInstance()->getDocumentFinder();
 
-        if (! is_null($state) && $state !== 'all') {
+        if ($state !== null && $state !== 'all') {
             $finder->setServerState($state);
         }
 
         switch ($sortOrder) {
             case 'author':
-                $finder->orderByAuthorLastname($sortReverse);
+                $finder->setOrder('Author', $sortReverse);
                 break;
             case 'publicationDate':
-                $finder->orderByServerDatePublished($sortReverse);
+                $finder->setOrder('ServerDatePublished', $sortReverse);
                 break;
             case 'docType':
-                $finder->orderByType($sortReverse);
+                $finder->setOrder('Type', $sortReverse);
                 break;
             case 'title':
-                $finder->orderByTitleMain($sortReverse);
+                $finder->setOrder('Title', $sortReverse);
                 break;
             default:
-                $finder->orderById($sortReverse);
+                $finder->setOrder('Id', $sortReverse);
                 break;
         }
 
-        return $finder->ids();
+        return $finder->getIds();
     }
 }
