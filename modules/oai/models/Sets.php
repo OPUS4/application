9c49a8dde4ab27e4341ee44d1a051aa721953201
j3nsch <j3nsch@gmail.com>
2022-01-12 19:03:58 +0100

    #487 Removed usage of DocumentFinder

--- modules/oai/models/Sets.php
+++ modules/oai/models/Sets.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of OPUS. The software OPUS has been originally developed
  * at the University of Stuttgart with funding from the German Research Net,
@@ -24,15 +25,12 @@
  * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * @category    Application
- * @package     Module_Oai
- * @author      Jens Schwidder <schwidder@zib.de>
  * @copyright   Copyright (c) 2011-2016, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
 
 use Opus\CollectionRole;
-use Opus\DocumentFinder;
+use Opus\Repository;
 
 class Oai_Model_Sets extends Application_Model_Abstract
 {
@@ -72,10 +70,10 @@ class Oai_Model_Sets extends Application_Model_Abstract
 
         $dcTypeHelper = new Application_View_Helper_DcType();
 
-        $finder = new DocumentFinder();
+        $finder = Repository::getInstance()->getDocumentFinder();
         $finder->setServerState('published');
 
-        foreach ($finder->groupedTypesPlusCount() as $doctype => $row) {
+        foreach ($finder->getDocumentTypes() as $doctype) {
             if (0 == preg_match("/^$setSpecPattern$/", $doctype)) {
                 $msg = "Invalid SetSpec (doctype='".$doctype."')."
                     . " Allowed characters are [$setSpecPattern].";
