a658a5e7adb6e866ab10fbd36a186520265ad7ce
j3nsch <j3nsch@gmail.com>
2022-05-10 14:35:16 +0200

    #558 Fixed SearchException references

--- library/Application/SearchException.php
+++ library/Application/SearchException.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of OPUS. The software OPUS has been originally developed
  * at the University of Stuttgart with funding from the German Research Net,
@@ -24,20 +25,19 @@
  * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * @category    Application
- * @package     Application
- * @author      Sascha Szott <szott@zib.de>
- * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @copyright   Copyright (c) 2008, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
+
+use Opus\Search\SearchException;
 
 class Application_SearchException extends Application_Exception
 {
 
     /**
      *
-     * @param Opus\Search\Exception $exception
-     * @param boolean $usePlainMessage set to true if exception message should not be translated
+     * @param SearchException $exception
+     * @param boolean         $usePlainMessage set to true if exception message should not be translated
      */
     public function __construct($exception, $usePlainMessage = false)
     {
