a6ce8cfb1a812915488cbebb73356e3592223394
haogatyp <gatermann@effective-webwork.de>
2023-07-21 17:01:04 +0200

    Thoughts on tasks, workers and jobs.

--- library/Application/Task/TaskInterface.php
+++ library/Application/Task/TaskInterface.php
@@ -29,6 +29,22 @@
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
 
+/*
+    There are two types of Tasks:
+    1. Opus Tasks, described by this Interface.
+    2. Crunz Tasks used by the Crunz cronjob, expectesd in the task directory specified in crunz.yml.
+       In our case there is and will be only one Crunz task: scheduledTasks.php in which the Crunz scheduler runs
+       all the configured Opus tasks.
+
+    An Opus task either contains direct code to complete the necessary sub-steps of a task
+    or delegates its task to a worker.
+    A worker processes a specific type of task by working through a job queue.
+    A runner hands over the jobs to be done to the worker and executes the worker.
+
+    TODO The runner class might be unnecessary and could eventually transformed e.g in a abstract class.
+    TODO An opus task could then inherit from it and start a worker directly.
+*/
+
 /**
  * Basic process interface as required to define
  * tasks for job cron job
