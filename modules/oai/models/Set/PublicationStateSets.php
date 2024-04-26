d7c7727391fb2797584a2d012bcb46f30473a247
j3nsch <j3nsch@gmail.com>
2024-04-26 17:12:34 +0200

    #1184 PublicationState OAI sets

--- modules/oai/models/Set/PublicationStateSets.php
+++ modules/oai/models/Set/PublicationStateSets.php
@@ -29,13 +29,18 @@
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
 
+use Opus\Common\Document;
 use Opus\Common\DocumentFinderInterface;
 use Opus\Common\DocumentInterface;
+use Opus\Common\PublicationState;
+use Opus\Common\Repository;
 
 /**
  * OAI sets based on document field PublicationState.
  *
  * TODO describe requirements for class
+ * TODO mapping of internal types to OAI sets (configurable?)
+ * TODO need function for getting how many documents are available in different states
  */
 class Oai_Model_Set_PublicationStateSets implements Oai_Model_Set_SetTypeInterface
 {
@@ -51,12 +56,19 @@ class Oai_Model_Set_PublicationStateSets implements Oai_Model_Set_SetTypeInterfa
         'updatedVersion',
     ];
 
+    /** @var string[] */
+    private $subsetMapping = [
+        PublicationState::DRAFT     => 'draft',
+        PublicationState::SUBMITTED => 'submittedVersion',
+        PublicationState::ACCEPTED  => 'acceptedVersion',
+        PublicationState::PUBLISHED => 'publishedVersion',
+        PublicationState::CORRECTED => 'updatedVersion',
+        PublicationState::ENHANCED  => 'updatedVersion',
+    ];
+
     /**
      * @param DocumentInterface|null $document
      * @return string[]
-     *
-     * TODO return 0 or 1 set for document if it has PublicationState and is visible in OAI
-     * TODO return all sets based on PublicationState that contain documents
      */
     public function getSets($document = null)
     {
@@ -64,12 +76,23 @@ class Oai_Model_Set_PublicationStateSets implements Oai_Model_Set_SetTypeInterfa
 
         if ($document !== null) {
             $publicationState = $document->getPublicationState();
-            if ($publicationState !== null) {
+            $subset           = $this->getSubsetName($publicationState);
+
+            if ($publicationState !== null && $subset !== null) {
                 // TODO check if document is visible in OAI
+                $setSpec        = $this->setName . ':' . $subset;
+                $sets[$setSpec] = $subset;
             }
         } else {
-            // TODO return all sets based on PublicationState containing documents
-            //      should be similar to DocumentTypeSets
+            $finder = Repository::getInstance()->getDocumentFinder();
+            $finder->setServerState(Document::STATE_PUBLISHED);
+
+            $states = $finder->getPublicationStateCount();
+            foreach ($states as $state => $docCount) {
+                $subset         = $this->getSubsetName($state);
+                $setSpec        = $this->setName . ':' . $subset;
+                $sets[$setSpec] = $subset;
+            }
         }
 
         return $sets;
@@ -81,7 +104,11 @@ class Oai_Model_Set_PublicationStateSets implements Oai_Model_Set_SetTypeInterfa
      */
     public function supports($setName)
     {
-        return $setName->getSetName() === $this->setName;
+        if ($setName->getSetName() !== $this->setName) {
+            return false;
+        }
+
+        return in_array($setName->getSubsetName(), $this->getSubsetNames());
     }
 
     /**
@@ -93,17 +120,56 @@ class Oai_Model_Set_PublicationStateSets implements Oai_Model_Set_SetTypeInterfa
     {
         $subsetName = $setName->getSubsetName();
 
-        if ($setName->getSetPartsCount() === 2 && in_array($subsetName, $this->getSubsetNames())) {
-            $finder->setPublicationState($subsetName);
-        } else {
+        if (! $this->supports($setName)) {
             throw new Oai_Model_Exception(
-
+                'The given set results in an empty list: ' . $setName->getFullSetName(),
+                Oai_Model_Error::NORECORDSMATCH
             );
         }
+
+        $publicationStates = $this->getPublicationStates($subsetName);
+        $finder->setPublicationState($publicationStates);
     }
 
+    /**
+     * @return string[]
+     */
     protected function getSubsetNames()
     {
         return $this->subsetNames;
+    }
+
+    /**
+     * @param string $publicationState
+     * @return string|null
+     */
+    public function getSubsetName($publicationState)
+    {
+        if (array_key_exists($publicationState, $this->subsetMapping)) {
+            return $this->subsetMapping[$publicationState];
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param string $subset
+     * @return string[]
+     */
+    public function getPublicationStates($subset)
+    {
+        $states = [];
+
+        switch ($subset) {
+            case 'updatedVersion':
+                $states[] = PublicationState::CORRECTED;
+                $states[] = PublicationState::ENHANCED;
+                break;
+
+            default:
+                return array_search($subset, $this->subsetMapping);
+        }
+
+        return $states;
     }
 }
--- tests/modules/oai/models/Set/PublicationStateSetsTest.php
+++ tests/modules/oai/models/Set/PublicationStateSetsTest.php
@@ -29,9 +29,9 @@
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  */
 
-use Opus\Common\Collection;
-use Opus\Common\CollectionRole;
 use Opus\Common\Document;
+use Opus\Common\PublicationState;
+use Opus\Common\Repository;
 
 class Oai_Model_Set_PublicationStateSetsTest extends ControllerTestCase
 {
@@ -40,29 +40,153 @@ class Oai_Model_Set_PublicationStateSetsTest extends ControllerTestCase
 
     public function testGetSets()
     {
+        $model = new Oai_Model_Set_PublicationStateSets();
 
+        $sets = $model->getSets();
+
+        $this->assertIsArray($sets);
+        $this->assertCount(1, $sets);
+        $this->assertEquals([
+            'status-type:draft' => 'draft',
+        ], $sets);
     }
 
     public function testGetSetsForDocument()
     {
+        $model = new Oai_Model_Set_PublicationStateSets();
 
+        $doc = $this->createTestDocument();
+        $doc->setServerState(Document::STATE_PUBLISHED);
+        $doc->setPublicationState(PublicationState::ACCEPTED);
+        $doc->store();
+
+        $sets = $model->getSets($doc);
+
+        $this->assertIsArray($sets);
+        $this->assertCount(1, $sets);
+        $this->assertEquals([
+            'status-type:acceptedVersion' => 'acceptedVersion',
+        ], $sets);
     }
 
-    public function testGetSetsOnlyContainingDocuments()
+    public function testGetSetsForUnpublishedDocument()
     {
+        $model = new Oai_Model_Set_PublicationStateSets();
 
+        $doc = $this->createTestDocument();
+        $doc->setServerState(Document::STATE_UNPUBLISHED);
+        $doc->setPublicationState(PublicationState::ACCEPTED);
+        $doc->store();
+
+        // TODO should ServerState be checked and no set be returned?
+        $sets = $model->getSets($doc);
+
+        $this->assertIsArray($sets);
+        $this->assertCount(1, $sets);
+        $this->assertEquals([
+            'status-type:acceptedVersion' => 'acceptedVersion',
+        ], $sets);
     }
 
-    public function testSupports()
+    public function testGetSetsNoSetForProof()
     {
+        $model = new Oai_Model_Set_PublicationStateSets();
+
+        $doc = $this->createTestDocument();
+        $doc->setServerState(Document::STATE_PUBLISHED);
+        $doc->setPublicationState(PublicationState::PROOF);
+        $doc->store();
+
+        $sets = $model->getSets($doc);
+
+        $this->assertIsArray($sets);
+        $this->assertCount(0, $sets);
     }
 
-    public function testSupportsForUnsupportedSet()
+    /**
+     * @return array[]
+     */
+    public function setSpecDataProvider()
     {
+        return [
+            ['status-type:draft', true],
+            ['status-type:acceptedVersion', true],
+            ['status-type', false],
+            ['status-type:proof', false],
+        ];
+    }
 
+    /**
+     * @param string $setSpec
+     * @param bool   $expected
+     * @dataProvider setSpecDataProvider
+     */
+    public function testSupports($setSpec, $expected)
+    {
+        $model = new Oai_Model_Set_PublicationStateSets();
+
+        $this->assertEquals($expected, $model->supports(new Oai_Model_Set_SetName($setSpec)));
     }
 
     public function testConfigureFinder()
     {
+        $model = new Oai_Model_Set_PublicationStateSets();
+
+        $finder = Repository::getInstance()->getDocumentFinder();
+
+        $setName = new Oai_Model_Set_SetName('status-type:draft');
+        $model->configureFinder($finder, $setName);
+        $this->assertGreaterThan(0, $finder->getIds());
+
+        $setName = new Oai_Model_Set_SetName('status-type:submittedVersion');
+        $model->configureFinder($finder, $setName);
+        $this->assertCount(0, $finder->getIds());
+    }
+
+    public function testConfigureFinderUnsupportedSet()
+    {
+        $model = new Oai_Model_Set_PublicationStateSets();
+
+        $finder  = Repository::getInstance()->getDocumentFinder();
+        $setName = new Oai_Model_Set_SetName('pub-status:draft');
+
+        $this->expectException(Oai_Model_Exception::class);
+        $model->configureFinder($finder, $setName);
+    }
+
+    public function testConfigureFinderUnsupportedSubset()
+    {
+        $model = new Oai_Model_Set_PublicationStateSets();
+
+        $finder  = Repository::getInstance()->getDocumentFinder();
+        $setName = new Oai_Model_Set_SetName('status-type:proof');
+
+        $this->expectException(Oai_Model_Exception::class);
+        $model->configureFinder($finder, $setName);
+    }
+
+    public function testGetSubsetName()
+    {
+        $model = new Oai_Model_Set_PublicationStateSets();
+
+        $this->assertEquals('submittedVersion', $model->getSubsetName(PublicationState::SUBMITTED));
+        $this->assertEquals('updatedVersion', $model->getSubsetName(PublicationState::CORRECTED));
+        $this->assertEquals('updatedVersion', $model->getSubsetName(PublicationState::ENHANCED));
+        $this->assertNull($model->getSubsetName(PublicationState::PROOF));
+        $this->assertNull($model->getSubsetName(PublicationState::AUTHORS_VERSION));
+    }
+
+    public function testGetPublicationStates()
+    {
+        $model = new Oai_Model_Set_PublicationStateSets();
+
+        $states = $model->getPublicationStates('updatedVersion');
+
+        $this->assertIsArray($states);
+        $this->assertCount(2, $states);
+        $this->assertEqualsCanonicalizing([
+            PublicationState::ENHANCED,
+            PublicationState::CORRECTED,
+        ], $states);
     }
 }
