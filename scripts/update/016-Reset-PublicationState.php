0710b0c06650a775298cc7ddaa6b01f0842ee30d
j3nsch <j3nsch@gmail.com>
2026-02-06 13:06:35 +0100

    #1224 Add help to update script

--- scripts/update/016-Reset-PublicationState.php
+++ scripts/update/016-Reset-PublicationState.php
@@ -12,8 +12,24 @@
 require_once dirname(__FILE__) . '/../common/update.php';
 
 use Opus\Database;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+$help = <<<TXT
+Resetting <info>PublicationState</info>
+--------------------------
+
+In the past the document field <info>PublicationState</info> already existed in the database, but it was not used in standard OPUS 4. For OPUS 4.9 the field has been activated and integrated into the user interface. 
+
+The field should be reset before starting to use it, because it might have been set to DRAFT or other default values that are not correct.
+
+<error>If OPUS 4 was modified locally in order to use PublicationState, you might want to keep the current values.</error>
+TXT;
 
 $helper = new Application_Update_Helper();
+
+$output = new ConsoleOutput();
+$output->writeln($help);
+$output->writeln('');
 
 if ($helper->askYesNo('Reset PublicationState of all documents to NULL [Y|n]? ', true)) {
     $helper->log('Reseting PublicationState values ...');
