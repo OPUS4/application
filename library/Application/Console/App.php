4085f82f90a2d0f7747f344188e0c2089be73476
j3nsch <j3nsch@gmail.com>
2026-04-21 14:09:52 +0200

    #630 addCommand not supported in PHP 8.1

--- library/Application/Console/App.php
+++ library/Application/Console/App.php
@@ -49,49 +49,49 @@ class Application_Console_App extends Application
         $commands        = $commandProvider->getCommands();
 
         foreach ($commands as $command) {
-            $this->addCommand($command);
+            $this->add($command);
         }
 
-        // $this->addCommand(new Application_Console_Index_RepairCommand());
-        // $this->addCommand(new Application_Console_Index_CheckCommand());
+        // $this->add(new Application_Console_Index_RepairCommand());
+        // $this->add(new Application_Console_Index_CheckCommand());
 
-        $this->addCommand(new Application_Console_Document_DeleteCommand());
-        $this->addCommand(new Application_Console_Document_DuplicatesCommand());
-        $this->addCommand(new Application_Console_Document_DiffCommand());
+        $this->add(new Application_Console_Document_DeleteCommand());
+        $this->add(new Application_Console_Document_DuplicatesCommand());
+        $this->add(new Application_Console_Document_DiffCommand());
 
-        $this->addCommand(new Application_Console_Orcid_InfoCommand());
-        $this->addCommand(new Application_Console_Orcid_NormalizeCommand());
-        $this->addCommand(new Application_Console_Orcid_ValidateCommand());
+        $this->add(new Application_Console_Orcid_InfoCommand());
+        $this->add(new Application_Console_Orcid_NormalizeCommand());
+        $this->add(new Application_Console_Orcid_ValidateCommand());
 
-        $this->addCommand(new Application_Console_Person_CleanCommand());
+        $this->add(new Application_Console_Person_CleanCommand());
 
-        $this->addCommand(new Application_Console_Debug_DocumentXmlCommand());
+        $this->add(new Application_Console_Debug_DocumentXmlCommand());
 
-        $this->addCommand(new Application_Console_Console_ConsoleCommand());
-        $this->addCommand(new Application_Console_Console_ExecCommand());
+        $this->add(new Application_Console_Console_ConsoleCommand());
+        $this->add(new Application_Console_Console_ExecCommand());
 
-        $this->addCommand(new Application_Console_Collection_CopyCommand());
-        $this->addCommand(new Application_Console_Collection_MoveCommand());
-        $this->addCommand(new Application_Console_Collection_RemoveCommand());
+        $this->add(new Application_Console_Collection_CopyCommand());
+        $this->add(new Application_Console_Collection_MoveCommand());
+        $this->add(new Application_Console_Collection_RemoveCommand());
 
-        $this->addCommand(new Application_Console_Admin_ChangePasswordCommand());
+        $this->add(new Application_Console_Admin_ChangePasswordCommand());
 
         if (class_exists(TaskManager::class)) {
             /*
                 Tasks commands do not work without the TaskManager. If the current PHP version is less than 7.4
                 there will be no TaskManager due to lack of crunz support.
             */
-            $this->addCommand(new Application_Console_Task_InfoCommand());
-            $this->addCommand(new Application_Console_Task_ListCommand());
-            $this->addCommand(new Application_Console_Task_RunCommand());
+            $this->add(new Application_Console_Task_InfoCommand());
+            $this->add(new Application_Console_Task_ListCommand());
+            $this->add(new Application_Console_Task_RunCommand());
         }
 
         // TODO use ModelCommandProvider
-        $this->addCommand(new Application_Console_Model_EnrichmentImportCommand());
-        $this->addCommand(new Application_Console_Model_EnrichmentListCommand());
-        $this->addCommand(new Application_Console_Model_EnrichmentExportCommand());
-        $this->addCommand(new Application_Console_Model_EnrichmentRenameCommand());
-        $this->addCommand(new Application_Console_Model_EnrichmentDeleteCommand());
+        $this->add(new Application_Console_Model_EnrichmentImportCommand());
+        $this->add(new Application_Console_Model_EnrichmentListCommand());
+        $this->add(new Application_Console_Model_EnrichmentExportCommand());
+        $this->add(new Application_Console_Model_EnrichmentRenameCommand());
+        $this->add(new Application_Console_Model_EnrichmentDeleteCommand());
 
         $this->setDefaultCommand('list');
     }
